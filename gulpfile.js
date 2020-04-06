b413143b60af1603dc0d1d80f3029fcaa17bec42
kyranjamie <kyran.burraston@gmail.com>
2020-04-06 09:07:20 +0200

    ci: lint openapi spec

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-const { src, dest, parallel } = require('gulp');
+const { src, dest, parallel, series } = require('gulp');
 const path = require('path');
 const prettier = require('gulp-prettier');
 const jsonschemaDeref = require('gulp-jsonschema-deref');
 const ghPages = require('gulp-gh-pages');
+const del = require('del');
 
 const schemaFiles = 'docs/**/*.schema.json';
 const buildFolder = '.tmp';
@@ -22,9 +23,13 @@ function copyFiles() {
 
 function deployToGithubPages() {
   const file = path.join(buildFolder, 'index.html'); 
-  return src([file]).pipe(ghPages({ cacheDir: buildFolder }));
+  return src([file]).pipe(ghPages());
+}
+
+function clean() {
+  return del('.publish');
 }
 
 exports.default = parallel(flattenSchemas, copyFiles);
 
-exports.deployDocs = deployToGithubPages;
+exports.deployDocs = series(deployToGithubPages, clean);
