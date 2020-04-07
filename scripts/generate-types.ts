c4f86e5893776e974ee74cb473a2538ad83ace07
kyranjamie <kyran.burraston@gmail.com>
2020-04-07 15:52:49 +0200

    refactor: type generation

--- scripts/generate-types.ts
+++ scripts/generate-types.ts
@@ -1,35 +1,47 @@
-#!/usr/bin/env ts-node
-'use strict';
-
 import { promises as fs } from 'fs';
+import * as path from 'path';
+
 import * as glob from 'glob';
+import { JSONSchema4 } from 'json-schema';
+import * as mergeSchemas from 'json-schema-merge-allof';
+import * as deref from '@apidevtools/json-schema-ref-parser';
 import { compile } from 'json-schema-to-typescript';
 
-const dataEncoding = 'utf8';
-const shouldGenerateTypeFor = (schema: { title?: string }) => !!schema.title;
-const typeFileName = '.tmp/index.ts';
+const root = path.join(__dirname, '..');
+const docsPath = path.join(root, 'docs');
+const tmpPath = path.join(root, '.tmp');
+const typeFilePath = path.join(tmpPath, 'index.d.ts');
 
-const clearFile = async () => await fs.writeFile(typeFileName, '');
+const clearFile = async () => {
+  try {
+    await fs.access(tmpPath);
+  } catch (e) {
+    await fs.mkdir(tmpPath);
+  }
+  await fs.writeFile(typeFilePath, '', 'utf8');
+};
 
-glob('.tmp/**/*.schema.json', (_err, files) => {
-  if (files.length === 0) {
-    console.log('No schemas. Have you ran `yarn deref-schemas`?');
+glob(path.join(docsPath, '**/*.schema.json'), async (err, files) => {
+  if (err) throw err;
+
+  await clearFile();
+
+  for await (const file of files) {
+    const derefedSchema = await deref.dereference(file);
+    const schema = mergeSchemas(derefedSchema, {
+      ignoreAdditionalProperties: true,
+    });
+    if (!schema.title) continue;
+    if (schema.type === 'object') {
+      schema.additionalProperties = false;
+    }
+    const outputType = await compile(schema as JSONSchema4, schema.title, {
+      bannerComment: '',
+      strictIndexSignatures: true,
+      declareExternallyReferenced: false,
+    });
+    await fs.appendFile(typeFilePath, outputType);
   }
 
-  (async function () {
-    await clearFile();
-
-    for await (const file of files) {
-      const schema = JSON.parse(await fs.readFile(file, dataEncoding));
-
-      if (!shouldGenerateTypeFor(schema)) continue;
-
-      const result = await compile(schema, schema.title, {
-        bannerComment: '',
-      });
-
-      await fs.appendFile(typeFileName, result);
-    }
-    process.exit(0);
-  })();
+  process.exit(0);
 });
