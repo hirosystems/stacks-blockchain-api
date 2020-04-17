787f19fef5b7b348bd6874d218e1de1cfa979fbe
Matthew Little <zone117x@gmail.com>
2020-04-17 17:06:53 +0200

    feat: add cors to core-node RPC proxy

---
+++ src/api/routes/core-node-rpc-proxy.ts
@@ -0,0 +1,26 @@
+import * as express from 'express';
+import * as cors from 'cors';
+import * as expressProxy from 'express-http-proxy';
+import { StacksCoreRpcClient } from '../../core-rpc/client';
+
+export function createCoreNodeRpcProxyRouter(): express.Router {
+  const router = express.Router();
+  router.use(cors());
+
+  const stacksNodeRpcEndpoint = new StacksCoreRpcClient().endpoint;
+
+  const proxyPathResolver = (req: express.Request): string | Promise<string> => {
+    console.info(`Proxy core-node RPC: ${req.originalUrl}`);
+    return req.originalUrl;
+  };
+
+  const stacksNodeRpcProxy = expressProxy(stacksNodeRpcEndpoint, {
+    https: false,
+    proxyErrorHandler: cors(),
+    proxyReqPathResolver: proxyPathResolver,
+  });
+
+  router.use(stacksNodeRpcProxy);
+
+  return router;
+}
