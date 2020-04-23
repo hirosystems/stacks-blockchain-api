0cb08a812cd69941e01ae896dea9d4c1a4440491
zone117x <zone117x@gmail.com>
2020-04-23 21:45:05 +0200

    fix: update follower stx addresses

--- src/api/routes/debug.ts
+++ src/api/routes/debug.ts
@@ -26,22 +26,22 @@ import { StacksCoreRpcClient } from '../../core-rpc/client';
 import { pubKeyfromPrivKey } from '@blockstack/stacks-transactions/lib/src/keys';
 
 const testnetKeys: { secretKey: string; stacksAddress: string }[] = [
-  {
-    secretKey: 'b8d99fd45da58038d630d9855d3ca2466e8e0f89d3894c4724f0efc9ff4b51f001',
-    stacksAddress: 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH',
+  { 
+    secretKey: "cb3df38053d132895220b9ce471f6b676db5b9bf0b4adefb55f2118ece2478df01",
+    stacksAddress: "STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6"
   },
-  {
-    secretKey: '3a4e84abb8abe0c1ba37cef4b604e73c82b1fe8d99015cb36b029a65099d373601',
-    stacksAddress: 'ST26FVX16539KKXZKJN098Q08HRX3XBAP541MFS0P',
+  { 
+    secretKey: "21d43d2ae0da1d9d04cfcaac7d397a33733881081f0b2cd038062cf0ccbb752601",
+    stacksAddress: "ST11NJTTKGVT6D1HY4NJRVQWMQM7TVAR091EJ8P2Y"
   },
-  {
-    secretKey: '052cc5b8f25b1e44a65329244066f76c8057accd5316c889f476d0ea0329632c01',
-    stacksAddress: 'ST3CECAKJ4BH08JYY7W53MC81BYDT4YDA5M7S5F53',
+  { 
+    secretKey: "c71700b07d520a8c9731e4d0f095aa6efb91e16e25fb27ce2b72e7b698f8127a01",
+    stacksAddress: "ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR"
   },
-  {
-    secretKey: '9aef533e754663a453984b69d36f109be817e9940519cc84979419e2be00864801',
-    stacksAddress: 'ST31HHVBKYCYQQJ5AQ25ZHA6W2A548ZADDQ6S16GP',
-  },
+  { 
+    secretKey: "e75dcb66f84287eaf347955e94fa04337298dbd95aa0dbb985771104ef1913db01",
+    stacksAddress: "STRYYQQ9M8KAF4NS7WNZQYY59X93XEKR31JP64CP"
+  }
 ];
 
 export function createDebugRouter(db: DataStore): RouterWithAsync {
