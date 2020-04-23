efbdffc3dab7d969da5c35ff6c933ba098b8b94c
zone117x <zone117x@gmail.com>
2020-04-23 21:47:35 +0200

    fix: lint errors

--- src/api/routes/debug.ts
+++ src/api/routes/debug.ts
@@ -26,22 +26,22 @@ import { StacksCoreRpcClient } from '../../core-rpc/client';
 import { pubKeyfromPrivKey } from '@blockstack/stacks-transactions/lib/src/keys';
 
 const testnetKeys: { secretKey: string; stacksAddress: string }[] = [
-  { 
-    secretKey: "cb3df38053d132895220b9ce471f6b676db5b9bf0b4adefb55f2118ece2478df01",
-    stacksAddress: "STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6"
+  {
+    secretKey: 'cb3df38053d132895220b9ce471f6b676db5b9bf0b4adefb55f2118ece2478df01',
+    stacksAddress: 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6',
   },
-  { 
-    secretKey: "21d43d2ae0da1d9d04cfcaac7d397a33733881081f0b2cd038062cf0ccbb752601",
-    stacksAddress: "ST11NJTTKGVT6D1HY4NJRVQWMQM7TVAR091EJ8P2Y"
+  {
+    secretKey: '21d43d2ae0da1d9d04cfcaac7d397a33733881081f0b2cd038062cf0ccbb752601',
+    stacksAddress: 'ST11NJTTKGVT6D1HY4NJRVQWMQM7TVAR091EJ8P2Y',
   },
-  { 
-    secretKey: "c71700b07d520a8c9731e4d0f095aa6efb91e16e25fb27ce2b72e7b698f8127a01",
-    stacksAddress: "ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR"
+  {
+    secretKey: 'c71700b07d520a8c9731e4d0f095aa6efb91e16e25fb27ce2b72e7b698f8127a01',
+    stacksAddress: 'ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR',
   },
-  { 
-    secretKey: "e75dcb66f84287eaf347955e94fa04337298dbd95aa0dbb985771104ef1913db01",
-    stacksAddress: "STRYYQQ9M8KAF4NS7WNZQYY59X93XEKR31JP64CP"
-  }
+  {
+    secretKey: 'e75dcb66f84287eaf347955e94fa04337298dbd95aa0dbb985771104ef1913db01',
+    stacksAddress: 'STRYYQQ9M8KAF4NS7WNZQYY59X93XEKR31JP64CP',
+  },
 ];
 
 export function createDebugRouter(db: DataStore): RouterWithAsync {
