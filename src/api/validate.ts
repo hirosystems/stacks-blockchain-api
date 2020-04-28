cc7fe155b4aa0e4c9d5d481e607342fd8487b199
kyranjamie <kyran.burraston@gmail.com>
2020-04-28 13:49:42 +0200

    fix: warn on validation fail

--- src/api/validate.ts
+++ src/api/validate.ts
@@ -4,5 +4,5 @@ export async function validate(schema: any, data: any) {
   if (process.env.NODE_ENV !== 'development') return;
   const ajv = new Ajv({ schemaId: 'auto' });
   const valid = await ajv.validate(schema, data);
-  if (!valid) throw new Error(`Schema validation:\n\n ${JSON.stringify(ajv.errors, null, 2)}`);
+  if (!valid) console.warn(`Schema validation:\n\n ${JSON.stringify(ajv.errors, null, 2)}`);
 }
