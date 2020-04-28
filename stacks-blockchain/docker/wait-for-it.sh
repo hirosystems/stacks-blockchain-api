fbc31a6eef7636aa00e40dd45dd1e0ae45f89fc9
Matthew Little <zone117x@gmail.com>
2020-04-28 11:05:12 +0200

    fix: add docker-host workaround for linux

--- stacks-blockchain/docker/wait-for-it.sh
+++ stacks-blockchain/docker/wait-for-it.sh
@@ -5,6 +5,13 @@ WAITFORIT_cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
 
+HOST_DOMAIN="host.docker.internal"
+ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
+  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
+fi
+
 usage()
 {
     cat << USAGE >&2
