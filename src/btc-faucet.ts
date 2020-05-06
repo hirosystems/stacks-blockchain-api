29ac3a1c1118dd8abd34412c79a0f20c490e5f84
Matthew Little <zone117x@gmail.com>
2020-05-06 15:52:24 +0200

    feat: add bitcoind to integration tests

--- src/btc-faucet.ts
+++ src/btc-faucet.ts
@@ -1,87 +1,177 @@
 import { RPCClient } from 'rpc-bitcoin';
 import * as btc from 'bitcoinjs-lib';
-import * as BN from 'bn.js';
+import { parsePort } from './helpers';
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import * as coinSelect from 'coinselect';
 
-type UnspentTxInfo = {
-  txid: string;
-  vout: number;
-  address: string;
-  acount: string;
-  scriptPubKey: string;
-  amount: number;
-  confirmations: number;
-  redeemScript: string;
-  spendable: boolean;
-  solvable: boolean;
-  safe: boolean;
-};
+export function getFaucetPk(): string {
+  const { BTC_FAUCET_PK } = process.env;
+  if (!BTC_FAUCET_PK) {
+    throw new Error('BTC Faucet not fully configured.');
+  }
+  return BTC_FAUCET_PK;
+}
 
-export const makeBTCFaucetPayment = async (address: string) => {
-  const { BTC_FAUCET_PK, BTC_RPC_PORT, BTC_RPC_HOST, BTC_RPC_PW, BTC_RPC_USER } = process.env;
-  if (!BTC_FAUCET_PK || !BTC_RPC_PORT || !BTC_RPC_HOST || !BTC_RPC_PW || !BTC_RPC_USER) {
+export function getFaucetWallet(
+  network: btc.Network
+): { key: btc.ECPairInterface; wif: string; address: string } {
+  const wif = getFaucetPk();
+  const key = btc.ECPair.fromWIF(wif, network);
+  const addr = getKeyAddress(key);
+  return {
+    key: key,
+    wif: wif,
+    address: addr,
+  };
+}
+
+export function getKeyAddress(key: btc.ECPairInterface): string {
+  const { address } = btc.payments.p2pkh({
+    pubkey: key.publicKey,
+    network: key.network,
+  });
+  if (!address) {
+    throw new Error('address generation failed');
+  }
+  return address;
+}
+
+export function getRpcClient(): RPCClient {
+  const { BTC_RPC_PORT, BTC_RPC_HOST, BTC_RPC_PW, BTC_RPC_USER } = process.env;
+  if (!BTC_RPC_PORT || !BTC_RPC_HOST || !BTC_RPC_PW || !BTC_RPC_USER) {
     throw new Error('BTC Faucet not fully configured.');
   }
   const client = new RPCClient({
     url: BTC_RPC_HOST,
-    port: parseInt(BTC_RPC_PORT, 10),
+    port: parsePort(BTC_RPC_PORT),
     user: BTC_RPC_USER,
     pass: BTC_RPC_PW,
   });
+  return client;
+}
 
-  const network = btc.networks.regtest;
+interface TxOutSetUnspent {
+  amount: number;
+  desc: string;
+  height: number;
+  scriptPubKey: string;
+  txid: string;
+  vout: number;
+}
 
-  const faucetPK = btc.ECPair.fromWIF(BTC_FAUCET_PK, btc.networks.regtest);
-  const faucetPayment = btc.payments.p2wpkh({
-    pubkey: faucetPK.publicKey,
-    network,
+interface TxOutSet {
+  bestblock: string;
+  height: number;
+  success: boolean;
+  total_amount: number;
+  txouts: number;
+  unspents: TxOutSetUnspent[];
+}
+
+interface PsbtInput {
+  txId: string;
+  txRaw: Buffer;
+  value: number;
+  script: Buffer;
+  vout: number;
+}
+
+interface PsbtOutput {
+  address: string;
+  value: number;
+}
+
+function performCoinSelect(
+  inputs: PsbtInput[],
+  outputs: PsbtOutput[],
+  feeRate: number
+): {
+  inputs: PsbtInput[];
+  outputs: PsbtOutput[];
+} {
+  const coinSelectResult = coinSelect(inputs, outputs, feeRate);
+  if (!coinSelectResult.inputs || !coinSelectResult.outputs) {
+    throw new Error('no utxo coin select solution found');
+  }
+  return coinSelectResult;
+}
+
+// Replace with client.estimatesmartfee() for testnet/mainnet
+const REGTEST_FEE_RATE = 2000;
+
+// TODO: this should be wrapped in p-queue so only one is performed at a time
+export async function makeBTCFaucetPayment(
+  network: btc.Network,
+  address: string,
+  /** Amount to send in BTC */
+  faucetAmount: number
+): Promise<{ txId: string }> {
+  const client = getRpcClient();
+  const faucetWallet = getFaucetWallet(network);
+
+  const faucetAmountSats = faucetAmount * 1e8;
+
+  const txOutSet: TxOutSet = await client.scantxoutset({
+    action: 'start',
+    scanobjects: [`addr(${faucetWallet.address})`],
   });
-  const p2sh = btc.payments.p2sh({ redeem: faucetPayment, network });
 
-  const unspent: UnspentTxInfo[] = await client.listunspent({ minconf: 4 });
+  const spendableUtxos = txOutSet.unspents.filter(utxo => txOutSet.height - utxo.height > 100);
+  const totalSpendableAmount = spendableUtxos.reduce((prev, utxo) => prev + utxo.amount, 0);
+  if (totalSpendableAmount < faucetAmount) {
+    throw new Error(`not enough total amount in utxo set: ${totalSpendableAmount}`);
+  }
 
-  const faucetAmount = new BN(50000000); // 0.5 BTC
-  const inputs: UnspentTxInfo[] = [];
-  const satsPerBTC = new BN(100000000);
-  let inputTotals = new BN(0);
-  const btcToSat = (btcVal: number) => {
-    const btc = new BN(btcVal);
-    const sats = btc.mul(satsPerBTC);
-    return sats;
-  };
-  for (const input of unspent) {
-    const sats = btcToSat(input.amount);
-    inputTotals = inputTotals.add(sats);
-    inputs.push(input);
-    if (inputTotals.gt(faucetAmount)) {
+  const minUtxoSet: PsbtInput[] = [];
+  let minAmount = 0;
+  // Typical btc transaction with 1 input and 2 outputs is around 250 bytes
+  const estimatedTotalFee = 500 * REGTEST_FEE_RATE;
+  for (const utxo of spendableUtxos) {
+    const rawTxHex = await client.getrawtransaction({ txid: utxo.txid });
+    const txOut = btc.Transaction.fromHex(rawTxHex).outs[utxo.vout];
+    const rawTxBuffer = Buffer.from(rawTxHex, 'hex');
+    minUtxoSet.push({
+      script: txOut.script,
+      value: txOut.value,
+      txRaw: rawTxBuffer,
+      txId: utxo.txid,
+      vout: utxo.vout,
+    });
+    minAmount += utxo.amount;
+    if (minAmount >= faucetAmount + estimatedTotalFee) {
       break;
     }
   }
 
-  const tx = new btc.TransactionBuilder(btc.networks.regtest);
-  console.log(`Using ${inputs.length} inputs, worth ${inputTotals.toNumber()}`);
-  inputs.forEach((input, i) => {
-    tx.addInput(input.txid, i);
+  const outputTarget: PsbtOutput = { address: address, value: faucetAmountSats };
+  const coinSelectResult = performCoinSelect(minUtxoSet, [outputTarget], REGTEST_FEE_RATE);
+  const psbt = new btc.Psbt({ network: network });
+
+  coinSelectResult.inputs.forEach(input => {
+    psbt.addInput({
+      hash: input.txId,
+      index: input.vout,
+      nonWitnessUtxo: input.txRaw,
+    });
   });
 
-  const receipientScriptPub = btc.address.toOutputScript(address, btc.networks.regtest);
-  tx.addOutput(receipientScriptPub, faucetAmount.toNumber());
-
-  const change = inputTotals.sub(faucetAmount);
-  console.log('Change amount:', change.toNumber());
-  const faucetScriptPub = btc.address.toOutputScript(
-    faucetPayment.address as string,
-    btc.networks.regtest
-  );
-  const fee = new BN(500);
-  tx.addOutput(faucetScriptPub, change.sub(fee).toNumber());
-
-  inputs.forEach((input, i) => {
-    tx.sign(i, faucetPK, p2sh.redeem?.output, undefined, btcToSat(input.amount).toNumber());
+  coinSelectResult.outputs.forEach(output => {
+    if (!output.address) {
+      // output change address
+      output.address = faucetWallet.address;
+    }
+    psbt.addOutput({ address: output.address, value: output.value });
   });
 
-  const builtTX = tx.build();
-  console.log(`Faucet TXID: ${builtTX.getId()}`);
-  await client.sendrawtransaction({ hexstring: builtTX.toHex() });
+  psbt.signAllInputs(faucetWallet.key);
+  if (!psbt.validateSignaturesOfAllInputs()) {
+    throw new Error('invalid pbst signature');
+  }
+  psbt.finalizeAllInputs();
 
-  return builtTX;
-};
+  const tx = psbt.extractTransaction();
+  const sendTxResult: string = await client.sendrawtransaction({ hexstring: tx.toHex() });
+
+  return { txId: sendTxResult };
+}
--- src/tests/faucet-tests.ts
+++ src/tests/faucet-tests.ts
@@ -1,7 +1,30 @@
-import { makeBTCFaucetPayment } from '../btc-faucet';
+import * as bitcoin from 'bitcoinjs-lib';
+import { makeBTCFaucetPayment, getRpcClient, getFaucetWallet, getKeyAddress } from '../btc-faucet';
 
-test.skip('trying out btc faucet', async () => {
-  const addr = 'n1CVz1z4UTaGCzXHPBujyYyNdC9Vrf4MUv';
-  const tx = await makeBTCFaucetPayment(addr);
-  expect(tx.getId()).not.toBeFalsy();
+test('test btc faucet', async () => {
+  const regtest = bitcoin.networks.regtest;
+
+  const client = getRpcClient();
+
+  const wallet = getFaucetWallet(regtest);
+
+  // Mint btc to the faucet wallet address.
+  await client.generatetoaddress({ address: wallet.address, nblocks: 110 });
+
+  const btcToSend = 55.2;
+  const recipientAddress = getKeyAddress(bitcoin.ECPair.makeRandom({ network: regtest }));
+  const paymentResult = await makeBTCFaucetPayment(regtest, recipientAddress, btcToSend);
+  expect(paymentResult.txId).toBeTruthy();
+
+  // Mine some blocks
+  await client.generatetoaddress({ address: wallet.address, nblocks: 10 });
+
+  // Test that recipient address received btc
+  await client.createwallet({ wallet_name: 'recipient_wallet' });
+  await client.importaddress({ address: recipientAddress, rescan: true }, 'recipient_wallet');
+  const getBalanceResult: number = await client.getbalance(
+    { include_watchonly: true },
+    'recipient_wallet'
+  );
+  expect(getBalanceResult).toBe(btcToSend);
 });
