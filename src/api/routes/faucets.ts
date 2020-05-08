450141df70ca60d583805d7dc60d302fb3d069f6
Matthew Little <zone117x@gmail.com>
2020-05-08 12:54:52 +0200

    feat: faucet endpoint to check account balance

--- src/api/routes/faucets.ts
+++ src/api/routes/faucets.ts
@@ -2,23 +2,29 @@ import * as express from 'express';
 import { addAsync, RouterWithAsync } from '@awaitjs/express';
 import * as btc from 'bitcoinjs-lib';
 import PQueue from 'p-queue';
-import { makeBTCFaucetPayment } from '../../btc-faucet';
+import { makeBtcFaucetPayment, getBtcBalance } from '../../btc-faucet';
 
 export function createFaucetRouter(): RouterWithAsync {
   const router = addAsync(express.Router());
 
   const faucetRequestQueue = new PQueue({ concurrency: 1 });
 
-  router.postAsync('/btc', async (req: express.Request, res: express.Response) => {
+  router.postAsync('/btc', async (req, res) => {
     const address: string = req.query.address || req.body.address;
     const tx = await faucetRequestQueue.add(async () => {
-      return await makeBTCFaucetPayment(btc.networks.regtest, address, 0.5);
+      return await makeBtcFaucetPayment(btc.networks.regtest, address, 0.5);
     });
     res.json({
       txId: tx.txId,
       rawTX: tx.rawTx,
       success: true,
     });
+  });
+
+  router.getAsync('/btc/:address', async (req, res) => {
+    const { address } = req.params;
+    const balance = await getBtcBalance(btc.networks.regtest, address);
+    res.json({ balance });
   });
 
   return router;
--- src/tests/faucet-tests.ts
+++ src/tests/faucet-tests.ts
@@ -1,5 +1,20 @@
 import * as bitcoin from 'bitcoinjs-lib';
-import { makeBTCFaucetPayment, getRpcClient, getFaucetWallet, getKeyAddress } from '../btc-faucet';
+import {
+  makeBtcFaucetPayment,
+  getRpcClient,
+  getFaucetWallet,
+  getKeyAddress,
+  getBtcBalance,
+} from '../btc-faucet';
+
+async function getBalanceWithWalletImport(address: string): Promise<number> {
+  const client = getRpcClient();
+  const walletName = `recipient_wallet_${address}`;
+  await client.createwallet({ wallet_name: walletName });
+  await client.importaddress({ address: address, rescan: true }, walletName);
+  const getBalanceResult: number = await client.getbalance({ include_watchonly: true }, walletName);
+  return getBalanceResult;
+}
 
 test('test btc faucet', async () => {
   const regtest = bitcoin.networks.regtest;
@@ -11,20 +26,38 @@ test('test btc faucet', async () => {
   // Mint btc to the faucet wallet address.
   await client.generatetoaddress({ address: wallet.address, nblocks: 110 });
 
-  const btcToSend = 55.2;
-  const recipientAddress = getKeyAddress(bitcoin.ECPair.makeRandom({ network: regtest }));
-  const paymentResult = await makeBTCFaucetPayment(regtest, recipientAddress, btcToSend);
-  expect(paymentResult.txId).toBeTruthy();
+  const btcToSend1 = 55.2;
+  const recipientAddress1 = getKeyAddress(bitcoin.ECPair.makeRandom({ network: regtest }));
+  const paymentResult1 = await makeBtcFaucetPayment(regtest, recipientAddress1, btcToSend1);
+  expect(paymentResult1.txId).toBeTruthy();
+
+  const btcToSend2 = 60.5;
+  const recipientAddress2 = getKeyAddress(bitcoin.ECPair.makeRandom({ network: regtest }));
+  const paymentResult2 = await makeBtcFaucetPayment(regtest, recipientAddress2, btcToSend2);
+  expect(paymentResult2.txId).toBeTruthy();
+
+  const btcToSend3 = 51.8;
+  const recipientAddress3 = getKeyAddress(bitcoin.ECPair.makeRandom({ network: regtest }));
+  const paymentResult3 = await makeBtcFaucetPayment(regtest, recipientAddress3, btcToSend3);
+  expect(paymentResult3.txId).toBeTruthy();
+
+  // Test balance with mempool transactions
+  const fetchedBalance3 = await getBtcBalance(regtest, recipientAddress3);
+  expect(fetchedBalance3).toBe(btcToSend3);
 
   // Mine some blocks
   await client.generatetoaddress({ address: wallet.address, nblocks: 10 });
 
-  // Test that recipient address received btc
-  await client.createwallet({ wallet_name: 'recipient_wallet' });
-  await client.importaddress({ address: recipientAddress, rescan: true }, 'recipient_wallet');
-  const getBalanceResult: number = await client.getbalance(
-    { include_watchonly: true },
-    'recipient_wallet'
-  );
-  expect(getBalanceResult).toBe(btcToSend);
+  // Test that recipient addresses received btc
+  const getBalanceResult1 = await getBalanceWithWalletImport(recipientAddress1);
+  expect(getBalanceResult1).toBe(btcToSend1);
+
+  const fetchedBalance1 = await getBtcBalance(regtest, recipientAddress1);
+  expect(fetchedBalance1).toBe(btcToSend1);
+
+  const getBalanceResult2 = await getBalanceWithWalletImport(recipientAddress2);
+  expect(getBalanceResult2).toBe(btcToSend2);
+
+  const fetchedBalance2 = await getBtcBalance(regtest, recipientAddress2);
+  expect(fetchedBalance2).toBe(btcToSend2);
 });
