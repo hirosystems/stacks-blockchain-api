da19ab07732fb23ae17ee369ceae256714cde20a
Matthew Little <zone117x@gmail.com>
2020-05-09 04:38:58 +0200

    chore: remove no longer used p2p stream socket reader code

--- src/p2p/messages.ts
+++ src/p2p/messages.ts
@@ -1,5 +1,5 @@
 import * as net from 'net';
-import { BinaryReader, BufferReader } from '../binary-reader';
+import { BufferReader } from '../binary-reader';
 import { readBlocks, Block } from './block';
 import { Transaction, readTransaction } from './tx';
 import { StacksMessageParsingError, NotImplementedError } from '../errors';
@@ -62,8 +62,7 @@ export const PREAMBLE_ENCODED_SIZE =
   MESSAGE_SIGNATURE_ENCODED_SIZE + // signature
   4; // payload_len
 
-async function readPreamble(stream: BinaryReader): Promise<Preamble> {
-  const cursor = await stream.readFixed(PREAMBLE_ENCODED_SIZE);
+export function readPreamble(cursor: BufferReader): Preamble {
   const preamble: Preamble = {
     peerVersion: cursor.readUInt32BE(),
     networkId: cursor.readUInt32BE(),
@@ -110,53 +109,3 @@ export function isStacksMessageTransaction(msg: StacksMessage): msg is StacksMes
 }
 
 type StacksMessage = StacksMessageBlocks | StacksMessageTransaction;
-
-export async function* readMessages(stream: BinaryReader): AsyncGenerator<StacksMessage> {
-  while (!stream.readableStream.destroyed) {
-    const preamble = await readPreamble(stream);
-    const reader = await stream.readFixed(preamble.payloadLength);
-    // ignore relayer data
-    readRelayers(reader);
-    const messageTypeId = reader.readUInt8Enum(StacksMessageTypeID, n => {
-      throw new StacksMessageParsingError(`unexpected Stacks message type ID ${n}`);
-    });
-    if (messageTypeId === StacksMessageTypeID.Blocks) {
-      const blocks = readBlocks(reader);
-      const msg: StacksMessageBlocks = {
-        messageTypeId: StacksMessageTypeID.Blocks,
-        blocks: blocks,
-      };
-      yield msg;
-    } else if (messageTypeId === StacksMessageTypeID.Transaction) {
-      const tx = readTransaction(reader);
-      const msg: StacksMessageTransaction = {
-        messageTypeId: StacksMessageTypeID.Transaction,
-        transaction: tx,
-      };
-      yield msg;
-    } else {
-      throw new NotImplementedError(
-        `stacks message type: ${getEnumDescription(StacksMessageTypeID, messageTypeId)}`
-      );
-    }
-  }
-}
-
-/**
- * Partial implementation of Stacks P2P message reader.
- */
-export async function p2pReadSocket(socket: net.Socket): Promise<void> {
-  const binaryReader = new BinaryReader(socket);
-  for await (const message of readMessages(binaryReader)) {
-    const msgType = message.messageTypeId;
-    if (msgType === StacksMessageTypeID.Blocks) {
-      console.log(`${Date.now()} Received Stacks message type: StacksMessageTypeID.Blocks`);
-    } else if (msgType === StacksMessageTypeID.Transaction) {
-      console.log(`${Date.now()} Received Stacks message type: StacksMessageTypeID.Transaction`);
-    } else {
-      throw new NotImplementedError(
-        `handler for message type: ${getEnumDescription(StacksMessageTypeID, msgType)}`
-      );
-    }
-  }
-}
