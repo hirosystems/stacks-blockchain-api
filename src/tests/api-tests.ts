0bbf8d8e8f4f2770b5f7d5bf6b5b0c3debe2d13a
Matthew Little <zone117x@gmail.com>
2020-05-14 12:42:06 +0200

    feat: clarity values returned as both hex string and repr string

--- src/tests/api-tests.ts
+++ src/tests/api-tests.ts
@@ -57,7 +57,7 @@ describe('api tests', () => {
       functionArgs: [],
       fee: new BN(200),
       senderKey: 'b8d99fd45da58038d630d9855d3ca2466e8e0f89d3894c4724f0efc9ff4b51f001',
-      postConditions: [pc1, pc2, pc3]
+      postConditions: [pc1, pc2, pc3],
     });
 
     const serialized = txBuilder.serialize();
@@ -85,7 +85,7 @@ describe('api tests', () => {
       throw Error('not found');
     }
     const expectedResp = JSON.parse(
-      '{"block_hash":"ff","block_height":123,"burn_block_time":345,"canonical":true,"tx_id":"bd5f225be4f1afb9a57f83cdc4c41cac761a8de0a87bc830323b049c6f3c5797","tx_index":2,"tx_status":"success","tx_type":"contract_call","fee_rate":"200","sender_address":"ST11NJTTKGVT6D1HY4NJRVQWMQM7TVAR091EJ8P2Y","sponsored":false,"post_condition_mode":"deny","post_conditions":[{"type":"non_fungible","condition_code":"not_sent","principal":{"type_id":"principal_standard","address":"ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR"},"asset_value":"0x020000000b61737365742d76616c7565","asset":{"contract_name":"hello","asset_name":"asset-name","contract_address":"STRYYQQ9M8KAF4NS7WNZQYY59X93XEKR31JP64CP"}},{"type":"fungible","condition_code":"sent_greater_than","amount":"123456","principal":{"type_id":"principal_standard","address":"ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR"},"asset":{"contract_name":"hello-ft","asset_name":"asset-name-ft","contract_address":"STRYYQQ9M8KAF4NS7WNZQYY59X93XEKR31JP64CP"}},{"type":"stx","condition_code":"sent_less_than","amount":"36723458","principal":{"type_id":"principal_standard","address":"ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR"}}],"contract_call":{"contract_id":"ST11NJTTKGVT6D1HY4NJRVQWMQM7TVAR091EJ8P2Y.hello-world","function_name":"fn-name"},"events":[]}'
+      '{"block_hash":"ff","block_height":123,"burn_block_time":345,"canonical":true,"tx_id":"bd5f225be4f1afb9a57f83cdc4c41cac761a8de0a87bc830323b049c6f3c5797","tx_index":2,"tx_status":"success","tx_type":"contract_call","fee_rate":"200","sender_address":"ST11NJTTKGVT6D1HY4NJRVQWMQM7TVAR091EJ8P2Y","sponsored":false,"post_condition_mode":"deny","post_conditions":[{"type":"non_fungible","condition_code":"not_sent","principal":{"type_id":"principal_standard","address":"ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR"},"asset_value":{"hex":"0x020000000b61737365742d76616c7565","repr":"\\"asset-value\\""},"asset":{"contract_name":"hello","asset_name":"asset-name","contract_address":"STRYYQQ9M8KAF4NS7WNZQYY59X93XEKR31JP64CP"}},{"type":"fungible","condition_code":"sent_greater_than","amount":"123456","principal":{"type_id":"principal_standard","address":"ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR"},"asset":{"contract_name":"hello-ft","asset_name":"asset-name-ft","contract_address":"STRYYQQ9M8KAF4NS7WNZQYY59X93XEKR31JP64CP"}},{"type":"stx","condition_code":"sent_less_than","amount":"36723458","principal":{"type_id":"principal_standard","address":"ST1HB1T8WRNBYB0Y3T7WXZS38NKKPTBR3EG9EPJKR"}}],"contract_call":{"contract_id":"ST11NJTTKGVT6D1HY4NJRVQWMQM7TVAR091EJ8P2Y.hello-world","function_name":"fn-name"},"events":[]}'
     );
     expect(txQuery.result).toEqual(expectedResp);
   });
