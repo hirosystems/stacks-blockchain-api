a753c9614a9d62ad3cb6579ec36b44a4443208ad
Nouman Ahmad <nouman.ahmad@tintash.com>
2020-08-21 21:46:53 +0500

    feat: fill out rosetta network list and options calls
    The Rosetta /network/list and /network/options endpoints
    now return useful information.
    
    Left to do:
    - add schema validation
    - add type information

--- src/api/routes/rosetta/network.ts
+++ src/api/routes/rosetta/network.ts
@@ -1,21 +1,50 @@
 import * as express from 'express';
 import { addAsync, RouterWithAsync } from '@awaitjs/express';
 import { DataStore } from '../../../datastore/common';
+import {
+  RosettaConstants,
+  RosettaOperationTypes,
+  RosettaOperationStatuses,
+  RosettaErrors,
+} from '../../rosetta-constants';
 
 export function createRNetworkRouter(db: DataStore): RouterWithAsync {
   const router = addAsync(express.Router());
 
-  router.postAsync('/list', async (req, res) => {
-    res.json({status: 'ready'});
+  router.postAsync('/list', async (_req, res) => {
+    const response = {
+      network_identifiers: [
+        {
+          blockchain: RosettaConstants.blockchain,
+          network: RosettaConstants.network,
+        },
+      ],
+    };
+
+    res.json(response);
   });
 
   router.postAsync('/status', async (req, res) => {
     res.json({status: 'ready'});
   });
 
-  router.postAsync('/options', async (req, res) => {
-    res.json({status: 'ready'});
+  router.postAsync('/options', async (_req, res) => {
+    const response = {
+      version: {
+        rosetta_version: RosettaConstants.rosettaVersion,
+        node_version: process.version,
+        middleware_version: process.env.npm_package_version,
+      },
+      allow: {
+        operation_statuses: RosettaOperationStatuses,
+        operation_types: RosettaOperationTypes,
+        errors: Object.values(RosettaErrors),
+        historical_balance_lookup: true,
+      },
+    };
+
+    res.json(response);
   });
 
   return router;
-};
+}
