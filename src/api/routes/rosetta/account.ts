53812fe519717824151c6531375a94c2baec1d85
Faried Nawaz <faried@tintash.com>
2020-08-23 02:27:24 +0500

    style: cleanup for lint
    In several cases, the request handlers are marked async.  Since the
    code inside them is not implemented in this PR (waiting on the types PR),
    the linter cannot find an "await" keyword inside the request handlers, and
    errors out.  This temporarily fixes it so lint is happy.

--- src/api/routes/rosetta/account.ts
+++ src/api/routes/rosetta/account.ts
@@ -6,9 +6,9 @@ export function createRAccountRouter(db: DataStore): RouterWithAsync {
   const router = addAsync(express.Router());
   router.use(express.json());
 
-  router.postAsync('/balance', async (req, res) => {
-    res.json({status: 'ready'});
+  router.post('/balance', (req, res) => {
+    res.json({ status: 'ready' });
   });
 
   return router;
-};
+}
