fa7db9c3f48d349091c3c95171aaa63bd9c9698d
Faried Nawaz <faried@tintash.com>
2020-08-28 23:57:46 +0500

    chore: expand types to include mempool transactions
    validHexId might receive a partial transaction block for /block
    which might not include a "hash" field; handle that case.

--- src/api/rosetta-constants.ts
+++ src/api/rosetta-constants.ts
@@ -142,6 +142,8 @@ export type RosettaRequestType =
   | T.RosettaAccountBalanceRequest
   | T.RosettaBlockRequest
   | T.RosettaBlockTransactionRequest
+  | T.RosettaMempoolTransactionListRequest
+  | T.RosettaMempoolTransactionRequest
   | T.RosettaNetworkListRequest
   | T.RosettaOptionsRequest
   | T.RosettaStatusRequest;
