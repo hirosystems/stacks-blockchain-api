feda1a396baa106f37d412a9d6007d589ae73173
Faried Nawaz <faried@tintash.com>
2020-08-28 06:29:09 +0500

    chore: add validation for rosetta block api
    Ensure that the hash figure for block_indentifier or
    transaction_identifier (if provided) is valid.
    
    Expand RosettaRequestType to cover all request types.

--- src/api/rosetta-constants.ts
+++ src/api/rosetta-constants.ts
@@ -47,47 +47,47 @@ export interface RosettaError {
 export const RosettaErrors: Record<string, RosettaError> = {
   invalidAccount: {
     code: 601,
-    message: 'Invalid Account',
+    message: 'Invalid Account.',
     retriable: true,
   },
   insufficientFunds: {
     code: 602,
-    message: 'Insufficient Funds',
+    message: 'Insufficient Funds.',
     retriable: true,
   },
   accountEmpty: {
     code: 603,
-    message: 'Account is empty',
+    message: 'Account is empty.',
     retriable: true,
   },
   invalidBlockIndex: {
     code: 604,
-    message: 'Invalid block index',
+    message: 'Invalid block index.',
     retriable: true,
   },
   blockNotFound: {
     code: 605,
-    message: 'Block not found',
+    message: 'Block not found.',
     retriable: true,
   },
   invalidBlockHash: {
     code: 606,
-    message: 'Invalid block hash',
+    message: 'Invalid block hash.',
     retriable: true,
   },
   transactionNotFound: {
     code: 607,
-    message: 'Transaction not found',
+    message: 'Transaction not found.',
     retriable: true,
   },
   invalidTransactionHash: {
     code: 608,
-    message: 'Invalid transaction hash',
+    message: 'Invalid transaction hash.',
     retriable: true,
   },
   invalidParams: {
     code: 609,
-    message: 'invalid params',
+    message: 'Invalid params.',
     retriable: true,
   },
   invalidNetwork: {
@@ -107,35 +107,44 @@ export const RosettaErrors: Record<string, RosettaError> = {
   },
   emptyNetworkIdentifier: {
     code: 613,
-    message: 'Network identifier object is null',
+    message: 'Network identifier object is null.',
     retriable: true,
   },
   emptyAccountIdentifier: {
     code: 614,
-    message: 'Account identifier object is null',
+    message: 'Account identifier object is null.',
     retriable: true,
   },
   invalidBlockIdentifier: {
     code: 615,
-    message: 'Block identifier is null',
+    message: 'Block identifier is null.',
+    retriable: true,
+  },
+  invalidTransactionIdentifier: {
+    code: 616,
+    message: 'Transaction identifier is null.',
     retriable: true,
   },
   emptyBlockchain: {
-    code: 616,
-    message: 'Blockchain name is null',
+    code: 617,
+    message: 'Blockchain name is null.',
     retriable: true,
   },
   emptyNetwork: {
-    code: 617,
-    message: 'Network name is null',
+    code: 618,
+    message: 'Network name is null.',
     retriable: true,
   },
 };
 
-// All request types, used to validate input (eventually).
-// This does not contain RosettaNetworkListRequest because
-// it's the only request without a NetworkIdentifier.
-export type RosettaRequestType = T.RosettaOptionsRequest | T.RosettaStatusRequest;
+// All request types, used to validate input.
+export type RosettaRequestType =
+  | T.RosettaAccountBalanceRequest
+  | T.RosettaBlockRequest
+  | T.RosettaBlockTransactionRequest
+  | T.RosettaNetworkListRequest
+  | T.RosettaOptionsRequest
+  | T.RosettaStatusRequest;
 
 export interface SchemaFiles {
   request: string;
@@ -160,5 +169,16 @@ export const RosettaSchemas: Record<string, SchemaFiles> = {
       '@blockstack/stacks-blockchain-api-types/api/rosetta/rosetta-network-status-request.schema.json',
     response:
       '@blockstack/stacks-blockchain-api-types/api/rosetta/rosetta-network-status-response.schema.json',
+  },
+  '/rosetta/v1/block': {
+    request:
+      '@blockstack/stacks-blockchain-api-types/api/rosetta/rosetta-block-request.schema.json',
+    response:
+      '@blockstack/stacks-blockchain-api-types/api/rosetta/rosetta-block-response.schema.json',
+  },
+  '/rosetta/v1/block/transaction': {
+    request:
+      '@blockstack/stacks-blockchain-api-types/api/rosetta/rosetta-block-transaction-request.schema.json',
+    response: '@blockstack/stacks-blockchain-api-types/api/rosetta/rosetta-transaction.schema.json',
   },
 };
