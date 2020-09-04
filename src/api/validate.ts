835d8e2d04cc44ec75b355d8087485afe383e9d0
Faried Nawaz <faried@tintash.com>
2020-09-05 01:20:14 +0500

    chore: add and use getOrAddAsync(), similar to getOrAdd()
    Change made by @zone117x in https://github.com/fariedt/stacks-blockchain-api/pull/2

--- src/api/validate.ts
+++ src/api/validate.ts
@@ -1,11 +1,18 @@
 import * as Ajv from 'ajv';
 import * as RefParser from '@apidevtools/json-schema-ref-parser';
-import { logger } from '../helpers';
+import { logger, getOrAdd, getOrAddAsync } from '../helpers';
+
+const derefSchemaCache: Map<string, RefParser.JSONSchema> = new Map();
+export async function dereferenceSchema(schemaFilePath: string): Promise<RefParser.JSONSchema> {
+  return getOrAddAsync(derefSchemaCache, schemaFilePath, () =>
+    RefParser.dereference(schemaFilePath)
+  );
+}
 
 export async function validate(schemaFilePath: string, data: any) {
   if (process.env.NODE_ENV !== 'development') return;
 
-  const schemaDef = await RefParser.dereference(schemaFilePath);
+  const schemaDef = await dereferenceSchema(schemaFilePath);
   const ajv = new Ajv({ schemaId: 'auto' });
   const valid = await ajv.validate(schemaDef, data);
   if (!valid) {
