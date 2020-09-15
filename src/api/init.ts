190b17b5006b81f5679f8f1c5a8ec1af7781acc2
CharlieC3 <charlie@blockstack.com>
2020-09-15 16:33:19 -0400

    style: fix lint errors

--- src/api/init.ts
+++ src/api/init.ts
@@ -24,6 +24,8 @@ import { createRosettaAccountRouter } from './routes/rosetta/account';
 import { createRosettaConstructionRouter } from './routes/rosetta/construction';
 import { logger } from '../helpers';
 import { createWsRpcRouter } from './routes/ws-rpc';
+import * as prometheusMiddleware from '@promster/express';
+import * as prometheusServer from '@promster/server';
 
 export interface ApiServer {
   expressApp: ExpressWithAsync;
@@ -41,8 +43,8 @@ export async function startApiServer(datastore: DataStore): Promise<ApiServer> {
   const apiPort = parseInt(process.env['STACKS_BLOCKCHAIN_API_PORT'] ?? '');
   const dev = process.env.NODE_ENV !== 'production';
 
-  const { createMiddleware: createPrometheusMiddleware } = require('@promster/express');
-  const { createServer: createPrometheusServer } = require('@promster/server');
+  const { createMiddleware: createPrometheusMiddleware } = prometheusMiddleware;
+  const { createServer: createPrometheusServer } = prometheusServer;
 
   if (!apiHost) {
     throw new Error(
@@ -69,7 +71,13 @@ export async function startApiServer(datastore: DataStore): Promise<ApiServer> {
   );
 
   if (!dev) {
-    createPrometheusServer({ port: 9153 }).then(() => console.log(`@promster/server started on port 9153.`));
+    await new Promise((resolve, reject) =>
+      resolve(
+        createPrometheusServer({ port: 9153 }).then(() =>
+          console.log(`@promster/server started on port 9153.`)
+        )
+      )
+    );
   }
 
   app.get('/', (req, res) => {
