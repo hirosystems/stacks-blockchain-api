8ecd617f292d1a1e56a415d15a7c280e8b82a153
Faizan Dastgir <faizan.dastgir@tintash.com>
2020-09-16 12:50:07 +0500

    feat: test validation with rosetta-cli

--- jest.config.rosetta-cli.js
+++ jest.config.rosetta-cli.js
@@ -2,16 +2,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: 'src',
-  testMatch: ['<rootDir>/tests-rosetta-cli/validate-rosetta-construction.ts'],
-  testPathIgnorePatterns: [
-    '<rootDir>/tests-rosetta-cli/setup.ts',
-    '<rootDir>/tests-rosetta-cli/teardown.ts',
-  ],
+  testMatch: ['<rootDir>/tests-rosetta-cli/**/*.ts'],
+  testPathIgnorePatterns: ['<rootDir>/tests-rosetta-cli/setup.ts', '<rootDir>/tests-rosetta-cli/teardown.ts'],
   collectCoverageFrom: ['<rootDir>/**/*.ts'],
   coveragePathIgnorePatterns: ['<rootDir>/tests'],
   coverageDirectory: '../coverage',
   globalSetup: '<rootDir>/tests-rosetta-cli/setup.ts',
   globalTeardown: '<rootDir>/tests-rosetta-cli/teardown.ts',
   testTimeout: 180000,
-  transformIgnorePatterns: ['node_modules/(?!(@blockstack/stacks-transactions)/)'],
-};
+  transformIgnorePatterns: [
+    "node_modules/(?!(@blockstack/stacks-transactions)/)"
+  ]
+}
