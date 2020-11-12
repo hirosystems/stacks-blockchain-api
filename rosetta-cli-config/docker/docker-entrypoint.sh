b28a5e1046073cfe549468b3103672a683a9c07f
Asim Mehmood <asim@tintash.com>
2020-11-12 18:28:59 +0500

    fix: fixed rebase with master issues

--- rosetta-cli-config/docker/docker-entrypoint.sh
+++ rosetta-cli-config/docker/docker-entrypoint.sh
@@ -3,10 +3,10 @@
 # See https://github.com/bufferings/docker-access-host
 
 HOST_DOMAIN="host.docker.internal"
-ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
+ping -q -c1 $HOST_DOMAIN >/dev/null 2>&1
 if [ $? -ne 0 ]; then
   HOST_IP=$(ip route | awk 'NR==1 {print $3}')
-  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
+  echo -e "$HOST_IP\t$HOST_DOMAIN" >>/etc/hosts
 fi
 
 cat /etc/hosts
