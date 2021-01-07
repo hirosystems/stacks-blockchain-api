2acc7196c8e24e36efd2e399339bbb9f482c0ab9
Faried Nawaz <faried@tintash.com>
2021-01-08 04:15:06 +0500

    chore: add pg migration for subdomains table

---
+++ src/migrations/1610030345948_bns-subdomain.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable('subdomains', {
+    id: {
+      type: 'serial',
+      primaryKey: true,
+    },
+    name: {
+      type: 'string',
+      notNull: true,
+    },
+    namespace_id: {
+      type: 'string',
+      notNull: true
+    },
+    fully_qualified_subdomain: {
+      type: 'string',
+      notNull: true
+    },
+    owner: {
+      type: 'string',
+      notNull: true,
+    },
+    zonefile: {
+      type: 'string',
+      notNull: true,
+    },
+    zonefile_hash: {
+      type: 'string',
+      notNull: true,
+    },
+    parent_zonefile_hash: {
+      type: 'string',
+      notNull: true,
+    },
+    parent_zonefile_index: {
+      type: 'integer',
+      notNull: true,
+    },
+    block_height: {
+      type: 'integer',
+      notNull: true,
+    },
+    zonefile_offset: {
+      type: 'integer',
+      notNull: false,
+    },
+    resolver: {
+      type: 'string',
+      notNull: false,
+    },
+    latest: {
+      type: 'boolean',
+      notNull: true,
+      default: true
+    },
+    tx_id: {
+      type: 'string',
+      notNull: false,
+    },
+    canonical: {
+      type: 'boolean',
+      notNull: true,
+      default: true
+    },
+    index_block_hash: {
+      type: 'bytea',
+      notNull: false
+    },
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable('subdomains');
+}
