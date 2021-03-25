4092f4a0a5595783505ff61ba5ac773225e5cade
Matthew Little <zone117x@gmail.com>
2021-03-25 13:13:32 +0100

    chore: do not yet enable strict unhandledPromiseRejects

--- src/shutdown-handler.ts
+++ src/shutdown-handler.ts
@@ -1,12 +1,6 @@
-import { logger } from './helpers';
+import { logError, logger } from './helpers';
 
-const SHUTDOWN_SIGNALS = [
-  'beforeExit',
-  'uncaughtException',
-  'unhandledRejection',
-  'SIGINT',
-  'SIGTERM',
-];
+const SHUTDOWN_SIGNALS = ['SIGINT', 'SIGTERM'] as const;
 
 export type ShutdownHandler = () => void | PromiseLike<void>;
 
@@ -19,13 +13,14 @@ function registerShutdownSignals() {
   }
   shutdownSignalsRegistered = true;
   let runHandlers: undefined | (() => Promise<never>) = async () => {
+    runHandlers = undefined;
     let errorEncountered = false;
     for (const handler of shutdownHandlers) {
       try {
         await Promise.resolve(handler());
       } catch (error) {
         errorEncountered = true;
-        logger.error('Error running shutdown handler', error);
+        logError('Error running shutdown handler', error);
       }
     }
     if (errorEncountered) {
@@ -36,11 +31,24 @@ function registerShutdownSignals() {
   };
 
   SHUTDOWN_SIGNALS.forEach(sig => {
-    process.once(sig as any, () => {
+    process.once(sig, () => {
       logger.warn(`Shutting down... received signal: ${sig}`);
       void runHandlers?.();
-      runHandlers = undefined;
     });
+  });
+  process.once('unhandledRejection', error => {
+    // TODO: This should be enabled in a standalone update, as it may cause previously ignored non-critical errors to exit the program.
+    // In the meantime, log the error without propagated it to uncaughtException.
+    // throw error;
+    logError(`unhandledRejection ${(error as any)?.message ?? error}`, error as Error);
+  });
+  process.once('uncaughtException', () => {
+    logger.warn(`Shutting down... received uncaughtException`);
+    void runHandlers?.();
+  });
+  process.once('beforeExit', () => {
+    logger.warn(`Shutting down... received beforeExit`);
+    void runHandlers?.();
   });
 }
 
