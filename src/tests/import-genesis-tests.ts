25d73d4cd433af1c3b080d5395a743fd1e919482
Matthew Little <zone117x@gmail.com>
2021-04-21 12:42:51 +0200

    chore: additional token offering import tests

--- src/tests/import-genesis-tests.ts
+++ src/tests/import-genesis-tests.ts
@@ -16,6 +16,22 @@ describe('import genesis data tests', () => {
     await importV1TokenOfferingData(db);
     const newDbConfigState = await db.getConfigState();
     expect(newDbConfigState.token_offering_imported).toBe(true);
+
+    const addr1 = 'SP04MFJ3RWTADV6ZWTWD68DBZ14EJSDXT50Q7TE6';
+    const res1 = await db.getTokenOfferingLocked(addr1);
+    expect(res1?.result?.total_locked).toEqual('33115155552');
+
+    const addr2 = 'SM2M7XTPCJK6S5XG7JKH4SGYDY9W49ZQ962MC4XPM';
+    const res2 = await db.getTokenOfferingLocked(addr2);
+    expect(res2?.result?.total_locked).toEqual('111111111108');
+
+    const addr3 = 'SM37EFPD9ZVR3YRJE7673MJ3W0T350JM1HVZVCDC3';
+    const res3 = await db.getTokenOfferingLocked(addr3);
+    expect(res3?.result?.total_locked).toEqual('111111109');
+
+    const addr4 = 'SM260QHD6ZM2KKPBKZB8PFE5XWP0MHSKTD1B7BHYR';
+    const res4 = await db.getTokenOfferingLocked(addr4);
+    expect(res4?.result?.total_locked).toEqual('1666666664');
   });
 
   afterEach(async () => {
