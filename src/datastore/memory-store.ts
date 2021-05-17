98912d0723f6f6608f85c3dfea0adb5158c9f08b
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2021-05-17 20:35:44 -0300

    fix: merge issues

--- src/datastore/memory-store.ts
+++ src/datastore/memory-store.ts
@@ -702,11 +702,11 @@ export class MemoryDataStore
     throw new Error('Method not implemented');
   }
 
-  close() {
-    return Promise.resolve();
-  }
-
   getUnlockedAddressesAtBlock(burnBlockHeight: number): Promise<StxUnlockEvent[]> {
     throw new Error('Method not implemented');
+  }
+
+  close() {
+    return Promise.resolve();
   }
 }
