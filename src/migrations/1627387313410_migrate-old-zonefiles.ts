9b3f765fdffaef853cf743e58611c7257e4100ca
MHassanTariq <mhtariq29@gmail.com>
2021-07-27 19:37:45 +0500

    fix: changed the query to built in pgm functions

--- src/migrations/1627387313410_migrate-old-zonefiles.ts
+++ src/migrations/1627387313410_migrate-old-zonefiles.ts
@@ -12,11 +12,14 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
         SELECT zonefile, zonefile_hash
         FROM subdomains
     `);
-    await pgm.db.query('ALTER TABLE subdomains DROP zonefile');
-    await pgm.db.query('ALTER TABLE names DROP zonefile');
+
+    // dropping these tables as we won't be needing them, now
+    pgm.dropColumn('subdomains', 'zonefile');
+    pgm.dropColumn('names', 'zonefile');
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
+    // use pgm function to add column
     await pgm.db.query('ALTER TABLE subdomains ADD zonefile varchar');
     await pgm.db.query('ALTER TABLE names ADD zonefile varchar');
     await pgm.db.query(`
