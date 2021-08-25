4982bd8fbe81f2797f22b679301ae0beebfc2eed
Rafael Cárdenas <rafael@rafaelcr.com>
2021-08-25 10:45:47 -0500

    feat: add build script to write git info to usable ts file

---
+++ src/api/routes/status.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+import { addAsync, RouterWithAsync } from '@awaitjs/express';
+import { DataStore } from 'src/datastore/common';
+import GitInfo from 'src/git-info';
+
+export function createStatusRouter(db: DataStore): RouterWithAsync {
+  const router = addAsync(express.Router());
+
+  router.get('/', (req, res) =>
+    res.status(200).json({
+      server_version: `stacks-blockchain-api ${GitInfo.tag} (${GitInfo.branch}:${GitInfo.commit})`,
+      status: 'ready',
+    })
+  );
+
+  return router;
+}
