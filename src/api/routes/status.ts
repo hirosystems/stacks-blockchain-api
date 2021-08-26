dcdba23e0071600156097aa531ed30dcee26827a
Rafael Cárdenas <rafael@rafaelcr.com>
2021-08-26 09:42:41 -0500

    chore: create status endpoint openapi schema and example

--- src/api/routes/status.ts
+++ src/api/routes/status.ts
@@ -2,16 +2,18 @@ import * as express from 'express';
 import { addAsync, RouterWithAsync } from '@awaitjs/express';
 import { DataStore } from 'src/datastore/common';
 import GitInfo from '../../git-info';
+import { ServerStatusResponse } from 'docs/generated';
 
 export function createStatusRouter(db: DataStore): RouterWithAsync {
   const router = addAsync(express.Router());
 
-  router.get('/', (req, res) =>
-    res.status(200).json({
+  router.get('/', (req, res) => {
+    const response: ServerStatusResponse = {
       server_version: `stacks-blockchain-api ${GitInfo.tag} (${GitInfo.branch}:${GitInfo.commit})`,
       status: 'ready',
-    })
-  );
+    };
+    res.json(response);
+  });
 
   return router;
 }
