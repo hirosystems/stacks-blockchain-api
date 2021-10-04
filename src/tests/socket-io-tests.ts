9ff407e2ee159c04aed835cc2d0feb15e22609a4
Rafael Cárdenas <rafael@rafaelcr.com>
2021-10-04 11:53:30 -0500

    chore: finish socket-io test

--- src/tests/socket-io-tests.ts
+++ src/tests/socket-io-tests.ts
@@ -3,9 +3,9 @@ import { ChainID } from '@stacks/common';
 import { PoolClient } from 'pg';
 import { ApiServer, startApiServer } from '../api/init';
 import { cycleMigrations, runMigrations, PgDataStore } from '../datastore/postgres-store';
-import { DbBlock, DbMicroblockPartial } from 'src/datastore/common';
-import { waiter, Waiter } from 'src/helpers';
-import { Microblock } from 'docs/generated';
+import { DbBlock, DbMicroblockPartial, DbTx, DbTxTypeId } from '../datastore/common';
+import { I32_MAX, waiter, Waiter } from '../helpers';
+import { Microblock } from '../../docs/generated';
 
 describe('socket-io', () => {
   let apiServer: ApiServer;
@@ -25,9 +25,8 @@ describe('socket-io', () => {
   });
 
   test('socket-io > microblock updates', async () => {
-    const address = apiServer.address;
-    const socket = io(`http://${address}`, { query: { subscriptions: 'microblock' } });
-
+    const addr1 = 'ST28D4Q6RCQSJ6F7TEYWQDS4N1RXYEP9YBWMYSB97';
+    const addr2 = 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6';
     const block: DbBlock = {
       block_hash: '0x1234',
       index_block_hash: '0xdeadbeef',
@@ -47,6 +46,40 @@ describe('socket-io', () => {
       execution_cost_write_count: 0,
       execution_cost_write_length: 0,
     };
+    const tx1: DbTx = {
+      tx_id: '0x01',
+      tx_index: 0,
+      anchor_mode: 3,
+      nonce: 0,
+      raw_tx: Buffer.alloc(0),
+      index_block_hash: block.index_block_hash,
+      block_hash: block.block_hash,
+      block_height: block.block_height,
+      burn_block_time: block.burn_block_time,
+      parent_burn_block_time: 1626122935,
+      type_id: DbTxTypeId.Coinbase,
+      status: 1,
+      raw_result: '0x0100000000000000000000000000000001', // u1
+      canonical: true,
+      post_conditions: Buffer.from([0x01, 0xf5]),
+      fee_rate: 1234n,
+      sponsored: false,
+      sponsor_address: undefined,
+      sender_address: addr1,
+      origin_hash_mode: 1,
+      coinbase_payload: Buffer.from('hi'),
+      event_count: 1,
+      parent_index_block_hash: '',
+      parent_block_hash: '',
+      microblock_canonical: true,
+      microblock_sequence: I32_MAX,
+      microblock_hash: '',
+      execution_cost_read_count: 0,
+      execution_cost_read_length: 0,
+      execution_cost_runtime: 0,
+      execution_cost_write_count: 0,
+      execution_cost_write_length: 0,
+    };
     const mb1: DbMicroblockPartial = {
       microblock_hash: '0xff01',
       microblock_sequence: 0,
@@ -56,16 +89,94 @@ describe('socket-io', () => {
       parent_burn_block_hash: '0xaa',
       parent_burn_block_time: 1626122935,
     };
+    const mbTx1: DbTx = {
+      tx_id: '0x02',
+      tx_index: 0,
+      anchor_mode: 3,
+      nonce: 0,
+      raw_tx: Buffer.alloc(0),
+      type_id: DbTxTypeId.TokenTransfer,
+      status: 1,
+      raw_result: '0x0100000000000000000000000000000001', // u1
+      canonical: true,
+      post_conditions: Buffer.from([0x01, 0xf5]),
+      fee_rate: 1234n,
+      sponsored: false,
+      sender_address: addr1,
+      sponsor_address: undefined,
+      origin_hash_mode: 1,
+      token_transfer_amount: 50n,
+      token_transfer_memo: Buffer.from('hi'),
+      token_transfer_recipient_address: addr2,
+      event_count: 1,
+      parent_index_block_hash: block.index_block_hash,
+      parent_block_hash: block.block_hash,
+      microblock_canonical: true,
+      microblock_sequence: mb1.microblock_sequence,
+      microblock_hash: mb1.microblock_hash,
+      parent_burn_block_time: mb1.parent_burn_block_time,
+      execution_cost_read_count: 0,
+      execution_cost_read_length: 0,
+      execution_cost_runtime: 0,
+      execution_cost_write_count: 0,
+      execution_cost_write_length: 0,
+      index_block_hash: '',
+      block_hash: '',
+      burn_block_time: -1,
+      block_height: -1,
+    };
 
+    const address = apiServer.address;
+    const socket = io(`http://${address}`, { query: { subscriptions: 'microblock' } });
     const updateWaiter: Waiter<Microblock> = waiter();
+
     socket.on('microblock', microblock => {
       updateWaiter.finish(microblock);
     });
-    await db.updateMicroblocks({ microblocks: [mb1], txs: [] });
+    await db.update({
+      block: block,
+      microblocks: [],
+      minerRewards: [],
+      txs: [
+        {
+          tx: tx1,
+          stxLockEvents: [],
+          stxEvents: [],
+          ftEvents: [],
+          nftEvents: [],
+          contractLogEvents: [],
+          smartContracts: [],
+          names: [],
+          namespaces: [],
+        },
+      ],
+    });
+    await db.updateMicroblocks({
+      microblocks: [mb1],
+      txs: [
+        {
+          tx: mbTx1,
+          stxLockEvents: [],
+          stxEvents: [],
+          ftEvents: [],
+          nftEvents: [],
+          contractLogEvents: [],
+          smartContracts: [],
+          names: [],
+          namespaces: [],
+        },
+      ],
+    });
 
     const result = await updateWaiter;
-    expect(result.microblock_hash).toEqual('0xff01');
-    socket.emit('unsubscribe', 'microblock');
+    try {
+      expect(result.microblock_hash).toEqual('0xff01');
+      expect(result.microblock_parent_hash).toEqual(block.block_hash);
+      expect(result.txs[0]).toEqual(mbTx1.tx_id);
+    } finally {
+      socket.emit('unsubscribe', 'microblock');
+      socket.close();
+    }
   });
 
   afterEach(async () => {
