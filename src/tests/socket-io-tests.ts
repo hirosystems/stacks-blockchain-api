11e013d6aaea046a55c6fd4eff3c7c59bee4f2c6
Rafael Cárdenas <rafael@rafaelcr.com>
2021-10-07 14:30:05 -0500

    chore: add address tx test

--- src/tests/socket-io-tests.ts
+++ src/tests/socket-io-tests.ts
@@ -4,15 +4,23 @@ import { PoolClient } from 'pg';
 import { ApiServer, startApiServer } from '../api/init';
 import { cycleMigrations, runMigrations, PgDataStore } from '../datastore/postgres-store';
 import {
+  DbAssetEventTypeId,
   DbBlock,
+  DbEventTypeId,
   DbMempoolTx,
   DbMicroblockPartial,
+  DbStxEvent,
   DbTx,
   DbTxStatus,
   DbTxTypeId,
 } from '../datastore/common';
 import { I32_MAX, waiter, Waiter } from '../helpers';
-import { Block, Microblock, MempoolTransaction } from '../../docs/generated';
+import {
+  Block,
+  Microblock,
+  MempoolTransaction,
+  AddressTransactionWithTransfers,
+} from '../../docs/generated';
 
 describe('socket-io', () => {
   let apiServer: ApiServer;
@@ -359,6 +367,116 @@ describe('socket-io', () => {
       expect(result.tx_id).toEqual('0x01');
     } finally {
       socket.emit('unsubscribe', 'mempool');
+      socket.close();
+    }
+  });
+
+  test('socket-io > address tx updates', async () => {
+    const addr1 = 'ST28D4Q6RCQSJ6F7TEYWQDS4N1RXYEP9YBWMYSB97';
+    const addr2 = 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6';
+    const block: DbBlock = {
+      block_hash: '0x1234',
+      index_block_hash: '0xdeadbeef',
+      parent_index_block_hash: '0x00',
+      parent_block_hash: '0xff0011',
+      parent_microblock_hash: '',
+      block_height: 1,
+      burn_block_time: 94869286,
+      burn_block_hash: '0x1234',
+      burn_block_height: 123,
+      miner_txid: '0x4321',
+      canonical: true,
+      parent_microblock_sequence: 0,
+      execution_cost_read_count: 0,
+      execution_cost_read_length: 0,
+      execution_cost_runtime: 0,
+      execution_cost_write_count: 0,
+      execution_cost_write_length: 0,
+    };
+    const tx1: DbTx = {
+      tx_id: '0x8912',
+      tx_index: 1,
+      anchor_mode: 3,
+      nonce: 0,
+      raw_tx: Buffer.from('raw-tx-test'),
+      index_block_hash: block.block_hash,
+      block_hash: block.block_hash,
+      block_height: block.block_height,
+      burn_block_time: 2837565,
+      parent_burn_block_time: 1626122935,
+      type_id: DbTxTypeId.TokenTransfer,
+      status: DbTxStatus.Success,
+      raw_result: '0x0100000000000000000000000000000001', // u1
+      canonical: true,
+      post_conditions: Buffer.from([0x01, 0xf5]),
+      fee_rate: 50n,
+      sponsored: false,
+      sponsor_address: undefined,
+      sender_address: addr1,
+      origin_hash_mode: 1,
+      token_transfer_recipient_address: addr2,
+      token_transfer_amount: 100n,
+      token_transfer_memo: Buffer.from('memo'),
+      event_count: 1,
+      parent_index_block_hash: '',
+      parent_block_hash: '',
+      microblock_canonical: true,
+      microblock_sequence: I32_MAX,
+      microblock_hash: '',
+      execution_cost_read_count: 0,
+      execution_cost_read_length: 0,
+      execution_cost_runtime: 0,
+      execution_cost_write_count: 0,
+      execution_cost_write_length: 0,
+    };
+    const stxEvent: DbStxEvent = {
+      canonical: tx1.canonical,
+      event_type: DbEventTypeId.StxAsset,
+      asset_event_type_id: DbAssetEventTypeId.Transfer,
+      event_index: 0,
+      tx_id: tx1.tx_id,
+      tx_index: tx1.tx_index,
+      block_height: tx1.block_height,
+      amount: tx1.token_transfer_amount as bigint,
+      recipient: tx1.token_transfer_recipient_address,
+      sender: tx1.sender_address,
+    };
+
+    const address = apiServer.address;
+    const socket = io(`http://${address}`, {
+      query: { subscriptions: `address-transaction:${addr1}` },
+    });
+    const updateWaiter: Waiter<AddressTransactionWithTransfers> = waiter();
+
+    socket.on(`address-transaction:${addr1}`, (_, tx) => {
+      updateWaiter.finish(tx);
+    });
+    await db.update({
+      block: block,
+      microblocks: [],
+      minerRewards: [],
+      txs: [
+        {
+          tx: tx1,
+          stxLockEvents: [],
+          stxEvents: [stxEvent],
+          ftEvents: [],
+          nftEvents: [],
+          contractLogEvents: [],
+          smartContracts: [],
+          names: [],
+          namespaces: [],
+        },
+      ],
+    });
+
+    const result = await updateWaiter;
+    try {
+      expect(result.tx.tx_id).toEqual('0x8912');
+      expect(result.stx_sent).toEqual('150'); // Incl. fees
+      expect(result.stx_transfers[0].amount).toEqual('100');
+    } finally {
+      socket.emit('unsubscribe', `address-transaction:${addr1}`);
       socket.close();
     }
   });
