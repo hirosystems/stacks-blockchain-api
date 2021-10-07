7340dea55e5880f9019f462739b603fbeb5fdebe
Rafael Cárdenas <rafael@rafaelcr.com>
2021-10-07 09:40:19 -0500

    chore: add block test

--- src/tests/socket-io-tests.ts
+++ src/tests/socket-io-tests.ts
@@ -5,7 +5,7 @@ import { ApiServer, startApiServer } from '../api/init';
 import { cycleMigrations, runMigrations, PgDataStore } from '../datastore/postgres-store';
 import { DbBlock, DbMicroblockPartial, DbTx, DbTxTypeId } from '../datastore/common';
 import { I32_MAX, waiter, Waiter } from '../helpers';
-import { Microblock } from '../../docs/generated';
+import { Block, Microblock } from '../../docs/generated';
 
 describe('socket-io', () => {
   let apiServer: ApiServer;
@@ -22,6 +22,99 @@ describe('socket-io', () => {
       chainId: ChainID.Testnet,
       httpLogLevel: 'silly',
     });
+  });
+
+  test('socket-io > block updates', async () => {
+    const addr1 = 'ST28D4Q6RCQSJ6F7TEYWQDS4N1RXYEP9YBWMYSB97';
+    const block: DbBlock = {
+      block_hash: '0x1234',
+      index_block_hash: '0xdeadbeef',
+      parent_index_block_hash: '0x00',
+      parent_block_hash: '0xff0011',
+      parent_microblock_hash: '',
+      block_height: 1,
+      burn_block_time: 94869286,
+      burn_block_hash: '0x1234',
+      burn_block_height: 123,
+      miner_txid: '0x4321',
+      canonical: true,
+      parent_microblock_sequence: 0,
+      execution_cost_read_count: 0,
+      execution_cost_read_length: 0,
+      execution_cost_runtime: 0,
+      execution_cost_write_count: 0,
+      execution_cost_write_length: 0,
+    };
+    const tx1: DbTx = {
+      tx_id: '0x01',
+      tx_index: 0,
+      anchor_mode: 3,
+      nonce: 0,
+      raw_tx: Buffer.alloc(0),
+      index_block_hash: block.index_block_hash,
+      block_hash: block.block_hash,
+      block_height: block.block_height,
+      burn_block_time: block.burn_block_time,
+      parent_burn_block_time: 1626122935,
+      type_id: DbTxTypeId.Coinbase,
+      status: 1,
+      raw_result: '0x0100000000000000000000000000000001', // u1
+      canonical: true,
+      post_conditions: Buffer.from([0x01, 0xf5]),
+      fee_rate: 1234n,
+      sponsored: false,
+      sponsor_address: undefined,
+      sender_address: addr1,
+      origin_hash_mode: 1,
+      coinbase_payload: Buffer.from('hi'),
+      event_count: 1,
+      parent_index_block_hash: '',
+      parent_block_hash: '',
+      microblock_canonical: true,
+      microblock_sequence: I32_MAX,
+      microblock_hash: '',
+      execution_cost_read_count: 0,
+      execution_cost_read_length: 0,
+      execution_cost_runtime: 0,
+      execution_cost_write_count: 0,
+      execution_cost_write_length: 0,
+    };
+
+    const address = apiServer.address;
+    const socket = io(`http://${address}`, { query: { subscriptions: 'block' } });
+    const updateWaiter: Waiter<Block> = waiter();
+
+    socket.on('block', block => {
+      updateWaiter.finish(block);
+    });
+    await db.update({
+      block: block,
+      microblocks: [],
+      minerRewards: [],
+      txs: [
+        {
+          tx: tx1,
+          stxLockEvents: [],
+          stxEvents: [],
+          ftEvents: [],
+          nftEvents: [],
+          contractLogEvents: [],
+          smartContracts: [],
+          names: [],
+          namespaces: [],
+        },
+      ],
+    });
+
+    const result = await updateWaiter;
+    try {
+      expect(result.hash).toEqual('0x1234');
+      expect(result.burn_block_hash).toEqual('0x1234');
+      expect(result.txs[0]).toEqual(tx1.tx_id);
+    } finally {
+      socket.emit('unsubscribe', 'block');
+      socket.close();
+    }
   });
 
   test('socket-io > microblock updates', async () => {
