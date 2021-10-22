fed0a2af5d2da1c5de80a0be8b24f5d104105463
Rafael Cárdenas <rafael@rafaelcr.com>
2021-10-22 10:21:08 -0500

    fix: consider block height

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -10,28 +10,60 @@ type AddressBalanceResult = {
   nodeBalance?: bigint;
 };
 
-async function getTopAccounts() {
+type BlockInfo = {
+  block_height: number;
+  block_hash: Buffer;
+  index_block_hash: Buffer;
+};
+
+type TableCellValue = string | number | bigint | undefined;
+
+/**
+ * Prints the account balance as reported by the local DB and the Stacks node of the `count`
+ * accounts with the greatest number of STX transfer events.
+ * @param count Number of top accounts to query
+ * @param blockHeight Specific block height at which to query balances
+ */
+async function printTopAccountBalances(count: number, blockHeight: number) {
   const db = await PgDataStore.connect(true);
 
-  const blockHeight = await db.query(async client => {
-    return await db.getChainTip(client);
+  const blockInfo = await db.query(async client => {
+    console.log(blockHeight);
+    const result = await client.query<BlockInfo>(
+      `
+      SELECT block_height, block_hash, index_block_hash
+      FROM blocks
+      WHERE canonical = true AND block_height = (
+        CASE
+          WHEN $1=0 THEN (SELECT MAX(block_height) FROM blocks)
+          ELSE $1
+        END
+      )
+      `,
+      [blockHeight]
+    );
+    return result.rows[0];
   });
+  // First, get the top addresses.
   const addressBalances = await db.query(async client => {
-    const result = await client.query<AddressBalanceResult>(`
+    const result = await client.query<AddressBalanceResult>(
+      `
       WITH addresses AS ((
         SELECT
           sender AS address
         FROM
           stx_events
         WHERE
-          sender IS NOT NULL)
+          sender IS NOT NULL
+          AND block_height <= $1)
       UNION ALL (
         SELECT
           recipient AS address
         FROM
           stx_events
         WHERE
-          recipient IS NOT NULL)
+          recipient IS NOT NULL
+          AND block_height <= $1)
       )
       SELECT
         COUNT(*) AS count, address
@@ -41,18 +73,21 @@ async function getTopAccounts() {
         address
       ORDER BY
         count DESC
-      LIMIT 10;
-    `);
+      LIMIT $2;
+      `,
+      [blockInfo.block_height, count]
+    );
     return result.rows;
   });
+  // Next, fill them up with balances from DB and node.
   await Promise.all(
     addressBalances.map(async item => {
-      const balance = await db.getStxBalanceAtBlock(item.address, blockHeight.blockHeight);
+      const balance = await db.getStxBalanceAtBlock(item.address, blockInfo.block_height);
       item.apiBalance = balance.balance;
     })
   );
 
-  const tabularData: (string | number | bigint | undefined)[][] = [
+  const tabularData: TableCellValue[][] = [
     ['event count', 'address', 'api balance', 'node balance'],
   ];
   addressBalances.forEach(item => {
@@ -64,9 +99,7 @@ async function getTopAccounts() {
 }
 
 async function handleProgramArgs() {
-  const parsedOpts = getopts(process.argv.slice(2), {
-    boolean: [],
-  });
+  const parsedOpts = getopts(process.argv.slice(2));
   const args = {
     operand: parsedOpts._[0],
     options: parsedOpts,
@@ -74,13 +107,17 @@ async function handleProgramArgs() {
     operand: 'stx-balances';
     options: {
       ['count']?: number;
+      ['block-height']?: number;
     };
   };
 
   if (args.operand === 'stx-balances') {
-    await getTopAccounts();
+    await printTopAccountBalances(args.options.count ?? 10, args.options['block-height'] ?? 0);
   } else if (parsedOpts._[0]) {
     throw new Error(`Unexpected program argument: ${parsedOpts._[0]}`);
+  } else {
+    // TODO: Display usage.
+    throw new Error(`Program arguments required`);
   }
 }
 
