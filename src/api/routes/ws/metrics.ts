aa7b81b59a590d352277fda3ada06520a1cf68ce
Rafael Cárdenas <rafael@rafaelcr.com>
2021-11-24 10:53:28 -0600

    feat: add histogram for subscription duration

--- src/api/routes/ws/metrics.ts
+++ src/api/routes/ws/metrics.ts
@@ -1,11 +1,17 @@
 import * as prom from 'prom-client';
+import * as WebSocket from 'ws';
 import { Topic } from '@stacks/stacks-blockchain-api-types';
+import { Socket } from 'socket.io';
 
 export type WebSocketMetricsPrefix = 'socket_io' | 'websocket';
+
+export type WebSocketSubscriber = WebSocket | Socket;
 
 interface WebSocketMetrics {
   // Current number of active subscriptions (labeled by topic).
   subscriptions: prom.Gauge<string>;
+  // Time spent subscribed to a particular topic.
+  subscriptionTimers: prom.Histogram<string>;
   // Total connections.
   connectTotal: prom.Counter<string>;
   // Total disconnections.
@@ -19,12 +25,20 @@ interface WebSocketMetrics {
  */
 export class WebSocketPrometheus {
   private metrics: WebSocketMetrics;
+  // Record of all dates when a particular socket started observing an event. Useful for measuring
+  // total subscription time.
+  private subscriptions = new Map<WebSocketSubscriber, Map<string, Date>>();
 
   constructor(metricsNamePrefix: WebSocketMetricsPrefix) {
     this.metrics = {
       subscriptions: new prom.Gauge({
         name: `${metricsNamePrefix}_subscriptions`,
         help: 'Current subscriptions',
+        labelNames: ['topic'],
+      }),
+      subscriptionTimers: new prom.Histogram({
+        name: `${metricsNamePrefix}_subscription_timers`,
+        help: 'Subscription timers',
         labelNames: ['topic'],
       }),
       connectTotal: new prom.Counter({
@@ -47,23 +61,67 @@ export class WebSocketPrometheus {
     this.metrics.connectTotal.inc();
   }
 
-  public disconnect() {
-    this.metrics.disconnectTotal.inc();
+  public disconnect(subscriber: WebSocketSubscriber) {
+    this.doDisconnect(subscriber);
   }
 
-  public subscribe(topic: Topic | Topic[] | string) {
+  public subscribe(subscriber: WebSocketSubscriber, topic: Topic | Topic[] | string) {
     if (Array.isArray(topic)) {
-      topic.forEach(t => this.metrics.subscriptions.inc({ topic: t.toString() }));
+      topic.forEach(t => this.doSubscribe(subscriber, t));
     } else {
-      this.metrics.subscriptions.inc({ topic: topic.toString() });
+      this.doSubscribe(subscriber, topic);
     }
   }
 
-  public unsubscribe(topic: Topic | string) {
-    this.metrics.subscriptions.dec({ topic: topic.toString() });
+  public unsubscribe(subscriber: WebSocketSubscriber, topic: Topic | string) {
+    this.doUnsubscribe(subscriber, topic);
   }
 
   public sendEvent(event: string) {
     this.metrics.eventsSent.inc({ event: event });
+  }
+
+  private doSubscribe(subscriber: WebSocketSubscriber, topic: Topic | Topic[] | string) {
+    const topicStr = topic.toString();
+    // Increase subscription count.
+    this.metrics.subscriptions.inc({ topic: topicStr });
+    // Record the subscription date.
+    let map = this.subscriptions.get(subscriber);
+    if (!map) {
+      map = new Map();
+      this.subscriptions.set(subscriber, map);
+    }
+    map.set(topicStr, new Date());
+  }
+
+  private doUnsubscribe(subscriber: WebSocketSubscriber, topic: Topic | string) {
+    const topicStr = topic.toString();
+    // Decrease subscription count.
+    this.metrics.subscriptions.dec({ topic: topicStr });
+    // Report total subscription duration.
+    const map = this.subscriptions.get(subscriber);
+    if (map) {
+      const startDate = map.get(topicStr);
+      if (startDate) {
+        const elapsedSeconds = (new Date().getTime() - startDate.getTime()) / 1000;
+        this.metrics.subscriptionTimers.observe({ topic: topicStr }, elapsedSeconds);
+        map.delete(topicStr);
+        if (map.size === 0) {
+          this.subscriptions.delete(subscriber);
+        }
+      }
+    }
+  }
+
+  private doDisconnect(subscriber: WebSocketSubscriber) {
+    this.metrics.disconnectTotal.inc();
+    // Also unsubscribe from every topic.
+    const map = this.subscriptions.get(subscriber);
+    if (map) {
+      const topics = Array.from(map.keys());
+      topics.forEach(topic => {
+        this.doUnsubscribe(subscriber, topic);
+      });
+    }
   }
 }
