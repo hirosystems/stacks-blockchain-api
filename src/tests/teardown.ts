03f372b8ba7afe6597ea02e427c480ee14ef9f26
Matthew Little <zone117x@gmail.com>
2021-12-09 21:13:03 +0100

    chore: hack to print stuck handles while running tests #3

--- src/tests/teardown.ts
+++ src/tests/teardown.ts
@@ -1,6 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import * as whyIsNodeRunning from 'why-is-node-running';
+import * as isCI from 'is-ci';
 
 // ts-unused-exports:disable-next-line
 export default async (): Promise<void> => {
@@ -13,9 +11,14 @@ export default async (): Promise<void> => {
     });
   });
 
-  let whyRunInterval = 1000;
-  setInterval(() => {
-    console.log('\n\n\n\n_____WHY IS NODE RUNNING_____');
-    whyIsNodeRunning();
-  }, (whyRunInterval *= 2)).unref();
+  // If running in CI setup the "why am I still running?" log to detect stuck Jest tests
+  if (isCI) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const whyIsNodeRunning = require('why-is-node-running');
+    let whyRunInterval = 1000;
+    setInterval(() => {
+      console.log('\n\n\n\n_____WHY IS NODE RUNNING_____');
+      whyIsNodeRunning();
+    }, (whyRunInterval *= 2)).unref();
+  }
 };
