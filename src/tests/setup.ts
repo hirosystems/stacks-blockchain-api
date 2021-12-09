5112c1f154f6df201e571c81d2a0ac0930f97bc7
Matthew Little <zone117x@gmail.com>
2021-12-09 20:41:54 +0100

    chore: hack to print stuck handles while running tests

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -5,6 +5,10 @@ import { startEventServer } from '../event-stream/event-server';
 import { StacksCoreRpcClient } from '../core-rpc/client';
 import { ChainID } from '@stacks/transactions';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import * as whyIsNodeRunning from 'why-is-node-running';
+
 // ts-unused-exports:disable-next-line
 export default async (): Promise<void> => {
   console.log('Jest - setup..');
@@ -30,4 +34,7 @@ export default async (): Promise<void> => {
   console.log('Waiting for RPC connection to core node..');
   await new StacksCoreRpcClient().waitForConnection(60000);
   console.log('Jest - setup done');
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  setInterval(() => whyIsNodeRunning(), 5000).unref();
 };
