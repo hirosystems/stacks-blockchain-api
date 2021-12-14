b684ce7e082f2f2609ec0d8bd9095fa949b1f89f
Asim Mehmood <asim@tintash.com>
2021-12-14 16:00:38 +0500

    fix: fixed rebase issue

--- src/tests/api-tests.ts
+++ src/tests/api-tests.ts
@@ -4043,7 +4043,7 @@ describe('api tests', () => {
       createStxEvent(testAddr2, testContractAddr, 40, false),
       createStxEvent(testContractAddr, testAddr4, 15),
       createStxEvent(testAddr2, testAddr4, 35),
-      createStxEvent(testAddr2, testAddr5, 5000),
+      createStxEvent(testAddr2, testAddr7, 5000),
     ];
 
     const createFtEvent = (
@@ -4984,7 +4984,7 @@ describe('api tests', () => {
     const blockTxsRows = await api.datastore.getBlockTxsRows(block.block_hash);
     expect(blockTxsRows.found).toBe(true);
     const blockTxsRowsResult = blockTxsRows.result as DbTx[];
-    expect(blockTxsRowsResult[6]).toEqual({ ...contractCall, ...{ abi: contractJsonAbi } });
+    expect(blockTxsRowsResult).toContainEqual({ ...contractCall, ...{ abi: contractJsonAbi } });
 
     const searchResult8 = await supertest(api.server).get(
       `/extended/v1/search/0x1232000000000000000000000000000000000000000000000000000000000000?include_metadata`
@@ -4994,7 +4994,7 @@ describe('api tests', () => {
     expect(JSON.parse(searchResult8.text).result.metadata).toEqual(contractCallExpectedResults);
 
     const blockTxResult = await db.getTxsFromBlock('0x1234', 20, 0);
-    expect(blockTxResult.results[6]).toEqual({ ...contractCall, ...{ abi: contractJsonAbi } });
+    expect(blockTxResult.results).toContainEqual({ ...contractCall, ...{ abi: contractJsonAbi } });
   });
 
   test('list contract log events', async () => {
