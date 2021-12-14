hirosystems/stacks-blockchain-api

commit f18068c300465b0944b9160de7d0f6c3e1d18827

feat: return all the contracts implement a given trait
* feat: implement an api to get all contracts which implement a given trait

* test: add test cases for contract/trait endpoint

* docs: added docs for contract/trait endpoint

* fix: add return statment after response

* perf: add index for jsonb abi column

* refactor: use createIndex method

* fix: use get request instead of post for contracts trait

* docs: update docs for trait/contract api

* fix: fixed rebase issue

* test: add test for large query data

* refactor: change path of contract/by_trait api

src/api/routes/contract.ts
@@ -3,6 +3,8 @@ import { addAsync, RouterWithAsync } from '@awaitjs/express';
 import { DataStore } from '../../datastore/common';
 import { parseLimitQuery, parsePagingQueryInput } from '../pagination';
 import { parseDbEvent } from '../controllers/db-controller';
+import { ClarityAbi, ClarityAbiTypeId } from '@stacks/transactions';
+import { parseTraitAbi } from '../query-helpers';
 
 const MAX_EVENTS_PER_REQUEST = 50;
 const parseContractEventsQueryLimit = parseLimitQuery({
@@ -12,6 +14,23 @@ const parseContractEventsQueryLimit = parseLimitQuery({
 
 export function createContractRouter(db: DataStore): RouterWithAsync {
   const router = addAsync(express.Router());
+
+  router.getAsync('/by_trait', async (req, res, next) => {
+    const trait_abi = parseTraitAbi(req, res, next);
+    const limit = parseContractEventsQueryLimit(req.query.limit ?? 20);
+    const offset = parsePagingQueryInput(req.query.offset ?? 0);
+    const smartContracts = await db.getSmartContractByTrait({
+      trait: trait_abi,
+      limit,
+      offset,
+    });
+    if (!smartContracts.found) {
+      res.status(404).json({ error: `cannot find contract for this trait` });
+      return;
+    }
+    res.json({ limit, offset, results: smartContracts.result });
+  });
+
   router.getAsync('/:contract_id', async (req, res) => {
     const { contract_id } = req.params;
     const contractQuery = await db.getSmartContract(contract_id);

src/datastore/common.ts
@@ -652,6 +652,12 @@ export interface DataStore extends DataStoreEventEmitter {
     offset: number;
   }): Promise<FoundOrNot<DbSmartContractEvent[]>>;
 
+  getSmartContractByTrait(args: {
+    trait: ClarityAbi;
+    limit: number;
+    offset: number;
+  }): Promise<FoundOrNot<DbSmartContract[]>>;
+
   update(data: DataStoreBlockUpdateData): Promise<void>;
 
   updateMicroblocks(data: DataStoreMicroblockUpdateData): Promise<void>;

docs/generated.d.ts
@@ -29,6 +29,7 @@ export type SchemaMergeRootStub =
   | BurnchainRewardSlotHolderListResponse
   | BurnchainRewardListResponse
   | ReadOnlyFunctionSuccessResponse
+  | ContractListResponse
   | AccountDataResponse
   | MapEntryResponse
   | ContractInterfaceResponse
@@ -143,6 +144,7 @@ export type SchemaMergeRootStub =
   | BurnchainReward
   | BurnchainRewardsTotal
   | ReadOnlyFunctionArgs
+  | SmartContract
   | {
       target_block_time: number;
     }
@@ -1450,6 +1452,30 @@ export interface ReadOnlyFunctionSuccessResponse {
   okay: boolean;
   result?: string;
   cause?: string;
+}
+/**
+ * GET list of contracts
+ */
+export interface ContractListResponse {
+  /**
+   * The number of contracts to return
+   */
+  limit: number;
+  /**
+   * The number to contracts to skip (starting at `0`)
+   */
+  offset: number;
+  results: SmartContract[];
+}
+/**
+ * A Smart Contract Detail
+ */
+export interface SmartContract {
+  tx_id: string;
+  canonical: boolean;
+  block_height: number;
+  source_code: string;
+  abi: string;
 }
 /**
  * GET request for account data

docs/openapi.yaml
@@ -774,6 +774,42 @@ paths:
           type: boolean
           default: false
 
+  /extended/v1/contract/by_trait:
+    get:
+      summary: Get contracts by trait
+      description: Get all contracts implement a given trait
+      tags:
+        - Smart Contracts
+      operationId: get_contracts_by_trait
+      responses:
+        200:
+          description: List of contracts implement given trait
+          content:
+            application/json:
+              schema:
+                $ref: ./api/contract/smart-contract-list-response.schema.json
+              example:
+                $ref: ./api/contract/smart-contract-list-response.example.json
+      parameters:
+        - name: trait_abi
+          in: query
+          description: JSON abi of the trait
+          required: true
+          schema:
+            type: string
+        - name: limit
+          in: query
+          description: max number of contracts fetch
+          required: false
+          schema:
+            type: integer
+        - name: offset
+          in: query
+          description: index of first contract event to fetch
+          required: false
+          schema:
+            type: integer
+
   /extended/v1/contract/{contract_id}/events:
     get:
       summary: Get contract events
@@ -782,6 +818,12 @@ paths:
         - Smart Contracts
       operationId: get_contract_events_by_id
       parameters:
+        - name: contract_id
+          in: path
+          description: Contract identifier formatted as `<contract_address>.<contract_name>`
+          required: true
+          schema:
+            type: string
         - name: limit
           in: query
           description: max number of contract events to fetch
@@ -794,12 +836,6 @@ paths:
           required: false
           schema:
             type: integer
-        - name: contract_id
-          in: path
-          description: Contract identifier formatted as `<contract_address>.<contract_name>`
-          required: true
-          schema:
-            type: string
         - name: unanchored
           in: query
           description: Include transaction data from unanchored (i.e. unconfirmed) microblocks

src/api/controllers/db-controller.ts
@@ -1011,7 +1011,7 @@ function parseContractCallMetadata(
   if (!contract.result.abi) {
     return parsedTx;
   }
-  const contractAbi: ClarityAbi = JSON.parse(contract.result.abi);
+  const contractAbi: ClarityAbi = JSON.parse(JSON.stringify(contract.result.abi));
   const functionAbi = contractAbi.functions.find(
     fn => fn.name === parsedTx.contract_call.function_name
   );

src/api/query-helpers.ts
@@ -1,3 +1,4 @@
+import { ClarityAbi } from '@stacks/transactions';
 import { NextFunction, Request, Response } from 'express';
 import { has0xPrefix } from './../helpers';
 
@@ -175,4 +176,19 @@ export function parseUntilBlockQuery(
     }
   }
   handleBadRequest(res, next, 'until_block must be either `string` or `number`');
+}
+
+export function parseTraitAbi(req: Request, res: Response, next: NextFunction): ClarityAbi | never {
+  if (!('trait_abi' in req.query)) {
+    handleBadRequest(res, next, `Can't find query param 'trait_abi'`);
+  }
+  const trait = req.query.trait_abi;
+  if (typeof trait === 'string') {
+    const trait_abi: ClarityAbi = JSON.parse(trait);
+    if (!('functions' in trait_abi)) {
+      handleBadRequest(res, next, `Invalid 'trait_abi'`);
+    }
+    return trait_abi;
+  }
+  handleBadRequest(res, next, `Invalid 'trait_abi'`);
 }

src/datastore/memory-store.ts
@@ -43,6 +43,7 @@ import { logger, FoundOrNot } from '../helpers';
 import { AddressTokenOfferingLocked, TransactionType } from '@stacks/stacks-blockchain-api-types';
 import { getTxTypeId } from '../api/controllers/db-controller';
 import { RawTxQueryResult } from './postgres-store';
+import { ClarityAbi } from '@stacks/transactions';
 
 export class MemoryDataStore
   extends (EventEmitter as { new (): DataStoreEventEmitter })
@@ -758,6 +759,14 @@ export class MemoryDataStore
     _limit: number,
     _excludingEntries: number[]
   ): Promise<DbTokenMetadataQueueEntry[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  getSmartContractByTrait(args: {
+    trait: ClarityAbi;
+    limit: number;
+    offset: number;
+  }): Promise<FoundOrNot<DbSmartContract[]>> {
     throw new Error('Method not implemented.');
   }
 }

commit fa5e4965166c5928deb217ca996a32a55e1d7ee7

fix: regression introduced in #860 where `contract` http response `abi` field is no longer a json string

src/api/routes/contract.ts
@@ -28,7 +28,11 @@ export function createContractRouter(db: DataStore): RouterWithAsync {
       res.status(404).json({ error: `cannot find contract for this trait` });
       return;
     }
-    res.json({ limit, offset, results: smartContracts.result });
+    const contractResults = smartContracts.result.map(contract => ({
+      ...contract,
+      abi: JSON.stringify(contract.abi),
+    }));
+    res.json({ limit, offset, results: contractResults });
   });
 
   router.getAsync('/:contract_id', async (req, res) => {
@@ -38,7 +42,11 @@ export function createContractRouter(db: DataStore): RouterWithAsync {
       res.status(404).json({ error: `cannot find contract by ID ${contract_id}` });
       return;
     }
-    res.json(contractQuery.result);
+    const contractResult = {
+      ...contractQuery.result,
+      abi: JSON.stringify(contractQuery.result.abi),
+    };
+    res.json(contractResult);
   });
 
   router.getAsync('/:contract_id/events', async (req, res) => {

src/datastore/common.ts
@@ -235,6 +235,7 @@ export interface DbSmartContract {
   contract_id: string;
   block_height: number;
   source_code: string;
+  // TODO: this appears to be a parsed JSON object now when returned from the sql query
   abi: string;
 }
 

src/tests/api-tests.ts
@@ -5100,6 +5100,103 @@ describe('api tests', () => {
     });
   });
 
+  test('get contract by ID', async () => {
+    const block1: DbBlock = {
+      block_hash: '0x1234',
+      index_block_hash: '0xdeadbeef',
+      parent_index_block_hash: '0x00',
+      parent_block_hash: '0xff0011',
+      parent_microblock_hash: '',
+      parent_microblock_sequence: 0,
+      block_height: 1,
+      burn_block_time: 1594647996,
+      burn_block_hash: '0x1234',
+      burn_block_height: 123,
+      miner_txid: '0x4321',
+      canonical: true,
+      execution_cost_read_count: 0,
+      execution_cost_read_length: 0,
+      execution_cost_runtime: 0,
+      execution_cost_write_count: 0,
+      execution_cost_write_length: 0,
+    };
+    const txId1 = '0x421234';
+    const smartContract1: DbSmartContract = {
+      tx_id: txId1,
+      canonical: true,
+      block_height: block1.block_height,
+      contract_id: 'some-contract-id',
+      source_code: '(some-contract-src)',
+      abi: '{"some-abi":1}',
+    };
+    const tx1: DbTx = {
+      tx_id: txId1,
+      tx_index: 0,
+      anchor_mode: 3,
+      nonce: 0,
+      raw_tx: Buffer.alloc(0),
+      index_block_hash: '0x1234',
+      block_hash: '0x5678',
+      block_height: block1.block_height,
+      burn_block_time: 1594647995,
+      parent_burn_block_time: 1626122935,
+      type_id: DbTxTypeId.SmartContract,
+      status: 1,
+      raw_result: '0x0100000000000000000000000000000001', // u1
+      canonical: true,
+      microblock_canonical: true,
+      microblock_sequence: I32_MAX,
+      microblock_hash: '',
+      parent_index_block_hash: '',
+      parent_block_hash: '',
+      post_conditions: Buffer.from([]),
+      fee_rate: 1234n,
+      sponsored: false,
+      sponsor_address: undefined,
+      sender_address: 'sender-addr',
+      origin_hash_mode: 1,
+      smart_contract_contract_id: smartContract1.contract_id,
+      smart_contract_source_code: smartContract1.source_code,
+      event_count: 0,
+      execution_cost_read_count: 0,
+      execution_cost_read_length: 0,
+      execution_cost_runtime: 0,
+      execution_cost_write_count: 0,
+      execution_cost_write_length: 0,
+    };
+
+    await db.update({
+      block: block1,
+      microblocks: [],
+      minerRewards: [],
+      txs: [
+        {
+          tx: tx1,
+          stxLockEvents: [],
+          stxEvents: [],
+          ftEvents: [],
+          nftEvents: [],
+          contractLogEvents: [],
+          smartContracts: [smartContract1],
+          names: [],
+          namespaces: [],
+        },
+      ],
+    });
+
+    const fetchTx = await supertest(api.server).get('/extended/v1/contract/some-contract-id');
+    expect(fetchTx.status).toBe(200);
+    expect(fetchTx.type).toBe('application/json');
+    expect(JSON.parse(fetchTx.text)).toEqual({
+      tx_id: '0x421234',
+      canonical: true,
+      contract_id: 'some-contract-id',
+      block_height: 1,
+      source_code: '(some-contract-src)',
+      abi: '{"some-abi":1}',
+    });
+  });
+
   test('list contract with given trait', async () => {
     const block1: DbBlock = {
       block_hash: '0x1235',
@@ -6278,7 +6375,7 @@ describe('api tests', () => {
       `/extended/v1/contract/by_trait?trait_abi=${JSON.stringify(traitJsonAbiRequest)}`
     );
     expect(query.status).toBe(200);
-    expect(query.body.results[0].abi).toStrictEqual(contractJsonAbi);
+    expect(JSON.parse(query.body.results[0].abi)).toStrictEqual(contractJsonAbi);
 
     const traitJsonAbiRequest1 = {
       maps: [],