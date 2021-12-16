8a31bc9c1b91e22aaa452b0a5190f6cbe9af561e
Rafael Cárdenas <rafael@rafaelcr.com>
2021-12-16 09:29:00 -0600

    chore: expand mempool tx

--- src/test-utils/test-builders.ts
+++ src/test-utils/test-builders.ts
@@ -8,6 +8,7 @@ import {
   DbMicroblockPartial,
   DbSmartContract,
   DbSmartContractEvent,
+  DbTxStatus,
   DbTxTypeId,
 } from '../datastore/common';
 import { bufferCVFromString, serializeCV, uintCV } from '@stacks/transactions';
@@ -22,6 +23,7 @@ const BURN_BLOCK_HASH = '0xf44f44';
 const BURN_BLOCK_HEIGHT = 713000;
 const BURN_BLOCK_TIME = 94869286;
 const SENDER_ADDRESS = 'SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27';
+const RECIPIENT_ADDRESS = 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6';
 const TX_ID = '0x1234';
 const CONTRACT_ID = 'ST27W5M8BRKA7C5MZE2R1S1F4XTPHFWFRNHA9M04Y.hello-world';
 const CONTRACT_SOURCE = '(some-contract-src)';
@@ -158,14 +160,16 @@ export function testTx(args?: TestTxArgs): DataStoreTxEventData {
 }
 
 export interface TestMempoolTxArgs {
+  contract_call_contract_id?: string;
+  contract_call_function_args?: Buffer;
+  contract_call_function_name?: string;
+  pruned?: boolean;
   sender_address?: string;
+  smart_contract_contract_id?: string;
+  status?: DbTxStatus;
+  token_transfer_recipient_address?: string;
   tx_id?: string;
   type_id?: DbTxTypeId;
-  pruned?: boolean;
-  smart_contract_contract_id?: string;
-  contract_call_contract_id?: string;
-  contract_call_function_name?: string;
-  contract_call_function_args?: Buffer;
 }
 
 export function testMempoolTx(args?: TestMempoolTxArgs): DbMempoolTx {
@@ -177,7 +181,7 @@ export function testMempoolTx(args?: TestMempoolTxArgs): DbMempoolTx {
     raw_tx: Buffer.from('test-raw-tx'),
     type_id: args?.type_id ?? DbTxTypeId.TokenTransfer,
     receipt_time: (new Date().getTime() / 1000) | 0,
-    status: 1,
+    status: args?.status ?? DbTxStatus.Pending,
     post_conditions: Buffer.from([0x01, 0xf5]),
     fee_rate: 1234n,
     sponsored: false,
@@ -186,6 +190,7 @@ export function testMempoolTx(args?: TestMempoolTxArgs): DbMempoolTx {
     sender_address: args?.sender_address ?? SENDER_ADDRESS,
     token_transfer_amount: 1234n,
     token_transfer_memo: Buffer.alloc(0),
+    token_transfer_recipient_address: args?.token_transfer_recipient_address ?? RECIPIENT_ADDRESS,
     smart_contract_contract_id: args?.smart_contract_contract_id ?? CONTRACT_ID,
     contract_call_contract_id: args?.contract_call_contract_id ?? CONTRACT_ID,
     contract_call_function_name: args?.contract_call_function_name ?? CONTRACT_CALL_FUNCTION_NAME,
--- src/tests/socket-io-tests.ts
+++ src/tests/socket-io-tests.ts
@@ -21,7 +21,11 @@ import {
   AddressTransactionWithTransfers,
   AddressStxBalanceResponse,
 } from '../../docs/generated';
-import { TestBlockBuilder, TestMicroblockStreamBuilder } from '../test-utils/test-builders';
+import {
+  TestBlockBuilder,
+  testMempoolTx,
+  TestMicroblockStreamBuilder,
+} from '../test-utils/test-builders';
 
 describe('socket-io', () => {
   let apiServer: ApiServer;
@@ -98,83 +102,23 @@ describe('socket-io', () => {
   });
 
   test('socket-io > tx updates', async () => {
-    const addr1 = 'ST28D4Q6RCQSJ6F7TEYWQDS4N1RXYEP9YBWMYSB97';
-    const addr2 = 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6';
-    const block: DbBlock = {
-      block_hash: '0x1234',
-      index_block_hash: '0xdeadbeef',
-      parent_index_block_hash: '0x00',
-      parent_block_hash: '0xff0011',
-      parent_microblock_hash: '',
-      block_height: 1,
-      burn_block_time: 94869286,
-      burn_block_hash: '0x1234',
-      burn_block_height: 123,
-      miner_txid: '0x4321',
-      canonical: true,
-      parent_microblock_sequence: 0,
-      execution_cost_read_count: 0,
-      execution_cost_read_length: 0,
-      execution_cost_runtime: 0,
-      execution_cost_write_count: 0,
-      execution_cost_write_length: 0,
-    };
-    const tx1: DbTx = {
-      tx_id: '0x01',
-      tx_index: 0,
-      anchor_mode: 3,
-      nonce: 0,
-      raw_tx: Buffer.alloc(0),
-      index_block_hash: block.index_block_hash,
-      block_hash: block.block_hash,
-      block_height: block.block_height,
-      burn_block_time: block.burn_block_time,
-      parent_burn_block_time: 1626122935,
-      type_id: DbTxTypeId.TokenTransfer,
-      status: 1,
-      raw_result: '0x0100000000000000000000000000000001', // u1
-      canonical: true,
-      post_conditions: Buffer.from([0x01, 0xf5]),
-      fee_rate: 1234n,
-      sponsored: false,
-      sponsor_address: undefined,
-      sender_address: addr1,
-      origin_hash_mode: 1,
-      token_transfer_recipient_address: addr2,
-      token_transfer_amount: 100n,
-      token_transfer_memo: Buffer.from('memo'),
-      event_count: 1,
-      parent_index_block_hash: '',
-      parent_block_hash: '',
-      microblock_canonical: true,
-      microblock_sequence: I32_MAX,
-      microblock_hash: '',
-      execution_cost_read_count: 0,
-      execution_cost_read_length: 0,
-      execution_cost_runtime: 0,
-      execution_cost_write_count: 0,
-      execution_cost_write_length: 0,
-    };
-    const mempoolTx: DbMempoolTx = {
-      ...tx1,
-      pruned: false,
-      status: DbTxStatus.Pending,
-      receipt_time: 123456,
-    };
-
     const address = apiServer.address;
     const socket = io(`http://${address}`, {
       query: { subscriptions: 'mempool,transaction:0x01' },
     });
     const mempoolWaiter: Waiter<MempoolTransaction> = waiter();
     const txWaiter: Waiter<MempoolTransaction> = waiter();
-
     socket.on('mempool', tx => {
       mempoolWaiter.finish(tx);
     });
     socket.on('transaction', tx => {
       txWaiter.finish(tx);
     });
+
+    const block = new TestBlockBuilder().addTx().build();
+    await db.update(block);
+
+    const mempoolTx = testMempoolTx({ tx_id: '0x01', status: DbTxStatus.Pending });
     await db.updateMempoolTxs({ mempoolTxs: [mempoolTx] });
 
     const mempoolResult = await mempoolWaiter;
