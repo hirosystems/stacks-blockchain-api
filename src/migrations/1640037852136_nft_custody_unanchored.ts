e4757d94d67c78eab7c9872ed62b8b10d430394b
Rafael Cárdenas <rafael@rafaelcr.com>
2022-01-03 09:49:57 -0600

    chore: keep canonical custody in unanchored too

--- src/migrations/1640037852136_nft_custody_unanchored.ts
+++ src/migrations/1640037852136_nft_custody_unanchored.ts
@@ -4,11 +4,17 @@ import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
+  /**
+   * This view is identical to `nft_custody` but it will only refreshed during microblock updates
+   * to calculate an unanchored snapshot of NFT custody.
+   */
   pgm.createMaterializedView('nft_custody_unanchored', {}, `
     SELECT
       DISTINCT ON(asset_identifier, value) asset_identifier, value, recipient, tx_id
     FROM
       nft_events
+    WHERE
+      canonical = true AND microblock_canonical = true
     ORDER BY
       asset_identifier DESC,
       value DESC,
