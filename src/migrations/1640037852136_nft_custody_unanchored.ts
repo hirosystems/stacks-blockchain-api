12242b81942aef3b4c468e6b280a5d5ee553c2ec
Rafael Cárdenas <rafael@rafaelcr.com>
2022-01-07 08:31:57 -0600

    feat: add `/extended/v1/tokens/nft/holdings` endpoint
    * chore: add tx_id and unanchored custody view
    
    * chore: add deprecated msg to old nft_events endpoint
    
    * feat: first version of endpoint
    
    * feat: start returning tx metadata
    
    * feat: filter by asset_identifier
    
    * chore: add openapi docs
    
    * chore: generate new api types
    
    * chore: type api responses correctly
    
    * chore: doc tweaks
    
    * chore: provide param examples
    
    * fix: unanchored view name
    
    * fix: db-controller import path
    
    * feat: support multiple asset identifiers
    
    * chore: doc tweaks
    
    * chore: add unit tests
    
    * fix: execute complete search in one query
    
    * chore: return old unused imports, otherwise PR gets big
    
    * chore: remove debug code
    
    * chore: provide tx metadata example
    
    * chore: keep canonical custody in unanchored too
    
    * feat: set chain tip cache headers
    
    * fix: refresh views with proper logic
    
    * fix: outdated cond
    
    * fix: only consider nft_events from canonical txs
    
    * chore: add default values to params in openapi

--- src/migrations/1640037852136_nft_custody_unanchored.ts
+++ src/migrations/1640037852136_nft_custody_unanchored.ts
@@ -4,7 +4,11 @@ import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.createMaterializedView('nft_custody', {}, `
+  /**
+   * This view is identical to `nft_custody` but it will only refreshed during microblock updates
+   * to calculate an unanchored snapshot of NFT custody.
+   */
+  pgm.createMaterializedView('nft_custody_unanchored', {}, `
     SELECT
       DISTINCT ON(asset_identifier, value) asset_identifier, value, recipient, tx_id, nft.block_height
     FROM
@@ -19,9 +23,9 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
       nft.block_height DESC
   `);
 
-  pgm.createIndex('nft_custody', ['asset_identifier', 'value']);
-  pgm.createIndex('nft_custody', 'recipient');
-  pgm.createIndex('nft_custody', [
+  pgm.createIndex('nft_custody_unanchored', ['asset_identifier', 'value']);
+  pgm.createIndex('nft_custody_unanchored', 'recipient');
+  pgm.createIndex('nft_custody_unanchored', [
     { name: 'block_height', sort: 'DESC' }
   ]);
 }
