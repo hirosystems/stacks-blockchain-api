2a74457f754ccd200550cc2033dc3c6b4d150152
Rafael Cárdenas <rafael@rafaelcr.com>
2022-01-18 12:23:40 -0600

    fix: revert block height changes

--- src/datastore/postgres-store.ts
+++ src/datastore/postgres-store.ts
@@ -2764,8 +2764,8 @@ export class PgDataStore
         `
         SELECT ${BLOCK_COLUMNS}
         FROM blocks
-        WHERE block_hash = $1 AND canonical = true
-        ORDER BY block_height DESC
+        WHERE block_hash = $1
+        ORDER BY canonical DESC, block_height DESC
         LIMIT 1
         `,
         [hexToBuffer(blockIdentifer.hash)]
@@ -2775,7 +2775,8 @@ export class PgDataStore
         `
         SELECT ${BLOCK_COLUMNS}
         FROM blocks
-        WHERE block_height = $1 AND canonical = true
+        WHERE block_height = $1
+        ORDER BY canonical DESC
         LIMIT 1
         `,
         [blockIdentifer.height]
@@ -2785,8 +2786,8 @@ export class PgDataStore
         `
         SELECT ${BLOCK_COLUMNS}
         FROM blocks
-        WHERE burn_block_hash = $1 AND canonical = true
-        ORDER BY block_height DESC
+        WHERE burn_block_hash = $1
+        ORDER BY canonical DESC, block_height DESC
         LIMIT 1
         `,
         [hexToBuffer(blockIdentifer.burnBlockHash)]
@@ -2796,8 +2797,8 @@ export class PgDataStore
         `
         SELECT ${BLOCK_COLUMNS}
         FROM blocks
-        WHERE burn_block_height = $1 AND canonical = true
-        ORDER BY block_height DESC
+        WHERE burn_block_height = $1
+        ORDER BY canonical DESC, block_height DESC
         LIMIT 1
         `,
         [blockIdentifer.burnBlockHeight]
