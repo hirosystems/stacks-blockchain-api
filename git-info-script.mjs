067bd8e75e2be81e5a365038a39c48c5d12e26b4
Asim Mehmood <asim@tintash.com>
2022-01-19 16:35:42 +0500

    fix: use shalljs to support cross platform shell script

--- git-info-script.mjs
+++ git-info-script.mjs
@@ -1,13 +1,10 @@
-import { exec } from 'child_process';
+import  shell from 'shelljs';
 
-exec('git --version', (error, _stdout, stderr) => {
-  if (error) {
-    throw new Error(`"git is missing", please install git and retry\n${error.message}`);
-  }
-  if (stderr) {
-    throw new Error(`"git is missing", please install git and retry\n${stderr}`);
-  }
-  const gitInfo = `echo \"$(git rev-parse --abbrev-ref HEAD)\n$(git log -1 --pretty=format:%h)\n$(git describe --tags --abbrev=0)\" > ./.git-info`;
-  console.log(gitInfo);
-  exec(gitInfo);
-});
+if (!shell.which('git')) {
+  throw new Error(`"git is missing", please install git and retry`);
+}
+shell
+  .exec(
+    `echo \"$(git rev-parse --abbrev-ref HEAD)\n$(git log -1 --pretty=format:%h)\n$(git describe --tags --abbrev=0)\"`
+  )
+  .to('./.git-info');
