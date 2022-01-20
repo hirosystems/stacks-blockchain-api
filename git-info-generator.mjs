cd6ed19bec2f3bf3528d800af7a2c56400014d95
Asim Mehmood <asim@tintash.com>
2022-01-20 12:26:36 +0500

    refactor: make code more readable

--- git-info-generator.mjs
+++ git-info-generator.mjs
@@ -1,10 +1,14 @@
-import  shell from 'shelljs';
+import { execSync } from 'child_process';
+import { writeFileSync } from 'fs';
+import shell from 'shelljs';
 
 if (!shell.which('git')) {
   throw new Error(`"git is missing", please install git and retry`);
 }
-shell
-  .exec(
-    `echo \"$(git rev-parse --abbrev-ref HEAD)\n$(git log -1 --pretty=format:%h)\n$(git describe --tags --abbrev=0)\"`
-  )
-  .to('./.git-info');
+
+const gitInfo = [
+  'git rev-parse --abbrev-ref HEAD',
+  'git log -1 --pretty=format:%h',
+  'git describe --tags --abbrev=0',
+].map(r => execSync(r, { encoding: 'utf8' }).trim());
+writeFileSync('.git-info', gitInfo.join('\n'));
