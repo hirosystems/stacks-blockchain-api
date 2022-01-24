61d23137e169aa35b22156b9e32c9c575a74d686
Asim Mehmood <asim@tintash.com>
2022-01-24 21:09:11 +0500

    chore: remove shelljs and update tsconfig

--- git-info-generator.mjs
+++ git-info-generator.mjs
@@ -1,22 +1,24 @@
 import { execSync } from 'child_process';
 import { writeFileSync } from 'fs';
-import shell from 'shelljs';
 
-if (!shell.which('git')) {
-  throw new Error(`"git is missing", please install git and retry`);
+try {
+  execSync('git --version');
+  const gitInfo = [
+    'git rev-parse --abbrev-ref HEAD',
+    'git log -1 --pretty=format:%h',
+    'git describe --tags --abbrev=0',
+  ].map((r, index) => {
+    try {
+      execSync(r, { encoding: 'utf8' }).trim();
+    } catch (error) {
+      console.error(error.message);
+      if (index === 2)
+        throw new Error(`no tag found fetch tags by running "git fetch --all --tags"`);
+      throw error;
+    }
+  });
+  writeFileSync('.git-info', gitInfo.join('\n'));
+} catch (error) {
+  console.error(error.message);
+  throw new Error(`git is missing, please install git and retry`);
 }
-
-const gitInfo = [
-  'git rev-parse --abbrev-ref HEAD',
-  'git log -1 --pretty=format:%h',
-  'git describe --tags --abbrev=0',
-].map((r, index) => {
-  try {
-    execSync(r, { encoding: 'utf8' }).trim();
-  } catch (error) {
-    if (index === 2)
-      throw new Error(`"no tag found"`, 'fetch tags by running "git fetch --all --tags"');
-    throw error;
-  }
-});
-writeFileSync('.git-info', gitInfo.join('\n'));
