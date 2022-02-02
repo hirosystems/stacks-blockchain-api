98d1eceb8282ed8027eff224b6a93166c4c1752f
Rafael Cárdenas <rafael@rafaelcr.com>
2022-02-02 10:42:00 -0600

    feat: include block hashes

--- src/migrations/1643755236533_chain_tip.ts
+++ src/migrations/1643755236533_chain_tip.ts
@@ -5,28 +5,35 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.createMaterializedView('chain_tip', {}, `
-    WITH block_s AS (
-      SELECT MAX(block_height) AS blocks
-      FROM blocks
-      WHERE canonical = TRUE
-    ), microblock_s AS (
-      SELECT COUNT(*)::INTEGER AS microblocks
+    WITH block_stats AS (
+      SELECT block_height, block_hash, index_block_hash
+        FROM blocks
+        WHERE block_height = (SELECT MAX(block_height) FROM blocks WHERE canonical = TRUE)
+    ), microblock_stats AS (
+      SELECT microblock_hash
       FROM microblocks
       WHERE canonical = TRUE AND microblock_canonical = TRUE
-    ), tx_s AS (
-      SELECT COUNT(*)::INTEGER AS txs
+      ORDER BY block_height DESC
+      LIMIT 1
+    ), microblock_count AS (
+      SELECT COUNT(*)::INTEGER AS microblock_count
+      FROM microblocks
+      WHERE canonical = TRUE AND microblock_canonical = TRUE
+    ), tx_count AS (
+      SELECT COUNT(*)::INTEGER AS tx_count
       FROM txs
       WHERE canonical = TRUE AND microblock_canonical = TRUE
         AND block_height <= (SELECT MAX(block_height) FROM blocks WHERE canonical = TRUE)
-    ), tx_s_unanchored AS (
-      SELECT COUNT(*)::INTEGER AS txs_unanchored
+    ), tx_count_unanchored AS (
+      SELECT COUNT(*)::INTEGER AS tx_count_unanchored
       FROM txs
       WHERE canonical = TRUE AND microblock_canonical = TRUE
     )
-    SELECT block_s.blocks AS block_height, *
-    FROM block_s
-    CROSS JOIN microblock_s
-    CROSS JOIN tx_s
-    CROSS JOIN tx_s_unanchored
+    SELECT *, block_stats.block_height AS block_count
+    FROM block_stats
+    CROSS JOIN microblock_stats
+    CROSS JOIN microblock_count
+    CROSS JOIN tx_count
+    CROSS JOIN tx_count_unanchored
   `);
 }
