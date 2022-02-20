0ac18700d53c034be969dfed5d19a72eafcdc0a5
Rafael Cárdenas <rafael@rafaelcr.com>
2022-02-19 18:26:40 -0600

    feat: return chain tip info

--- src/api/routes/status.ts
+++ src/api/routes/status.ts
@@ -4,16 +4,26 @@ import { DataStore } from '../../datastore/common';
 import { ServerStatusResponse } from '@stacks/stacks-blockchain-api-types';
 import { logger } from '../../helpers';
 
-export function createStatusRouter(_: DataStore): express.Router {
+export function createStatusRouter(db: DataStore): express.Router {
   const router = express.Router();
 
-  const statusHandler = (_: Request, res: any) => {
+  const statusHandler = async (_: Request, res: any) => {
     try {
       const [branch, commit, tag] = fs.readFileSync('.git-info', 'utf-8').split('\n');
       const response: ServerStatusResponse = {
         server_version: `stacks-blockchain-api ${tag} (${branch}:${commit})`,
         status: 'ready',
       };
+      const chainTip = await db.getUnanchoredChainTip();
+      if (chainTip.found) {
+        response.chain_tip = {
+          block_height: chainTip.result.blockHeight,
+          block_hash: chainTip.result.blockHash,
+          index_block_hash: chainTip.result.indexBlockHash,
+          microblock_hash: chainTip.result.microblockHash,
+          microblock_sequence: chainTip.result.microblockSequence,
+        };
+      }
       res.json(response);
     } catch (error) {
       logger.error(`Unable to read git info`, error);
