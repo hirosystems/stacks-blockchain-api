1e1af51bf25b2a937f2ad9317aebd7ee8e72af08
Rafael Cárdenas <rafael@rafaelcr.com>
2022-03-18 09:34:23 -0600

    fix: only use bit_xor if we're on pg14+

--- src/migrations/1647573802137_mempool_digest.ts
+++ src/migrations/1647573802137_mempool_digest.ts
@@ -3,10 +3,21 @@ import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
+async function pgServerMajorVersion(pgm: MigrationBuilder): Promise<number | undefined> {
+  const result = await pgm.db.query(`SHOW server_version`);
+  if (result.rowCount === 0) {
+    return;
+  }
+  return parseInt(result.rows[0].server_version
+    .split(' ')[0] // Remove additional info e.g. "(Debian 11.12-1.pgdg100+1)"
+    .split('.')[0] // Take only the major version e.g. "14" from "14.2"
+  );
+}
+
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  const serverVersion = await pgm.db.query(`SHOW server_version`);
-  const majorVersion = parseInt(serverVersion.rows[0].server_version.split(' ')[0].split('.')[0]);
-  if (majorVersion >= 14) {
+  const serverVersion = await pgServerMajorVersion(pgm);
+  // The `bit_xor` function is available starting at PostgreSQL 14.
+  if (serverVersion && serverVersion >= 14) {
     pgm.createMaterializedView('mempool_digest', {}, `
       SELECT to_hex(bit_xor(tx_short_id)) AS digest
       FROM (
@@ -16,6 +27,9 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
       ) m
     `);
   } else {
+    // TODO: Create a backwards-compatible view.
+    // We could use postgres' `digest` function here, but that requires enabling the `pgcrypto`
+    // extension, which might not be possible for some users.
     pgm.createMaterializedView('mempool_digest', {}, `
       SELECT NULL AS digest
     `);
