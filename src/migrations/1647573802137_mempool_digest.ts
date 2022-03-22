90a83b1aa613a5cd205fdcf9475c640cb3047101
Rafael Cárdenas <rafael@rafaelcr.com>
2022-03-22 13:55:15 -0600

    fix: check if bit_xor is available instead of pg version

--- src/migrations/1647573802137_mempool_digest.ts
+++ src/migrations/1647573802137_mempool_digest.ts
@@ -3,31 +3,30 @@ import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
-async function pgServerMajorVersion(pgm: MigrationBuilder): Promise<number | undefined> {
-  const result = await pgm.db.query(`SHOW server_version`);
-  if (result.rowCount === 0) {
-    return;
+async function isBitXorAvailable(pgm: MigrationBuilder) {
+  try {
+    await pgm.db.query('SELECT bit_xor(1)');
+    return true;
+  } catch (error: any) {
+    if (error.code === '42883' /* UNDEFINED_FUNCTION */) {
+      return false;
+    }
+    throw error;
   }
-  return parseInt(result.rows[0].server_version
-    .split(' ')[0] // Remove additional info e.g. "(Debian 11.12-1.pgdg100+1)"
-    .split('.')[0] // Take only the major version e.g. "14" from "14.2"
-  );
 }
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  const serverVersion = await pgServerMajorVersion(pgm);
-  // The `bit_xor` function is available starting at PostgreSQL 14.
-  if (serverVersion && serverVersion >= 14) {
+  if (await isBitXorAvailable(pgm)) {
     pgm.createMaterializedView('mempool_digest', {}, `
       SELECT to_hex(bit_xor(tx_short_id)) AS digest
       FROM (
-          SELECT ('x' || encode(tx_id, 'hex'))::bit(64)::bigint tx_short_id
-          FROM mempool_txs
-          WHERE pruned = false
+        SELECT ('x' || encode(tx_id, 'hex'))::bit(64)::bigint tx_short_id
+        FROM mempool_txs
+        WHERE pruned = false
       ) m
     `);
   } else {
-    // TODO: Create a backwards-compatible view.
+    // Assume mempool cache is always invalid.
     // We could use postgres' `digest` function here, but that requires enabling the `pgcrypto`
     // extension which might not be possible for some users.
     pgm.createMaterializedView('mempool_digest', {}, `SELECT NULL AS digest`);
