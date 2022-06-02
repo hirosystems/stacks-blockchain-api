52d51597f51bae561225d6c75fcfca1122230c4f
Rafael Cárdenas <rafael@rafaelcr.com>
2022-06-02 09:52:02 -0500

    refactor: interface draft

--- src/api/routes/ws/listener.ts
+++ src/api/routes/ws/listener.ts
@@ -1,8 +1,38 @@
 import {
+  AddressStxBalanceResponse,
+  AddressTransactionWithTransfers,
+  Block,
+  MempoolTransaction,
+  Microblock,
+  Transaction,
+} from 'docs/generated';
+import {
   getBlockFromDataStore,
+  getMempoolTxsFromDataStore,
   getMicroblockFromDataStore,
+  getTxFromDataStore,
 } from '../../../api/controllers/db-controller';
 import { PgStore } from '../../../datastore/pg-store';
+
+export interface WebSocketChannel {
+  hasBlockListeners(): boolean;
+  sendBlock(block: Block): void;
+
+  hasMicroblockListeners(): boolean;
+  sendMicroblock(microblock: Microblock): void;
+
+  hasMempoolListeners(): boolean;
+  sendMempoolTransaction(transaction: MempoolTransaction): void;
+
+  hasTransactionListeners(txId: string): boolean;
+  sendTransaction(transaction: Transaction | MempoolTransaction): void;
+
+  hasPrincipalTransactionListeners(principal: string): boolean;
+  sendPrincipalTransaction(principal: string, transaction: AddressTransactionWithTransfers): void;
+
+  hasPrincipalStxBalanceListeners(principal: string): boolean;
+  sendPrincipalStxBalance(principal: string, stxBalance: AddressStxBalanceResponse): void;
+}
 
 export class EventListener {
   readonly db: PgStore;
@@ -20,6 +50,10 @@ export class EventListener {
     this.db.eventEmitter.addListener('addressUpdate', (address, blockHeight) =>
       this.addressUpdate(address, blockHeight)
     );
+  }
+
+  close() {
+    //
   }
 
   private async blockUpdate(blockHash: string) {
@@ -45,7 +79,23 @@ export class EventListener {
   }
 
   private async txUpdate(txId: string) {
-    //
+    const mempoolTxs = await getMempoolTxsFromDataStore(this.db, {
+      txIds: [txId],
+      includeUnanchored: true,
+    });
+    if (mempoolTxs.length > 0) {
+      // prometheus?.sendEvent('transaction');
+      // io.to(mempoolTopic).emit('transaction', mempoolTxs[0]);
+    } else {
+      const txQuery = await getTxFromDataStore(this.db, {
+        txId: txId,
+        includeUnanchored: true,
+      });
+      if (txQuery.found) {
+        // prometheus?.sendEvent('transaction');
+        // io.to(mempoolTopic).emit('transaction', txQuery.result);
+      }
+    }
   }
 
   private async addressUpdate(address: string, blockHeight: number) {
