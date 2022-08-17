6d32f42db30c667416850fe1fedfd0626e81710a
Matthew Little <zone117x@gmail.com>
2022-08-17 20:08:25 +0200

    feat: separate suite for RPC request tests

---
+++ src/tests-rpc/setup.ts
@@ -0,0 +1,12 @@
+import { loadDotEnv } from '../helpers';
+
+// ts-unused-exports:disable-next-line
+export default () => {
+  console.log('Jest - setup..');
+  if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'test';
+  }
+  loadDotEnv();
+  process.env.PG_DATABASE = 'postgres';
+  console.log('Jest - setup done');
+};
