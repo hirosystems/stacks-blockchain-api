20ae96c2866c67cf58e8cc3350715349760793e5
Rafael Cárdenas <rafael@rafaelcr.com>
2022-09-21 10:33:58 -0500

    fix: add ECONNRESET error code

--- src/datastore/helpers.ts
+++ src/datastore/helpers.ts
@@ -182,6 +182,8 @@ export function isPgConnectionError(error: any): string | false {
     return 'Postgres connection ETIMEDOUT';
   } else if (error.code === 'ENOTFOUND') {
     return 'Postgres connection ENOTFOUND';
+  } else if (error.code === 'ECONNRESET') {
+    return 'Postgres connection ECONNRESET';
   } else if (error.message) {
     const msg = (error as Error).message.toLowerCase();
     if (msg.includes('database system is starting up')) {
