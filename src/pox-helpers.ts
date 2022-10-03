86c9a5a2eb4024d3d47a632aa7f7f2b330bb1c5d
Matthew Little <zone117x@gmail.com>
2022-10-03 18:29:15 +0200

    chore: pin stacks2.1 images to hash

--- src/pox-helpers.ts
+++ src/pox-helpers.ts
@@ -16,11 +16,11 @@ enum PoXAddressVersion {
   P2SHP2WPKH = 0x02, // likely unused, as indistinguishable from P2SH
   /** (b58/legacy) p2wsh-p2sh address, and `hashbytes` is the 20-byte hash160 of a p2wsh witness script */
   P2SHP2WSH = 0x03, // likely unused, as indistinguishable from P2SH
-  /** (bech32/segwit) p2wpkh address, and `hashbytes` is the 20-byte hash160 of the witness script */
+  /** (bech32/segwit_v0) p2wpkh address, and `hashbytes` is the 20-byte hash160 of the witness script */
   P2WPKH = 0x04,
-  /** (bech32/segwit) p2wsh address, and `hashbytes` is the 32-byte sha256 of the witness script */
+  /** (bech32/segwit_v0) p2wsh address, and `hashbytes` is the 32-byte sha256 of the witness script */
   P2WSH = 0x05,
-  /** (bech32/segwit) p2tr address, and `hashbytes` is the 32-byte sha256 of the witness script */
+  /** (bech32m/segwit_v1) p2tr address, and `hashbytes` is the 32-byte sha256 of the witness script */
   P2TR = 0x06,
 }
 
