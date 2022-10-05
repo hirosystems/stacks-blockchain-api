00c5b81401c8e2ef659424d27c9046d0a0ee25cb
Rafael Cárdenas <rafael@rafaelcr.com>
2022-10-05 10:18:51 -0500

    feat: add event-replay test suite

--- src/tests-event-replay/teardown.ts
+++ src/tests-event-replay/teardown.ts
@@ -1,5 +1,17 @@
+import * as isCI from 'is-ci';
+
 // ts-unused-exports:disable-next-line
 export default (): void => {
   console.log('Jest - teardown');
-  console.log('Jest - teardown done');
+
+  // If running in CI setup the "why am I still running?" log to detect stuck Jest tests
+  if (isCI) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const whyIsNodeRunning = require('why-is-node-running');
+    let whyRunInterval = 1000;
+    setInterval(() => {
+      console.log('\n\n\n\n_____WHY IS NODE RUNNING_____');
+      whyIsNodeRunning();
+    }, (whyRunInterval *= 2)).unref();
+  }
 };
