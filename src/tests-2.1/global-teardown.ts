897a4e2bd3c3d2cdaa22d5a938f6a15109fa4693
Matthew Little <zone117x@gmail.com>
2022-10-18 17:10:29 +0200

    chore: lint fixes

--- src/tests-2.1/global-teardown.ts
+++ src/tests-2.1/global-teardown.ts
@@ -1,5 +1,6 @@
 import type { GlobalTestEnv } from './global-setup';
 
+// ts-unused-exports:disable-next-line
 export default async (): Promise<void> => {
   console.log('Jest - global teardown..');
   const testEnv: GlobalTestEnv = (global as any).globalTestEnv;
