758a378ab7ae196017110f20def93f5135484a6b
Rafael Cárdenas <rafael@rafaelcr.com>
2022-11-04 10:42:45 -0600

    chore: drop btc faucet support

--- src/api/routes/faucets.ts
+++ src/api/routes/faucets.ts
@@ -1,13 +1,11 @@
 import * as process from 'process';
 import * as express from 'express';
 import { asyncHandler } from '../async-handler';
-import * as btc from 'bitcoinjs-lib';
 import PQueue from 'p-queue';
 import * as BN from 'bn.js';
 import { BigNumber } from 'bignumber.js';
 import { AnchorMode, makeSTXTokenTransfer, SignedTokenTransferOptions } from '@stacks/transactions';
 import { StacksNetwork } from '@stacks/network';
-import { makeBtcFaucetPayment, getBtcBalance } from '../../btc-faucet';
 import { DbFaucetRequestCurrency } from '../../datastore/common';
 import { intMax, logger, stxToMicroStx } from '../../helpers';
 import { testnetKeys, getStacksTestnetNetwork } from './debug';
@@ -64,59 +62,6 @@ export function createFaucetRouter(db: PgWriteStore): express.Router {
   const router = express.Router();
   router.use(express.urlencoded({ extended: true }));
   router.use(express.json());
-
-  const btcFaucetRequestQueue = new PQueue({ concurrency: 1 });
-
-  router.post(
-    '/btc',
-    asyncHandler(async (req, res) => {
-      await btcFaucetRequestQueue.add(async () => {
-        const address: string = req.query.address || req.body.address;
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-        // Guard condition: requests are limited to 5 times per 5 minutes.
-        // Only based on address for now, but we're keeping the IP in case
-        // we want to escalate and implement a per IP policy
-        const lastRequests = await db.getBTCFaucetRequests(address);
-        const now = Date.now();
-        const window = 5 * 60 * 1000; // 5 minutes
-        const requestsInWindow = lastRequests.results
-          .map(r => now - r.occurred_at)
-          .filter(r => r <= window);
-        if (requestsInWindow.length >= 5) {
-          logger.warn(`BTC faucet rate limit hit for address ${address}`);
-          res.status(429).json({
-            error: 'Too many requests',
-            success: false,
-          });
-          return;
-        }
-
-        const tx = await makeBtcFaucetPayment(btc.networks.regtest, address, 0.5);
-        await db.insertFaucetRequest({
-          ip: `${ip}`,
-          address: address,
-          currency: DbFaucetRequestCurrency.BTC,
-          occurred_at: now,
-        });
-
-        res.json({
-          txid: tx.txId,
-          raw_tx: tx.rawTx,
-          success: true,
-        });
-      });
-    })
-  );
-
-  router.get(
-    '/btc/:address',
-    asyncHandler(async (req, res) => {
-      const { address } = req.params;
-      const balance = await getBtcBalance(btc.networks.regtest, address);
-      res.json({ balance });
-    })
-  );
 
   const stxFaucetRequestQueue = new PQueue({ concurrency: 1 });
 
