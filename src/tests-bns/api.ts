4a73bf4269478c225bfd55a7e5ccdb0e9b61b698
Rafael Cárdenas <rafael@rafaelcr.com>
2022-11-15 11:31:20 -0600

    fix: revert to 404 error code on bns name errors

--- src/tests-bns/api.ts
+++ src/tests-bns/api.ts
@@ -737,7 +737,7 @@ describe('BNS API tests', () => {
 
   test('Failure: name info', async () => {
     const query1 = await supertest(api.server).get(`/v1/names/testname`);
-    expect(query1.status).toBe(400);
+    expect(query1.status).toBe(404);
   });
 
   test('Success: fetching name info', async () => {
