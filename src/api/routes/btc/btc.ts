175d4d1d82e60cec916658ea3014ffe7815d8f7e
Nick Barnett <nbarnett@hiro.so>
2022-11-18 09:51:53 -0600

    feat(btc-api): work in progress

--- src/api/routes/btc/btc.ts
+++ src/api/routes/btc/btc.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import { BlockListResponse } from '@stacks/stacks-blockchain-api-types';
 import { getBlockFromDataStore, getBlocksWithMetadata } from '../../controllers/db-controller';
 import { has0xPrefix } from '../../../helpers';
 import { InvalidRequestError, InvalidRequestErrorType } from '../../../errors';
@@ -8,10 +7,21 @@ import { getBlockHeightPathParam, validateRequestHexInput } from '../../query-he
 import { getETagCacheHandler, setETagCacheHeaders } from '../../controllers/cache-controller';
 import { asyncHandler } from '../../async-handler';
 import { PgStore } from '../../../datastore/pg-store';
-import { getAddressInfo } from './utils';
-import { BLOCKCHAIN_INFO_API_ENDPOINT, STACKS_API_ENDPOINT } from './consts';
+import * as stacksApiClient from '@stacks/blockchain-api-client';
+import * as stackApiTypes from '@stacks/stacks-blockchain-api-types';
+
+import {
+  decodeLeaderBlockCommit,
+  decodeLeaderVrfKeyRegistration,
+  decodeStxTransferOp,
+  fetchJson,
+  getAddressInfo,
+} from './utils';
+import { BLOCKCHAIN_EXPLORER_ENDPOINT, BLOCKCHAIN_INFO_API_ENDPOINT, STACKS_API_ENDPOINT, STACKS_EXPLORER_ENDPOINT } from './consts';
 import fetch, { RequestInit } from 'node-fetch';
 import BigNumber from 'bignumber.js';
+import { b58ToC32 } from 'c32check';
+import * as btc from 'bitcoinjs-lib';
 
 export function createBtcRouter(db: PgStore): express.Router {
   const router = express.Router();
@@ -56,75 +66,371 @@ export function createBtcRouter(db: PgStore): express.Router {
   );
 
   router.get(
-    '/by_burn_block_height/:burnBlockHeight',
+    '/miner-participants/:block',
     cacheHandler,
     asyncHandler(async (req, res) => {
-      const burnBlockHeight = parseInt(req.params['burnBlockHeight'], 10);
-      if (!Number.isInteger(burnBlockHeight)) {
-        throw new InvalidRequestError(
-          `burnchain height is not a valid integer: ${req.params['burnBlockHeight']}`,
-          InvalidRequestErrorType.invalid_param
+      const stxApiConfig = new stacksApiClient.Configuration();
+      const stxBlockApi = new stacksApiClient.BlocksApi(stxApiConfig);
+      let stxBlockData: stacksApiClient.Block;
+      let stxBlockHash: string;
+      let stxBlockHeight: number;
+      if (typeof req.params.block === 'string') {
+        stxBlockHash = req.params.block.toLowerCase();
+        if (!stxBlockHash.startsWith('0x')) {
+          stxBlockHash + '0x' + stxBlockHash;
+        }
+        stxBlockData = (await stxBlockApi.getBlockByHash({
+          hash: stxBlockHash,
+        })) as stacksApiClient.Block;
+        stxBlockHeight = stxBlockData.height;
+      } else {
+        stxBlockHeight = req.params.block;
+        stxBlockData = (await stxBlockApi.getBlockByHeight({
+          height: stxBlockHeight,
+        })) as stacksApiClient.Block;
+        stxBlockHash = stxBlockData.hash;
+      }
+
+      const btcBlockDataUrl = new URL(
+        `/rawblock/${stxBlockData.burn_block_height}`,
+        BLOCKCHAIN_INFO_API_ENDPOINT
+      );
+      const btcBlockData = await fetchJson<{
+        hash: string;
+        height: number;
+        tx: {
+          hash: string;
+          inputs: {
+            prev_out: {
+              addr?: string;
+            };
+          }[];
+          out: {
+            script: string;
+            addr?: string;
+          }[];
+        }[];
+      }>({ url: btcBlockDataUrl });
+      if (btcBlockData.result !== 'ok') {
+        throw new Error(
+          `Status: ${btcBlockData.status}, response: ${JSON.stringify(btcBlockData.response)}`
         );
       }
-      if (burnBlockHeight < 1) {
-        throw new InvalidRequestError(
-          `burnchain height is not a positive integer: ${burnBlockHeight}`,
-          InvalidRequestErrorType.invalid_param
-        );
-      }
-      const block = await getBlockFromDataStore({ blockIdentifer: { burnBlockHeight }, db });
-      if (!block.found) {
-        res.status(404).json({ error: `cannot find block by height ${burnBlockHeight}` });
-        return;
-      }
-      setETagCacheHeaders(res);
-      // TODO: block schema validation
-      res.json(block.result);
+
+      const leaderBlockCommits = btcBlockData.response.tx
+        .filter(tx => tx.out.length > 0)
+        .map(tx => {
+          try {
+            const result = decodeLeaderBlockCommit(tx.out[0].script);
+            if (!result) {
+              return null;
+            }
+            const addr = tx.inputs[0]?.prev_out?.addr ?? null;
+            return {
+              txid: tx.hash,
+              address: addr,
+              stxAddress: addr ? b58ToC32(addr) : null,
+              ...result,
+            };
+          } catch (error) {
+            return null;
+          }
+        })
+        .filter(r => r !== null);
+
+      const winner = leaderBlockCommits.find(tx => tx?.txid === stxBlockData.miner_txid.slice(2));
+      const participants = leaderBlockCommits.map(tx => {
+        return {
+          btcTx: tx?.txid,
+          stxAddress: tx?.stxAddress,
+          btcAddress: tx?.address,
+        };
+      });
+      const payload = {
+        winner: winner?.stxAddress,
+        participants: participants,
+      };
+      res.send(payload);
     })
   );
 
   router.get(
-    '/:hash',
+    '/btc-block-stx-ops/:block',
     cacheHandler,
     asyncHandler(async (req, res) => {
-      const { hash } = req.params;
-
-      if (!has0xPrefix(hash)) {
-        return res.redirect('/extended/v1/block/0x' + hash);
+      const btcBlockDataUrl = new URL(
+        `/rawblock/${req.params.block}`,
+        BLOCKCHAIN_INFO_API_ENDPOINT
+      );
+      const btcBlockData = await fetchJson<{
+        hash: string;
+        height: number;
+        tx: {
+          hash: string;
+          inputs: {
+            prev_out: {
+              addr?: string;
+            };
+          }[];
+          out: {
+            script: string;
+            addr?: string;
+          }[];
+        }[];
+      }>({ url: btcBlockDataUrl });
+      if (btcBlockData.result !== 'ok') {
+        throw new Error(
+          `Status: ${btcBlockData.status}, response: ${JSON.stringify(btcBlockData.response)}`
+        );
       }
-      validateRequestHexInput(hash);
 
-      const block = await getBlockFromDataStore({ blockIdentifer: { hash }, db });
-      if (!block.found) {
-        res.status(404).json({ error: `cannot find block by hash ${hash}` });
-        return;
-      }
-      setETagCacheHeaders(res);
-      // TODO: block schema validation
-      res.json(block.result);
+      const leaderBlockCommits = btcBlockData.response.tx
+        .filter(tx => tx.out.length > 0)
+        .map(tx => {
+          try {
+            const result = decodeLeaderBlockCommit(tx.out[0].script);
+            if (!result) {
+              return null;
+            }
+            const addr = tx.inputs[0]?.prev_out?.addr ?? null;
+            return {
+              txid: tx.hash,
+              address: addr,
+              stxAddress: addr ? b58ToC32(addr) : null,
+              ...result,
+            };
+          } catch (error) {
+            return null;
+          }
+        })
+        .filter(r => r !== null);
+
+      const leaderVrfKeyRegistrations = btcBlockData.response.tx
+        .filter(tx => tx.out.length > 0)
+        .map(tx => {
+          try {
+            const result = decodeLeaderVrfKeyRegistration(tx.out[0].script);
+            if (!result) {
+              return null;
+            }
+            return {
+              txid: tx.hash,
+              address: tx.inputs[0]?.prev_out?.addr ?? null,
+              ...result,
+            };
+          } catch (error) {
+            return null;
+          }
+        })
+        .filter(r => r !== null);
+
+      const stxTransfers = btcBlockData.response.tx
+        .filter(tx => tx.out.length > 0)
+        .map(tx => {
+          try {
+            const result = decodeStxTransferOp(tx.out[0].script);
+            if (!result) {
+              return null;
+            }
+            const fromAddr = tx.inputs[0]?.prev_out?.addr ?? null;
+            const fromStxAddr = fromAddr ? b58ToC32(fromAddr) : null;
+            const toBtcAddr = tx.out[1]?.addr ?? null;
+            const toStxAddr = toBtcAddr ? b58ToC32(toBtcAddr) : null;
+            return {
+              txid: tx.hash,
+              address: fromAddr,
+              fromAddr: fromStxAddr,
+              toAddr: toStxAddr,
+              ...result,
+            };
+          } catch (error) {
+            return null;
+          }
+        })
+        .filter(r => r !== null);
+
+      const payload = {
+        bitcoinBlockHash: btcBlockData.response.hash,
+        bitcoinBlockHeight: btcBlockData.response.height,
+        stxTransfers: stxTransfers,
+        leaderVrfKeyRegistrations: leaderVrfKeyRegistrations,
+        leaderBlockCommits: leaderBlockCommits,
+      };
+      res.send(payload);
     })
   );
+
 
   router.get(
-    '/by_burn_block_hash/:burnBlockHash',
+    '/btc-info-from-stx-tx/:txid',  
     cacheHandler,
     asyncHandler(async (req, res) => {
-      const { burnBlockHash } = req.params;
-
-      if (!has0xPrefix(burnBlockHash)) {
-        return res.redirect('/extended/v1/block/by_burn_block_hash/0x' + burnBlockHash);
+      let { txid } = req.params;
+      txid = txid.toLocaleLowerCase();
+      if (!txid.startsWith('0x')) {
+        txid + '0x' + txid;
       }
+      const stxApiConfig = new stacksApiClient.Configuration();
+      const stxTxApi = new stacksApiClient.TransactionsApi(stxApiConfig);
+      const stxBlockApi = new stacksApiClient.BlocksApi(stxApiConfig);
+      const stxTxData = await stxTxApi.getTransactionById({ txId: txid }) as stackApiTypes.Transaction;
+      const stxBlockHash = stxTxData.block_hash;
+      const stxBlockData = await stxBlockApi.getBlockByHash({ hash: stxBlockHash }) as stackApiTypes.Block;
+      const btcMinerTx = stxBlockData.miner_txid.slice(2);
+      const btcBlockHash = stxBlockData.burn_block_hash.slice(2);
 
-      const block = await getBlockFromDataStore({ blockIdentifer: { burnBlockHash }, db });
-      if (!block.found) {
-        res.status(404).json({ error: `cannot find block by burn block hash ${burnBlockHash}` });
-        return;
+      const stacksBlockExplorerLink = new URL(`/block/${stxBlockHash}?chain=mainnet`, STACKS_EXPLORER_ENDPOINT);
+      const stacksTxExplorerLink = new URL(`/txid/${txid}?chain=mainnet`, STACKS_EXPLORER_ENDPOINT);
+
+      const btcBlockExplorerLink = new URL(`/btc/block/${btcBlockHash}`, BLOCKCHAIN_EXPLORER_ENDPOINT);
+      const btcTxExplorerLink = new URL(`/btc/tx/${btcMinerTx}`, BLOCKCHAIN_EXPLORER_ENDPOINT);
+
+      // const btcBlockDataUrl = new URL(`/rawblock/${btcBlockHash}`, BLOCKCHAIN_INFO_API_ENDPOINT);
+      const btcTxDataUrl = new URL(`/rawtx/${btcMinerTx}`, BLOCKCHAIN_INFO_API_ENDPOINT);
+
+      const btcTxData = await fetchJson<{inputs: { prev_out: { addr: string }}[]}>({ url: btcTxDataUrl });
+      const btcMinerAddr = btcTxData.result === 'ok' ? (btcTxData.response.inputs[0]?.prev_out?.addr ?? "") : "";
+      const btcMinerAddrExplorerLink = new URL(`/btc/address/${btcMinerAddr}`, BLOCKCHAIN_EXPLORER_ENDPOINT);
+
+      const stxMinerAddr = btcMinerAddr ? getAddressInfo(btcMinerAddr).stacks : "";
+      const stxMinerAddrExplorerLink = stxMinerAddr ? new URL(`/address/${stxMinerAddr}?chain=mainnet`, STACKS_EXPLORER_ENDPOINT) : null;
+
+      const payload = {
+        stacksTx: txid,
+        stacksTxExplorer: stacksTxExplorerLink.toString(),
+        stacksBlockHash: stxBlockHash,
+        stacksBlockExplorer: stacksBlockExplorerLink.toString(),
+        bitcoinBlockHash: btcBlockHash,
+        bitcoinBlockExplorer: btcBlockExplorerLink.toString(),
+        bitcoinTx: btcMinerTx,
+        bitcoinTxExplorer: btcTxExplorerLink.toString(),
+        minerBtcAddress: btcMinerAddr,
+        minerBtcAddressExplorer: btcMinerAddrExplorerLink.toString(),
+        minerStxAddress: stxMinerAddr,
+        minerStxAddressExplorer: stxMinerAddrExplorerLink?.toString() ?? "",
+      };
+
+      res.type('application/json').send(payload);
+  }));
+
+  router.get(
+    '/btc-info-from-stx-tx/:txid',  
+    cacheHandler,
+    asyncHandler(async (req, res) => {
+    const { block } = request.params;
+
+    const stxApiConfig = new stacksApiClient.Configuration({ fetchApi: undiciFetch });
+    const stxBlockApi = new stacksApiClient.BlocksApi(stxApiConfig);
+
+    let stxBlockData: stackApiTypes.Block;
+    let stxBlockHash: string;
+    let stxBlockHeight: number;
+    if (typeof block === 'string') {
+      stxBlockHash = block.toLowerCase();
+      if (!stxBlockHash.startsWith('0x')) {
+        stxBlockHash + '0x' + stxBlockHash;
       }
-      setETagCacheHeaders(res);
-      // TODO: block schema validation
-      res.json(block.result);
-    })
-  );
+      stxBlockData = await stxBlockApi.getBlockByHash({ hash: stxBlockHash }) as stackApiTypes.Block;
+      stxBlockHeight = stxBlockData.height;
+    } else {
+      stxBlockHeight = block;
+      stxBlockData = await stxBlockApi.getBlockByHeight({ height: stxBlockHeight }) as stackApiTypes.Block;
+      stxBlockHash = stxBlockData.hash;
+    }
+
+    const btcMinerTx = stxBlockData.miner_txid.slice(2);
+    const btcBlockHash = stxBlockData.burn_block_hash.slice(2);
+
+    const stacksBlockExplorerLink = new URL(`/block/${stxBlockHash}?chain=mainnet`, STACKS_EXPLORER_ENDPOINT);
+
+    const btcBlockExplorerLink = new URL(`/btc/block/${btcBlockHash}`, BLOCKCHAIN_EXPLORER_ENDPOINT);
+    const btcTxExplorerLink = new URL(`/btc/tx/${btcMinerTx}`, BLOCKCHAIN_EXPLORER_ENDPOINT);
+
+    const btcTxDataUrl = new URL(`/rawtx/${btcMinerTx}`, BLOCKCHAIN_INFO_API_ENDPOINT);
+
+    const btcTxData = await fetchJson<{inputs: { prev_out: { addr: string }}[]}>({ url: btcTxDataUrl });
+    const btcMinerAddr = btcTxData.result === 'ok' ? (btcTxData.response.inputs[0]?.prev_out?.addr ?? null) : "";
+    const btcMinerAddrExplorerLink = new URL(`/btc/address/${btcMinerAddr}`, BLOCKCHAIN_EXPLORER_ENDPOINT);
+
+    const stxMinerAddr = btcMinerAddr ? getAddressInfo(btcMinerAddr).stacks : "";
+    const stxMinerAddrExplorerLink = stxMinerAddr ? new URL(`/address/${stxMinerAddr}?chain=mainnet`, STACKS_EXPLORER_ENDPOINT) : "";
+
+    const payload = {
+      stacksBlockHash: stxBlockHash,
+      stacksBlockExplorer: stacksBlockExplorerLink.toString(),
+      bitcoinBlockHash: btcBlockHash,
+      bitcoinBlockExplorer: btcBlockExplorerLink.toString(),
+      bitcoinTx: btcMinerTx,
+      bitcoinTxExplorer: btcTxExplorerLink.toString(),
+      minerBtcAddress: btcMinerAddr,
+      minerBtcAddressExplorer: btcMinerAddrExplorerLink.toString(),
+      minerStxAddress: stxMinerAddr,
+      minerStxAddressExplorer: stxMinerAddrExplorerLink?.toString() ?? "",
+    };
+
+    reply.type('application/json').send(payload);
+  });
+
+  fastify.get('/stx-block', {
+    schema: {
+      tags: ['Bitcoin info'],
+      summary: 'Get the Stacks block associated with a Bitcoin block',
+      description: 'Get the Stacks block information associated with a given Bitcoin block hash or Bitcoin block height',
+      querystring: Type.Object({
+        'btc-block': Type.Union([
+          Type.String({
+            description: 'A bitcoin block hash',
+            pattern: '^([0-9a-fA-F]{64})$',
+          }),
+          Type.Integer({
+            description: 'A bitcoin block height'
+          })
+        ], {
+          examples: ['00000000000000000007a5a46a5989b1e787346c3179a7e7d31ad99abdbc57c8', 746815],
+        }),
+      }),
+      response: {
+        200: Type.Object({
+          height:  Type.Number({
+            description: 'Stacks block height',
+            examples: [69320]
+          }),
+          hash: Type.String({
+            description: 'Stacks block hash',
+            examples: ['0x2e4a0e32bc4ea2cd747644a65c73d8f07873fd97ff0227d1aec2e9b264d37f6a']
+          }),
+          parent_block_hash: Type.String({
+            description: 'Stacks parent block hash',
+            examples: ['0xf6312ad452b7dbed13f3f6754d851e03d1f93d649a78f83ca977fe878b2df69c']
+          }),
+        }, {
+          description: 'Stacks block related to the given Bitcoin block'
+        })
+      }
+    }
+  }, async (req, reply) => {
+    let stxBlock: any;
+    if (typeof req.query['btc-block'] === 'string') {
+      const stxBlockRes = await request(
+        `${STACKS_API_ENDPOINT}/extended/v1/block/by_burn_block_hash/0x${req.query['btc-block']}`,
+        { method: 'GET' }
+      );
+      stxBlock = await stxBlockRes.body.json();
+    } else {
+      const stxBlockRes = await request(
+        `${STACKS_API_ENDPOINT}/extended/v1/block/by_burn_block_height/${req.query['btc-block']}`,
+        { method: 'GET' }
+      );
+      stxBlock = await stxBlockRes.body.json();
+    }
+    reply.type('application/json').send({
+      height: stxBlock.height,
+      hash: stxBlock.hash,
+      parent_block_hash: stxBlock.parent_block_hash
+    });
+  });
+}
+
 
   return router;
 }
--- src/api/routes/btc/utils.ts
+++ src/api/routes/btc/utils.ts
@@ -1,4 +1,18 @@
 import * as c32check from 'c32check';
+import * as btc from 'bitcoinjs-lib';
+import BigNumber from 'bignumber.js';
+
+const defaultFetchTimeout = 15_000; // 15 seconds
+
+// function throwFetchError(...args: [Error, string] | [string] | [Error]): never {
+//   if (args.length === 2) {
+//     const FetchError = new Error('FETCH_ERROR', 'Server fetch error: %s', 500);
+//     throw new FetchError(args[1]);
+//   } else {
+//     const FetchError = new Error('FETCH_ERROR', 'Server fetch error: %s', 500);
+//     throw new FetchError(args[0]);
+//   }
+// }
 
 /** Provide either a Stacks or Bitcoin address, and receive the Stacks address, Bitcoin address, and network version */
 export function getAddressInfo(addr: string, network: 'mainnet' | 'testnet' = 'mainnet') {
@@ -67,5 +81,249 @@ export function getAddressInfo(addr: string, network: 'mainnet' | 'testnet' = 'm
     stacks: stxAddr,
     bitcoin: b58addr,
     network: networkName,
+  };
+}
+
+/**
+ * Parse Stacks Leader Block Commit data from a Bitcoin tx output script. Returns null if script is not a Leader Block Commit.
+ * https://github.com/stacksgov/sips/blob/main/sips/sip-001/sip-001-burn-election.md#leader-block-commit
+ */
+export function decodeLeaderBlockCommit(txOutScript: string) {
+  // Total byte length w/ OP_RETURN and lead block commit message is 83 bytes
+  if (txOutScript.length !== 166) {
+    return null;
+  }
+
+  const opReturnHex = '6a';
+  if (!txOutScript.startsWith(opReturnHex)) {
+    return null;
+  }
+  const decompiled = btc.script.decompile(Buffer.from(txOutScript, 'hex'));
+  if (decompiled?.length !== 2) {
+    return null;
+  }
+  const scriptData = decompiled[1];
+  if (!Buffer.isBuffer(scriptData)) {
+    return null;
+  }
+
+  const magicBytes = [88, 50]; // X2
+  if (scriptData[0] !== magicBytes[0] || scriptData[1] !== magicBytes[1]) {
+    return null;
+  }
+
+  const opLeaderBlockCommit = Buffer.from('[');
+  const stxOp = scriptData.subarray(2, 3);
+  if (stxOp[0] !== opLeaderBlockCommit[0]) {
+    return null;
+  }
+
+  // header block hash of the Stacks anchored block
+  const blockHash = scriptData.subarray(3, 35);
+  const blockHashHex = blockHash.toString('hex');
+
+  // the next value for the VRF seed
+  const newSeed = scriptData.subarray(35, 67);
+  const newSeedHex = newSeed.toString('hex');
+
+  // the burn block height of this block's parent
+  const parentBlock = scriptData.subarray(67, 71);
+  const parentBlockInt = parentBlock.readUInt32BE(0);
+
+  // the vtxindex for this block's parent's block commit
+  const parentTxOffset = scriptData.subarray(71, 73);
+  const parentTxOffsetInt = parentTxOffset.readUInt16BE(0);
+
+  // the burn block height of the miner's VRF key registration
+  const keyBlock = scriptData.subarray(73, 77);
+  const keyBlockInt = keyBlock.readUInt32BE(0);
+
+  // the vtxindex for this miner's VRF key registration
+  const keyTxOffset = scriptData.subarray(77, 79);
+  const keyTxOffsetInt = keyTxOffset.readUInt16BE(0);
+
+  // the burn block height at which this leader block commit was created modulo BURN_COMMITMENT_WINDOW (=6).
+  // That is, if the block commit is included in the intended burn block then this value should be equal to: (commit_burn_height - 1) % 6.
+  // This field is used to link burn commitments from the same miner together even if a commitment was included in a late burn block.
+  const burnParentModulus = scriptData.subarray(79, 80)[0];
+
+  return {
+    blockHash: blockHashHex,
+    newSeed: newSeedHex,
+    parentBlock: parentBlockInt,
+    parentTxOffset: parentTxOffsetInt,
+    keyBlock: keyBlockInt,
+    keyTxOffset: keyTxOffsetInt,
+    burnParentModulus,
+  };
+}
+
+export async function fetchJson<TOkResponse = unknown, TErrorResponse = unknown>(args: {
+  url: URL;
+  init?: RequestInit | undefined;
+  timeoutMs?: number;
+}): Promise<
+  (
+    | {
+        result: 'ok';
+        status: number;
+        response: TOkResponse;
+      }
+    | {
+        result: 'error';
+        status: number;
+        response: TErrorResponse;
+      }
+  ) & { getCurlCmd: () => string }
+> {
+  const requestInit: RequestInit = {
+    signal: (AbortSignal as any).timeout(args.timeoutMs ?? defaultFetchTimeout),
+    ...args.init,
+  };
+  const headers = new Headers(requestInit.headers);
+  headers.set('Content-Type', 'application/json');
+  headers.set('Accept', 'application/json');
+  requestInit.headers = headers;
+  const req = new Request(args.url, requestInit);
+
+  const getCurlCmd = () => {
+    let curl = `curl -i -X ${req.method} '${req.url}'`;
+    if (args.init?.body) {
+      if (typeof args.init.body === 'string') {
+        curl += ` -H 'Content-Type: application/json' -d '${args.init.body.replace(
+          /'/g,
+          `'\\''`
+        )}'`;
+      } else {
+        throw new Error(`Can only create curl command for request bodies with string type`);
+      }
+    }
+    return curl;
+  };
+
+  let resp: Response;
+  try {
+    resp = await fetch(req);
+  } catch (error) {
+    const errorMsg = `${req.method} ${req.url} - error performing fetch: ${error}`;
+    // throwFetchError(error as Error, errorMsg);
+    throw error;
+  }
+
+  let respText = '';
+  try {
+    respText = await resp.text();
+  } catch (error) {
+    const errorMsg = `${req.method} ${req.url} - error reading response ${resp.status}: ${respText}`;
+    // throwFetchError(error as Error, errorMsg);
+    throw error;
+  }
+
+  let respBody: unknown;
+  try {
+    respBody = JSON.parse(respText);
+  } catch (error) {
+    if (resp.ok) {
+      const errorMsg = `${req.method} ${req.url} - error parsing JSON response ${resp.status}: ${respText}`;
+      //   throwFetchError(error as Error, errorMsg);
+      throw error;
+    }
+  }
+
+  if (resp.ok) {
+    return { result: 'ok', status: resp.status, response: respBody as TOkResponse, getCurlCmd };
+  } else {
+    return {
+      result: 'error',
+      status: resp.status,
+      response: (respBody ?? respText) as TErrorResponse,
+      getCurlCmd,
+    };
+  }
+}
+
+/**
+ * Parse a STX-transfer operation from a Bitcoin tx out script.
+ */
+export function decodeStxTransferOp(txOutScript: string) {
+  const opReturnHex = '6a';
+  if (!txOutScript.startsWith(opReturnHex)) {
+    return null;
+  }
+  const decompiled = btc.script.decompile(Buffer.from(txOutScript, 'hex'));
+  if (decompiled?.length !== 2) {
+    return null;
+  }
+  const scriptData = decompiled[1];
+  if (!Buffer.isBuffer(scriptData)) {
+    return null;
+  }
+
+  const magicBytes = [88, 50]; // X2
+  if (scriptData[0] !== magicBytes[0] || scriptData[1] !== magicBytes[1]) {
+    return null;
+  }
+
+  const stxTransferOpCode = Buffer.from('$');
+  const stxOp = scriptData.subarray(2, 3);
+  if (stxOp[0] !== stxTransferOpCode[0]) {
+    return null;
+  }
+
+  const microAmount = BigInt('0x' + scriptData.subarray(3, 19).toString('hex'));
+  const stxAmount = new BigNumber(microAmount.toString()).shiftedBy(-6).toFixed(6);
+
+  return {
+    stxAmount: stxAmount,
+  };
+}
+
+/**
+ * Parse Stacks Leader VRF Key Registration data from a Bitcoin tx output script. Returns null if script is not a Leader VRF Key Registration.
+ * https://github.com/stacksgov/sips/blob/main/sips/sip-001/sip-001-burn-election.md#leader-vrf-key-registrations
+ */
+export function decodeLeaderVrfKeyRegistration(txOutScript: string) {
+  const opReturnHex = '6a';
+  if (!txOutScript.startsWith(opReturnHex)) {
+    return null;
+  }
+  const decompiled = btc.script.decompile(Buffer.from(txOutScript, 'hex'));
+  if (decompiled?.length !== 2) {
+    return null;
+  }
+  let scriptData = decompiled[1];
+  if (!Buffer.isBuffer(scriptData)) {
+    return null;
+  }
+
+  const magicBytes = [88, 50]; // X2
+  if (scriptData[0] !== magicBytes[0] || scriptData[1] !== magicBytes[1]) {
+    return null;
+  }
+
+  const opLeaderVrfKeyRegistration = Buffer.from('^');
+  const stxOp = scriptData.subarray(2, 3);
+  if (stxOp[0] !== opLeaderVrfKeyRegistration[0]) {
+    return null;
+  }
+
+  // the current consensus hash for the burnchain state of the Stacks blockchain
+  const consensusHash = scriptData.subarray(3, 23);
+  const consensusHashHex = consensusHash.toString('hex');
+
+  // the 32-byte public key used in the miner's VRF proof
+  const provingPublicKey = scriptData.subarray(23, 55);
+  const provingPublicKeyHex = provingPublicKey.toString('hex');
+
+  // a field for including a miner memo
+  let memo: string | null = null;
+  if (scriptData.length > 55) {
+    memo = scriptData.subarray(55).toString('hex');
+  }
+
+  return {
+    consensusHash: consensusHashHex,
+    provingPublicKey: provingPublicKeyHex,
+    memo: memo,
   };
 }
