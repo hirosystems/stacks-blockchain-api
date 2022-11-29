4357857b912415ed3eed6be82fee9cf9de85d3ec
Matthew Little <zone117x@gmail.com>
2022-11-29 21:14:03 +0100

    ci: add missing gh job for `event-replay` test suite

--- tests/jest.config.event-replay.js
+++ tests/jest.config.event-replay.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  rootDir: 'src',
+  rootDir: `${require('path').dirname(__dirname)}/src`,
   testMatch: ['<rootDir>/tests-event-replay/**/*.ts'],
   testPathIgnorePatterns: [
     '<rootDir>/tests-event-replay/setup.ts',
@@ -8,7 +8,7 @@ module.exports = {
   ],
   collectCoverageFrom: ['<rootDir>/**/*.ts'],
   coveragePathIgnorePatterns: ['<rootDir>/tests*'],
-  coverageDirectory: '../coverage',
+  coverageDirectory: '<rootDir>/../coverage',
   globalSetup: '<rootDir>/tests-event-replay/setup.ts',
   globalTeardown: '<rootDir>/tests-event-replay/teardown.ts',
   testTimeout: 20000,
