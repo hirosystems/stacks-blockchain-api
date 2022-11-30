e8574f4aec6c856080e7fc8d98df9e615cddea07
janniks <janniks@users.noreply.github.com>
2022-11-30 21:34:37 +0100

    test: split cycle offset tests into separate files

---
+++ src/tests-2.1/pox-2-rosetta-cycle-offset-4.ts
@@ -0,0 +1,9 @@
+import { testnetKeys } from '../api/routes/debug';
+import { accountFromKey } from '../test-utils/test-helpers';
+import { testRosettaStackWithOffset } from './test-helper';
+
+// Assuming a reward cycle length of 5
+describe('PoX-2 - Rosetta - Stack in cycle, offset by 4', () => {
+  const account = accountFromKey(testnetKeys[1].secretKey);
+  testRosettaStackWithOffset({ account, offset: 4 });
+});
---
+++ src/tests-2.1/test-helper.ts
@@ -0,0 +1,54 @@
+import {
+  Account,
+  stackStxWithRosetta,
+  standByUntilBurnBlock,
+  testEnv,
+} from '../test-utils/test-helpers';
+
+/**
+ * Run tests shifted to different points in a cycle.
+ * `offset` refers to the offset/shift from the start of a cycle.
+ * i.e. 0th, 1st, 2nd, etc. block of a cycle
+ */
+export function testRosettaStackWithOffset({
+  account,
+  offset,
+}: {
+  account: Account;
+  offset: number;
+}) {
+  test('Standby for cycle block mod', async () => {
+    const poxInfo = await testEnv.client.getPox();
+    const startHeight = poxInfo.next_cycle.reward_phase_start_block_height + offset;
+
+    await standByUntilBurnBlock(startHeight);
+  });
+
+  test('Rosetta - stack-stx tx', async () => {
+    const poxInfo = await testEnv.client.getPox();
+    const ustxAmount = BigInt(poxInfo.current_cycle.min_threshold_ustx * 1.2);
+
+    expect((poxInfo.current_burnchain_block_height as number) % poxInfo.reward_cycle_length).toBe(
+      offset
+    );
+
+    await stackStxWithRosetta({
+      stacksAddress: account.stxAddr,
+      privateKey: account.secretKey,
+      pubKey: account.pubKey,
+      cycleCount: 1,
+      ustxAmount,
+      btcAddr: account.btcAddr,
+    });
+  });
+
+  test('Verify stacked', async () => {
+    const poxInfo = await testEnv.client.getPox();
+
+    // Ensure api has time to process
+    await standByUntilBurnBlock((poxInfo.current_burnchain_block_height as number) + 1);
+
+    const coreBalance = await testEnv.client.getAccount(account.stxAddr);
+    expect(coreBalance.unlock_height).toBeGreaterThan(0);
+  });
+}
