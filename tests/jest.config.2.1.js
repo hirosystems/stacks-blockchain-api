a56ac8c7d1b31d7e7c1297e4e15d8c1a3a0bafb2
Matthew Little <zone117x@gmail.com>
2022-11-30 14:13:04 +0100

    chore: split up pox-2-rosetta test suites into smaller units

--- tests/jest.config.2.1.js
+++ tests/jest.config.2.1.js
@@ -16,7 +16,7 @@ const config = {
   globalSetup: '<rootDir>/tests-2.1/global-setup.ts',
   globalTeardown: '<rootDir>/tests-2.1/global-teardown.ts',
   setupFilesAfterEnv: ['<rootDir>/tests-2.1/env-setup.ts'],
-  testTimeout: 200_000,
+  testTimeout: 60_000,
   verbose: true,
   bail: true,
 };
