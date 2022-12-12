1024026d9e50acfec9e4dd323a62c5852b6a082c
Nicholas Barnett <n-barnett@hotmail.com>
2022-12-12 16:16:00 -0600

    fix(bns-test): clearing timeout and making sure settimeout is nto mocked

--- src/tests-bns/event-server-tests.ts
+++ src/tests-bns/event-server-tests.ts
@@ -1052,6 +1052,7 @@ describe('BNS event server tests', () => {
   })
 
   test('BNS middleware is async. /new_block posts return before importing BNS finishes', async () => {
+    jest.useRealTimers();
     process.env.BNS_IMPORT_DIR = 'src/tests-bns/import-test-files';
     const genesisBlock = await getGenesisBlockData('src/tests-event-replay/tsv/mainnet.tsv');
 
@@ -1067,5 +1068,16 @@ describe('BNS event server tests', () => {
     const configState = await db.getConfigState();
     expect(configState.bns_names_onchain_imported).toBe(false)
     expect(configState.bns_subdomains_imported).toBe(false)
+
+    const timeoutId: NodeJS.Timeout = await new Promise(resolve => {
+      const timeoutId = setTimeout(async() => {
+        const configState = await db.getConfigState();
+        expect(configState.bns_names_onchain_imported).toBe(true)
+        expect(configState.bns_subdomains_imported).toBe(true)
+        resolve(timeoutId)
+      }, 2000)
+    })
+
+    clearTimeout(timeoutId);  
   })
 })
