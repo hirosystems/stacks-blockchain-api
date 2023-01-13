35c28def8b5b1413174315e74fcac4c1f1ac904b
Matthew Little <zone117x@gmail.com>
2023-01-13 19:00:52 +0100

    chore: pg down migration must be manually specified for alterColumn type

--- migrations/1673632140730_pox_state_fix.js
+++ migrations/1673632140730_pox_state_fix.js
@@ -6,3 +6,10 @@ exports.up = pgm => {
   });
 
 }
+
+/** @param { import("node-pg-migrate").MigrationBuilder } pgm */
+exports.down = pgm => {
+  pgm.alterColumn('pox_state', 'pox_v1_unlock_height', {
+    type: 'integer'
+  });
+}
