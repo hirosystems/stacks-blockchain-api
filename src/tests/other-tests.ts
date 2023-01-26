8dd785d373546c138c4718f8a6b235d5062a0126
Matthew Little <zone117x@gmail.com>
2023-01-26 12:41:03 +0100

    test: update unit tests for stx supply

--- src/tests/other-tests.ts
+++ src/tests/other-tests.ts
@@ -128,8 +128,8 @@ describe('other tests', () => {
     expect(result1.status).toBe(200);
     expect(result1.type).toBe('application/json');
     const expectedResp1 = {
-      unlocked_percent: '17.38',
-      total_stx: '1352464600.000000',
+      unlocked_percent: '12.93',
+      total_stx: '1818000000.000000',
       unlocked_stx: microStxToStx(expectedTotalStx1),
       block_height: dbBlock1.block_height,
     };
@@ -153,8 +153,8 @@ describe('other tests', () => {
     expect(result2.status).toBe(200);
     expect(result2.type).toBe('application/json');
     const expectedResp2 = {
-      unlocked_percent: '16.64',
-      total_stx: '1352464600.000000',
+      unlocked_percent: '12.38',
+      total_stx: '1818000000.000000',
       unlocked_stx: microStxToStx(expectedTotalStx2),
       block_height: dbBlock1.block_height,
     };
@@ -183,8 +183,8 @@ describe('other tests', () => {
     expect(result3.status).toBe(200);
     expect(result3.type).toBe('application/json');
     const expectedResp3 = {
-      unlocked_percent: '17.75',
-      total_stx: '1352464600.000000',
+      unlocked_percent: '13.20',
+      total_stx: '1818000000.000000',
       unlocked_stx: microStxToStx(expectedTotalStx3),
       block_height: dbBlock1.block_height,
     };
@@ -193,7 +193,7 @@ describe('other tests', () => {
     const result4 = await supertest(api.server).get(`/extended/v1/stx_supply/total/plain`);
     expect(result4.status).toBe(200);
     expect(result4.type).toBe('text/plain');
-    expect(result4.text).toEqual('1352464600.000000');
+    expect(result4.text).toEqual('1818000000.000000');
 
     const result5 = await supertest(api.server).get(`/extended/v1/stx_supply/circulating/plain`);
     expect(result5.status).toBe(200);
@@ -205,9 +205,9 @@ describe('other tests', () => {
     expect(result6.status).toBe(200);
     expect(result6.type).toBe('application/json');
     const expectedResp6 = {
-      unlockedPercent: '17.75',
-      totalStacks: '1352464600.000000',
-      totalStacksFormatted: '1,352,464,600.000000',
+      unlockedPercent: '13.20',
+      totalStacks: '1818000000.000000',
+      totalStacksFormatted: '1,818,000,000.000000',
       unlockedSupply: microStxToStx(expectedTotalStx3),
       unlockedSupplyFormatted: new Intl.NumberFormat('en', {
         minimumFractionDigits: STACKS_DECIMAL_PLACES,
