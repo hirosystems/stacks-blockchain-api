2f9057c47f65bd7bc8f162d026278a4299f39fad
Rafael Cárdenas <rafael@rafaelcr.com>
2023-01-27 10:53:47 -0600

    fix: add block_height index to contract_logs

---
+++ src/migrations/1674838340313_contract_logs_block_height_index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createIndex('contract_logs', [
+    { name: 'block_height', sort: 'DESC' },
+    { name: 'microblock_sequence', sort: 'DESC' },
+    { name: 'tx_index', sort: 'DESC' },
+    { name: 'event_index', sort: 'DESC' },
+  ]);
+}
