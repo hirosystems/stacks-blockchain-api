de6433165bff0190fcbba7ceaba1dd52f362536b
Matthew Little <zone117x@gmail.com>
2023-02-10 22:10:15 +0100

    test: validation of synthetic tx for L1 deposit-nft-asset

--- src/tests-subnets/subnet-tests.ts
+++ src/tests-subnets/subnet-tests.ts
@@ -3,6 +3,7 @@ import {
   TransactionResults,
   TokenTransferTransaction,
   TransactionEventsResponse,
+  ContractCallTransaction,
 } from '@stacks/stacks-blockchain-api-types';
 import * as supertest from 'supertest';
 import {
@@ -289,7 +290,7 @@ describe('Subnets tests', () => {
       await standByUntilBurnBlock(curBlock.stacks_tip_height + 1);
     });
 
-    test('Step 4: Deposit the NFT onto the subnet', async () => {
+    test('Step 4a: Deposit the NFT onto the subnet', async () => {
       while (true) {
         try {
           const tx = await makeContractCall({
@@ -321,6 +322,84 @@ describe('Subnets tests', () => {
 
       const curBlock = await l1Client.getInfo();
       await standByUntilBurnBlock(curBlock.stacks_tip_height + 1);
+    });
+
+    test('Step 4b: Verify deposit-nft-asset synthetic tx', async () => {
+      const expectedContractID = `${accounts.USER.addr}.simple-nft-l2`;
+      while (true) {
+        const resp = await supertest(testEnv.api.server)
+          .get(`/extended/v1/tx?limit=1&type=contract_call`)
+          .expect(200);
+        const txListResp = resp.body as TransactionResults;
+        const tx = txListResp.results[0] as ContractCallTransaction;
+        if (txListResp.total === 0 || tx.contract_call.contract_id !== expectedContractID) {
+          await timeout(200);
+          continue;
+        }
+        expect(tx).toEqual(
+          expect.objectContaining({
+            anchor_mode: 'any',
+            canonical: true,
+            contract_call: {
+              contract_id: expectedContractID,
+              function_args: [
+                {
+                  hex: '0x0100000000000000000000000000000005',
+                  name: 'id',
+                  repr: 'u5',
+                  type: 'uint',
+                },
+                {
+                  hex: '0x051a43596b5386f466863e25658ddf94bd0fadab0048',
+                  name: 'recipient',
+                  repr: `'${accounts.USER.addr}`,
+                  type: 'principal',
+                },
+              ],
+              function_name: 'deposit-from-burnchain',
+              function_signature:
+                '(define-public (deposit-from-burnchain (id uint) (recipient principal)))',
+            },
+            event_count: 1,
+            events: [],
+            fee_rate: '0',
+            post_condition_mode: 'allow',
+            post_conditions: [],
+            sender_address: accounts.USER.addr,
+            sponsored: false,
+            tx_index: 0,
+            tx_result: {
+              hex: '0x0703',
+              repr: '(ok true)',
+            },
+            tx_status: 'success',
+            tx_type: 'contract_call',
+          })
+        );
+
+        const respEvents = await supertest(testEnv.api.server)
+          .get(`/extended/v1/tx/events?tx_id=${tx.tx_id}`)
+          .expect(200);
+        const txEvents = respEvents.body.events as TransactionEventsResponse['results'];
+        expect(txEvents).toEqual([
+          {
+            asset: {
+              asset_event_type: 'mint',
+              asset_id: `${accounts.USER.addr}.simple-nft-l2::nft-token`,
+              recipient: accounts.USER.addr,
+              sender: '',
+              value: {
+                hex: '0x0100000000000000000000000000000005',
+                repr: 'u5',
+              },
+            },
+            event_index: 0,
+            event_type: 'non_fungible_token_asset',
+            tx_id: tx.tx_id,
+          },
+        ]);
+        break;
+      }
     });
 
     test('Step 5: Transfer the NFT within the subnet', async () => {
