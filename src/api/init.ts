34827421071961423b3953b008c5977e42add679
Chris Guimaraes <cguimaraes@hiro.so>
2023-04-27 10:48:40 +0100

    feat: add logging information to ErrorRequestHandler

--- src/api/init.ts
+++ src/api/init.ts
@@ -306,10 +306,13 @@ export async function startApiServer(opts: {
     }
     if (error && !res.headersSent) {
       if (error instanceof InvalidRequestError) {
+        logger.warn(error, error.message);
         res.status(error.status).json({ error: error.message }).end();
       } else if (isPgConnectionError(error)) {
+        logger.error(error, error.message);
         res.status(503).json({ error: `The database service is unavailable` }).end();
       } else {
+        logger.error(error, error.message);
         res.status(500);
         const errorTag = uuid();
         Object.assign(error, { errorTag: errorTag });
