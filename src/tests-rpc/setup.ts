2191bace20226ad70d3aadef578cabf587f17f32
Matthew Little <zone117x@gmail.com>
2023-05-08 13:50:57 +0200

    tests: fix tokens tests

--- src/tests-rpc/setup.ts
+++ src/tests-rpc/setup.ts
@@ -1,54 +1,8 @@
-import { cycleMigrations } from '../datastore/migrations';
-import { StacksCoreRpcClient } from '../core-rpc/client';
-import { loadDotEnv, timeout } from '../helpers';
-import { PgWriteStore } from '../datastore/pg-write-store';
-import { EventStreamServer, startEventServer } from '../event-stream/event-server';
-import { ChainID } from '@stacks/common';
-
-export interface GlobalTestEnv {
-  db: PgWriteStore;
-  eventServer: EventStreamServer;
-}
-
-async function standByForPoxToBeReady(client: StacksCoreRpcClient): Promise<void> {
-  let tries = 0;
-  while (true) {
-    try {
-      tries++;
-      const poxInfo = await client.getPox();
-      if (!poxInfo.contract_id.includes('pox-3')) {
-        throw new Error(`Unexpected PoX version: ${poxInfo.contract_id}`);
-      }
-      break;
-    } catch (error) {
-      console.log(`Waiting on PoX-3 to be ready, retrying after ${error}`);
-      await timeout(500);
-    }
-  }
-}
+import { defaultSetupInit } from '../test-utils/shared-setup';
 
 // ts-unused-exports:disable-next-line
 export default async () => {
   console.log('Jest - setup..');
-  if (!process.env.NODE_ENV) {
-    process.env.NODE_ENV = 'test';
-  }
-  loadDotEnv();
-  process.env.PG_DATABASE = 'postgres';
-  process.env.STACKS_CHAIN_ID = '0x80000000';
-
-  await cycleMigrations();
-  const db = await PgWriteStore.connect({ usageName: 'tests' });
-  const eventServer = await startEventServer({ datastore: db, chainId: ChainID.Testnet });
-
-  const client = new StacksCoreRpcClient();
-  await standByForPoxToBeReady(client);
-
-  const testEnv: GlobalTestEnv = {
-    db: db,
-    eventServer: eventServer,
-  };
-  Object.assign(global, { globalTestEnv: testEnv });
-
+  await defaultSetupInit();
   console.log('Jest - setup done');
 };
--- src/tests-tokens/teardown.ts
+++ src/tests-tokens/teardown.ts
@@ -1,5 +1,8 @@
+import { defaultSetupTeardown } from '../test-utils/shared-setup';
+
 // ts-unused-exports:disable-next-line
-export default (): void => {
+export default async () => {
   console.log('Jest - teardown..');
+  await defaultSetupTeardown();
   console.log('Jest - teardown done');
 };
