f08609a08e65073c9089756a348721e9dc9119b9
Matthew Little <zone117x@gmail.com>
2023-05-09 12:38:35 +0200

    test: verbose logging for `rosetta-cli-data`

--- tests/jest.config.rosetta-cli-data.js
+++ tests/jest.config.rosetta-cli-data.js
@@ -1,5 +1,7 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest',
+  testEnvironment: 'node',
   rootDir: `${require('path').dirname(__dirname)}/src`,
   testMatch: ['<rootDir>/tests-rosetta-cli-data/**/*.ts'],
   testPathIgnorePatterns: ['<rootDir>/tests-rosetta-cli-data/setup.ts', '<rootDir>/tests-rosetta-cli-data/teardown.ts'],
@@ -9,7 +11,7 @@ module.exports = {
   globalSetup: '<rootDir>/tests-rosetta-cli-data/setup.ts',
   globalTeardown: '<rootDir>/tests-rosetta-cli-data/teardown.ts',
   testTimeout: 240000,
-  transformIgnorePatterns: [
-    "node_modules/(?!(@stacks/stacks-transactions)/)"
-  ]
+  verbose: true,
 };
+
+module.exports = config;
