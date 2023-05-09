7c750087f96494522499375ed10df9d03599c65a
Chris Guimaraes <cguimaraes@hiro.so>
2023-05-09 13:10:55 +0100

    feat: remove old ft metadata

--- src/api/routes/tokens/tokens.ts
+++ src/api/routes/tokens/tokens.ts
@@ -229,32 +229,6 @@ export function createTokenRouter(db: PgStore): express.Router {
   );
 
   router.get(
-    '/ft/metadata',
-    asyncHandler(async (req, res) => {
-      if (!isFtMetadataEnabled()) {
-        res.status(500).json({
-          error: 'FT metadata processing is not enabled on this server',
-        });
-        return;
-      }
-
-      const limit = getPagingQueryLimit(ResourceType.Token, req.query.limit);
-      const offset = parsePagingQueryInput(req.query.offset ?? 0);
-
-      const { results, total } = await db.getFtMetadataList({ offset, limit });
-
-      const response: FungibleTokensMetadataList = {
-        limit: limit,
-        offset: offset,
-        total: total,
-        results: results,
-      };
-
-      res.status(200).json(response);
-    })
-  );
-
-  router.get(
     '/nft/metadata',
     asyncHandler(async (req, res) => {
       if (!isNftMetadataEnabled()) {
@@ -283,51 +257,6 @@ export function createTokenRouter(db: PgStore): express.Router {
         results: results,
       };
 
-      res.status(200).json(response);
-    })
-  );
-
-  router.get(
-    '/:contractId/ft/metadata',
-    asyncHandler(async (req, res) => {
-      if (!isFtMetadataEnabled()) {
-        res.status(500).json({
-          error: 'FT metadata processing is not enabled on this server',
-        });
-        return;
-      }
-
-      const { contractId } = req.params;
-
-      const metadata = await db.getFtMetadata(contractId);
-      if (!metadata.found) {
-        res.status(404).json({ error: 'tokens not found' });
-        return;
-      }
-
-      const {
-        token_uri,
-        name,
-        description,
-        image_uri,
-        image_canonical_uri,
-        symbol,
-        decimals,
-        tx_id,
-        sender_address,
-      } = metadata.result;
-
-      const response: FungibleTokenMetadata = {
-        token_uri: token_uri,
-        name: name,
-        description: description,
-        image_uri: image_uri,
-        image_canonical_uri: image_canonical_uri,
-        symbol: symbol,
-        decimals: decimals,
-        tx_id: tx_id,
-        sender_address: sender_address,
-      };
       res.status(200).json(response);
     })
   );
