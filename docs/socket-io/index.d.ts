aa82e8d1711baff84ce1607d3a3ff49d419eed18
Matthew Little <zone117x@gmail.com>
2024-01-05 17:43:49 +0100

    fix: socket-io client should not disconnect with no event reply

--- docs/socket-io/index.d.ts
+++ docs/socket-io/index.d.ts
@@ -24,23 +24,19 @@ export type Topic =
   | NftAssetEventTopic
   | NftCollectionEventTopic;
 
-// Allows timeout callbacks for messages. See
-// https://socket.io/docs/v4/typescript/#emitting-with-a-timeout
-type WithTimeoutAck<isSender extends boolean, args extends any[]> = isSender extends true ? [Error, ...args] : args;
-
 export interface ClientToServerMessages {
   subscribe: (topic: Topic | Topic[], callback: (error: string | null) => void) => void;
   unsubscribe: (...topic: Topic[]) => void;
 }
 
-export interface ServerToClientMessages<isSender extends boolean = false> {
-  block: (block: Block, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
-  microblock: (microblock: Microblock, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
-  mempool: (transaction: MempoolTransaction, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
-  'nft-event': (event: NftEvent, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
-  [key: TransactionTopic]: (transaction: Transaction | MempoolTransaction, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
-  [key: NftAssetEventTopic]: (assetIdentifier: string, value: string, event: NftEvent, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
-  [key: NftCollectionEventTopic]: (assetIdentifier: string, event: NftEvent, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
-  [key: AddressTransactionTopic]: (address: string, stxBalance: AddressTransactionWithTransfers, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
-  [key: AddressStxBalanceTopic]: (address: string, stxBalance: AddressStxBalanceResponse, callback: (...args: WithTimeoutAck<isSender, [string]>) => void) => void;
+export interface ServerToClientMessages {
+  block: (block: Block) => void;
+  microblock: (microblock: Microblock) => void;
+  mempool: (transaction: MempoolTransaction) => void;
+  'nft-event': (event: NftEvent) => void;
+  [key: TransactionTopic]: (transaction: Transaction | MempoolTransaction) => void;
+  [key: NftAssetEventTopic]: (assetIdentifier: string, value: string, event: NftEvent) => void;
+  [key: NftCollectionEventTopic]: (assetIdentifier: string, event: NftEvent) => void;
+  [key: AddressTransactionTopic]: (address: string, stxBalance: AddressTransactionWithTransfers) => void;
+  [key: AddressStxBalanceTopic]: (address: string, stxBalance: AddressStxBalanceResponse) => void;
 }
