5505d354ecae6e52c751b3b634752fd56d24642f
Rafael Cárdenas <rafael@rafaelcr.com>
2024-01-12 08:04:56 -0600

    fix: optimize re-org queries and indexes (#1821)

--- src/tests/datastore-tests.ts
+++ src/tests/datastore-tests.ts
@@ -167,7 +167,7 @@ describe('postgres datastore', () => {
       createStxEvent('addrA', 'addrC', 35),
     ];
     for (const event of events) {
-      await db.updateStxEvent(client, tx, event);
+      await db.updateStxEvents(client, [{ tx, stxEvents: [event] }]);
     }
 
     const createStxLockEvent = (
