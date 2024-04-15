db13a1b2159fe5ad1ac0bac3b7e0a2cd90a13590
Rafael Cárdenas <rafael@rafaelcr.com>
2024-04-15 10:53:12 -0600

    Delete config/token-metadata-image-cache-imgix.js

--- config/token-metadata-image-cache-imgix.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node 
-const imgUrl = process.argv[2];
-const encodedUrl = encodeURIComponent(imgUrl);
-const [imgixDomain, imgixToken] = [process.env['IMGIX_DOMAIN'], process.env['IMGIX_TOKEN']];
-const signature = require('crypto').createHash('md5').update(imgixToken + '/' + encodedUrl).digest('hex');
-const resultUrl = new URL(encodedUrl + '?s=' + signature, imgixDomain);
-console.log(resultUrl.toString());
