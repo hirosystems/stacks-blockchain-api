538f243b5a5c6ac4ad467112a4e63b57d2cede25
Rafael Cardenas <rafael@rafaelcr.com>
2025-05-16 14:51:15 -0600

    fix: tests

--- src/datastore/pg-write-store.ts
+++ src/datastore/pg-write-store.ts
@@ -1896,7 +1896,7 @@ export class PgWriteStore extends PgStore {
       execution_cost_runtime: tx.execution_cost_runtime,
       execution_cost_write_count: tx.execution_cost_write_count,
       execution_cost_write_length: tx.execution_cost_write_length,
-      vm_error: tx.vm_error ?? null,
+      vm_error: tx.vm_error,
     }));
 
     let count = 0;
@@ -3493,7 +3493,7 @@ export class PgWriteStore extends PgStore {
       execution_cost_runtime: tx.execution_cost_runtime,
       execution_cost_write_count: tx.execution_cost_write_count,
       execution_cost_write_length: tx.execution_cost_write_length,
-      vm_error: tx.vm_error ?? null,
+      vm_error: tx.vm_error,
     }));
     await sql`INSERT INTO txs ${sql(values)}`;
   }
