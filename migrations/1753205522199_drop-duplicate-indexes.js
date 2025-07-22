0955a5ebec2f8cca973b7fd5e91dc72a750ad0b5
Rafael Cardenas <rafael@rafaelcr.com>
2025-07-22 11:39:56 -0600

    fix: explicitly add down

--- migrations/1753205522199_drop-duplicate-indexes.js
+++ migrations/1753205522199_drop-duplicate-indexes.js
@@ -8,3 +8,10 @@ exports.up = pgm => {
   pgm.dropIndex('mempool_txs', ['sponsor_address']);
   pgm.dropIndex('nft_events', ['asset_identifier']);
 };
+
+exports.down = pgm => {
+  pgm.createIndex('ft_balances', ['token']);
+  pgm.createIndex('mempool_txs', ['sender_address']);
+  pgm.createIndex('mempool_txs', ['sponsor_address']);
+  pgm.createIndex('nft_events', ['asset_identifier']);
+};
