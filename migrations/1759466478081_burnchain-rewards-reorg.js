d99ad5e4454ad733d33cfbad39034ac7888e56fd
Rafael Cardenas <rafael@rafaelcr.com>
2025-10-20 10:59:05 -0600

    fix: simplify migration

--- migrations/1759466478081_burnchain-rewards-reorg.js
+++ migrations/1759466478081_burnchain-rewards-reorg.js
@@ -4,17 +4,13 @@ exports.shorthands = undefined;
 
 exports.up = pgm => {
   pgm.sql(`
-    WITH burn_blocks AS (
-      SELECT DISTINCT ON (burn_block_height) burn_block_hash, canonical
-      FROM blocks
-      ORDER BY burn_block_height DESC, block_height DESC
-    )
     UPDATE burnchain_rewards
     SET canonical = COALESCE(
       (
         SELECT canonical
-        FROM burn_blocks
-        WHERE burnchain_rewards.burn_block_hash = burn_blocks.burn_block_hash
+        FROM blocks
+        WHERE blocks.burn_block_hash = burnchain_rewards.burn_block_hash
+        LIMIT 1
       ),
       false
     )
