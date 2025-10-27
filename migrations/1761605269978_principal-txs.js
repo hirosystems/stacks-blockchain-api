837a0c9250992a86539ab559737953a0c8424692
Rafael Cardenas <rafael@rafaelcr.com>
2025-10-27 16:51:07 -0600

    fix: migration order

---
+++ migrations/1761605269978_principal-txs.js
@@ -0,0 +1,218 @@
+/** @param { import("node-pg-migrate").MigrationBuilder } pgm */
+exports.up = pgm => {
+  pgm.dropTable('principal_stx_txs');
+
+  pgm.addColumn('txs', {
+    stx_transfer_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    stx_mint_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    stx_burn_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    stx_lock_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    ft_transfer_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    ft_mint_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    ft_burn_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    nft_transfer_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    nft_mint_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    nft_burn_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+    contract_log_event_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0,
+    },
+  });
+
+  /**
+   * Stores all `tx_id`s of transactions that affect a principal's STX balance since that cannot be
+   * directly determined from the `txs` table (an expensive JOIN with `stx_events` is required).
+   */
+  pgm.createTable('principal_txs', {
+    principal: {
+      type: 'string',
+      notNull: true,
+    },
+    tx_id: {
+      type: 'bytea',
+      notNull: true,
+    },
+    stx_balance_affected: {
+      type: 'boolean',
+      notNull: true,
+    },
+    ft_balance_affected: {
+      type: 'boolean',
+      notNull: true,
+    },
+    nft_balance_affected: {
+      type: 'boolean',
+      notNull: true,
+    },
+    stx_sent: {
+      type: 'bigint',
+      notNull: true,
+    },
+    stx_received: {
+      type: 'bigint',
+      notNull: true,
+    },
+    block_height: {
+      type: 'integer',
+      notNull: true,
+    },
+    index_block_hash: {
+      type: 'bytea',
+      notNull: true,
+    },
+    microblock_hash: {
+      type: 'bytea',
+      notNull: true,
+    },
+    microblock_sequence: {
+      type: 'integer',
+      notNull: true,
+    },
+    tx_index: {
+      type: 'smallint',
+      notNull: true,
+    },
+    canonical: {
+      type: 'boolean',
+      notNull: true,
+    },
+    microblock_canonical: {
+      type: 'boolean',
+      notNull: true,
+    },
+  });
+  pgm.createIndex('principal_txs', 'tx_id');
+  pgm.createIndex(
+    'principal_txs',
+    [
+      { name: 'principal' },
+      { name: 'block_height', sort: 'DESC' },
+      { name: 'microblock_sequence', sort: 'DESC' },
+      { name: 'tx_index', sort: 'DESC' },
+    ],
+    {
+      where: 'canonical = TRUE AND microblock_canonical = TRUE',
+    }
+  );
+  pgm.addConstraint(
+    'principal_txs',
+    'unique_principal_tx_id_index_block_hash_microblock_hash',
+    `UNIQUE(principal, tx_id, index_block_hash, microblock_hash)`
+  );
+};
+
+exports.down = pgm => {
+  pgm.dropTable('principal_txs');
+  pgm.createTable('principal_stx_txs', {
+    id: {
+      type: 'serial',
+      primaryKey: true,
+    },
+    principal: {
+      type: 'string',
+      notNull: true,
+    },
+    tx_id: {
+      type: 'bytea',
+      notNull: true,
+    },
+    block_height: {
+      type: 'integer',
+      notNull: true,
+    },
+    index_block_hash: {
+      type: 'bytea',
+      notNull: true,
+    },
+    microblock_hash: {
+      type: 'bytea',
+      notNull: true,
+    },
+    microblock_sequence: {
+      type: 'integer',
+      notNull: true,
+    },
+    tx_index: {
+      type: 'smallint',
+      notNull: true,
+    },
+    canonical: {
+      type: 'boolean',
+      notNull: true,
+    },
+    microblock_canonical: {
+      type: 'boolean',
+      notNull: true,
+    },
+  });
+  pgm.createIndex('principal_stx_txs', 'tx_id');
+  pgm.createIndex(
+    'principal_stx_txs',
+    [
+      { name: 'principal' },
+      { name: 'block_height', sort: 'DESC' },
+      { name: 'microblock_sequence', sort: 'DESC' },
+      { name: 'tx_index', sort: 'DESC' },
+    ],
+    {
+      where: 'canonical = TRUE AND microblock_canonical = TRUE',
+    }
+  );
+  pgm.addConstraint(
+    'principal_stx_txs',
+    'unique_principal_tx_id_index_block_hash_microblock_hash',
+    `UNIQUE(principal, tx_id, index_block_hash, microblock_hash)`
+  );
+  pgm.dropColumn('txs', 'stx_transfer_event_count');
+  pgm.dropColumn('txs', 'stx_mint_event_count');
+  pgm.dropColumn('txs', 'stx_burn_event_count');
+  pgm.dropColumn('txs', 'stx_lock_event_count');
+  pgm.dropColumn('txs', 'ft_transfer_event_count');
+  pgm.dropColumn('txs', 'ft_mint_event_count');
+  pgm.dropColumn('txs', 'ft_burn_event_count');
+  pgm.dropColumn('txs', 'nft_transfer_event_count');
+  pgm.dropColumn('txs', 'nft_mint_event_count');
+  pgm.dropColumn('txs', 'nft_burn_event_count');
+  pgm.dropColumn('txs', 'contract_log_event_count');
+};
