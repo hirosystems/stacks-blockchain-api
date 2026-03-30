2b8cf2cac4993f336182a00d0dbab160705ba197
Rafa Cárdenas <253999660+rafa-stacks@users.noreply.github.com>
2026-03-30 09:48:24 -0600

    fix: rename block time migration (#2519)

--- migrations/1774000000000_block-time-index.ts
+++ migrations/1774000000000_block-time-index.ts
@@ -1,12 +1,12 @@
-/** @param { import("node-pg-migrate").MigrationBuilder } pgm */
-exports.up = pgm => {
+import type { MigrationBuilder } from 'node-pg-migrate';
+
+export const up = (pgm: MigrationBuilder) => {
   pgm.createIndex('blocks', [{ name: 'block_time', sort: 'DESC' }], {
     where: 'canonical = true',
     name: 'blocks_canonical_block_time_desc_idx',
   });
 };
 
-/** @param { import("node-pg-migrate").MigrationBuilder } pgm */
-exports.down = pgm => {
+export const down = (pgm: MigrationBuilder) => {
   pgm.dropIndex('blocks', [], { name: 'blocks_canonical_block_time_desc_idx' });
 };
