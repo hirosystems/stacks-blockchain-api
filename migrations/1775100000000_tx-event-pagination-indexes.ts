97af18d0172e9a8755464ab1d82e040407fd7a0c
Rafa Cardenas <253999660+rafa-stacks@users.noreply.github.com>
2026-05-27 20:13:52 -0600

    remove index removal

--- migrations/1775100000000_tx-event-pagination-indexes.ts
+++ migrations/1775100000000_tx-event-pagination-indexes.ts
@@ -28,22 +28,9 @@ export const up = (pgm: MigrationBuilder) => {
     where: CANONICAL_EVENT_WHERE,
     ifNotExists: true,
   });
-
-  // Redundant after adding (tx_id, event_index) indexes above.
-  pgm.dropIndex('stx_events', 'tx_id', { ifExists: true });
-  pgm.dropIndex('ft_events', 'tx_id', { ifExists: true });
-  pgm.dropIndex('nft_events', 'tx_id', { ifExists: true });
-  pgm.dropIndex('stx_lock_events', 'tx_id', { ifExists: true });
-  pgm.dropIndex('contract_logs', 'tx_id', { ifExists: true });
 };
 
 export const down = (pgm: MigrationBuilder) => {
-  pgm.createIndex('stx_events', 'tx_id', { ifNotExists: true });
-  pgm.createIndex('ft_events', 'tx_id', { ifNotExists: true });
-  pgm.createIndex('nft_events', 'tx_id', { ifNotExists: true });
-  pgm.createIndex('stx_lock_events', 'tx_id', { ifNotExists: true });
-  pgm.createIndex('contract_logs', 'tx_id', { ifNotExists: true });
-
   pgm.dropIndex('stx_events', [], {
     name: 'stx_events_canonical_tx_id_event_index_idx',
     ifExists: true,
