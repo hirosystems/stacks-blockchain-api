e39a882df55b603eae8c51f53bd593ffa8607f5c
Rafa Cárdenas <253999660+rafa-stacks@users.noreply.github.com>
2026-05-27 20:23:20 -0600

    fix: only modify event indexes if they dont exist (#2567)
    * fix: only modify event indexes if they dont exist
    
    * remove index removal

--- migrations/1775100000000_tx-event-pagination-indexes.ts
+++ migrations/1775100000000_tx-event-pagination-indexes.ts
@@ -6,44 +6,49 @@ export const up = (pgm: MigrationBuilder) => {
   pgm.createIndex('stx_events', ['tx_id', 'event_index'], {
     name: 'stx_events_canonical_tx_id_event_index_idx',
     where: CANONICAL_EVENT_WHERE,
+    ifNotExists: true,
   });
   pgm.createIndex('ft_events', ['tx_id', 'event_index'], {
     name: 'ft_events_canonical_tx_id_event_index_idx',
     where: CANONICAL_EVENT_WHERE,
+    ifNotExists: true,
   });
   pgm.createIndex('nft_events', ['tx_id', 'event_index'], {
     name: 'nft_events_canonical_tx_id_event_index_idx',
     where: CANONICAL_EVENT_WHERE,
+    ifNotExists: true,
   });
   pgm.createIndex('stx_lock_events', ['tx_id', 'event_index'], {
     name: 'stx_lock_events_canonical_tx_id_event_index_idx',
     where: CANONICAL_EVENT_WHERE,
+    ifNotExists: true,
   });
   pgm.createIndex('contract_logs', ['tx_id', 'event_index'], {
     name: 'contract_logs_canonical_tx_id_event_index_idx',
     where: CANONICAL_EVENT_WHERE,
+    ifNotExists: true,
   });
-
-  // Redundant after adding (tx_id, event_index) indexes above.
-  pgm.dropIndex('stx_events', 'tx_id');
-  pgm.dropIndex('ft_events', 'tx_id');
-  pgm.dropIndex('nft_events', 'tx_id');
-  pgm.dropIndex('stx_lock_events', 'tx_id');
-  pgm.dropIndex('contract_logs', 'tx_id');
 };
 
 export const down = (pgm: MigrationBuilder) => {
-  pgm.createIndex('stx_events', 'tx_id');
-  pgm.createIndex('ft_events', 'tx_id');
-  pgm.createIndex('nft_events', 'tx_id');
-  pgm.createIndex('stx_lock_events', 'tx_id');
-  pgm.createIndex('contract_logs', 'tx_id');
-
-  pgm.dropIndex('stx_events', [], { name: 'stx_events_canonical_tx_id_event_index_idx' });
-  pgm.dropIndex('ft_events', [], { name: 'ft_events_canonical_tx_id_event_index_idx' });
-  pgm.dropIndex('nft_events', [], { name: 'nft_events_canonical_tx_id_event_index_idx' });
+  pgm.dropIndex('stx_events', [], {
+    name: 'stx_events_canonical_tx_id_event_index_idx',
+    ifExists: true,
+  });
+  pgm.dropIndex('ft_events', [], {
+    name: 'ft_events_canonical_tx_id_event_index_idx',
+    ifExists: true,
+  });
+  pgm.dropIndex('nft_events', [], {
+    name: 'nft_events_canonical_tx_id_event_index_idx',
+    ifExists: true,
+  });
   pgm.dropIndex('stx_lock_events', [], {
     name: 'stx_lock_events_canonical_tx_id_event_index_idx',
+    ifExists: true,
   });
-  pgm.dropIndex('contract_logs', [], { name: 'contract_logs_canonical_tx_id_event_index_idx' });
+  pgm.dropIndex('contract_logs', [], {
+    name: 'contract_logs_canonical_tx_id_event_index_idx',
+    ifExists: true,
+  });
 };
